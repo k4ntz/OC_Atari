c2c42c0a2a73921704fa002c1f99022638a8c3e5
Quentin Delfosse <q.delfosse@gmail.com>
2022-10-10 15:30:50 +0200

    First code

---
+++ extract_ram_info.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+
+def augment_info(info, ram_state, game_name):
+    """
+    Augment the info dictionary with object centric informations
+    """
+    if game_name == "Breakout":
+        _augment_info_breakout(info, ram_state)
+
+# Breakout
+def _augment_info_pong(info, ram_state):
+    print("FIND OFFSET")
+    info["ball"] = ram_state[99], ram_state[101]
+    info["player"] = ram_state[72], ram_state[51]
+    info["enemy"] = ram_state[]
+
+# Breakout
+def _augment_info_breakout(info, ram_state):
+    info["block_bitmap"] = _make_block_bitmap(ram_state)
+    info["ball"] = ram_state[99], ram_state[101]
+    info["player"] = ram_state[72] - 47, 189
+
+
+def _make_block_bitmap(ram_state):
+    """
+    Create an ordered block bitmap of the game BREAKOUT from the ram state.
+
+    input ram
+    output ordered block bitmap
+    """
+    array = ram_state[:36].reshape(-1, 6)
+    global previous_array_str
+    blocks_str = ""
+    for row in np.array(array).T:
+        row_str = ""
+        for j, bitnumber in enumerate(row):
+            if j == 0:
+                row_str = '{0:06b}'.format(bitnumber)[::-2] + row_str
+            elif j == 5:
+                row_str = '{0:08b}'.format(bitnumber)[1::-2] + row_str
+            else:
+                row_str = '{0:08b}'.format(bitnumber)[::-2] + row_str
+        blocks_str = row_str + "\n" + blocks_str
+    # convert str to binary array
+    blocks_int = np.array([list(el) for el in blocks_str.split("\n") if el], dtype=int)
+    correct_order = [0, 4, 3, 2, 1, 5, 6, 7, 8, 11, 12, 16, 15, 14, 13, 17, 18, 19]
+    blocks_int = blocks_int.T[correct_order].T
+    # diff(previous_array_str, str(blocks_int))
+    # previous_array_str = str(blocks_int)
+    return blocks_int
---
+++ ocatari.py
@@ -0,0 +1,25 @@
+import gym
+from termcolor import colored
+from extract_ram_info import augment_info
+
+AVAILABLE_GAMES = ["Breakout"]
+
+class OCAtari():
+    def __init__(self, env_name):
+        if env_name[:4] not in [gn[:4] for gn in AVAILABLE_GAMES]:
+            print(colored("Game not available in OCAtari", "red"))
+            print("Available games: ", AVAILABLE_GAMES)
+            exit(1)
+        self._env = gym.make(env_name)
+        self.game_name = env_name.split("-")[0].split("No")[0].split("Deterministic")[0]
+
+    def step(self, *args, **kwargs):
+        obs, reward, done, info = self._env.step(*args, **kwargs)
+        augment_info(info, self._env.env.unwrapped.ale.getRAM(), self.game_name)
+        return obs, reward, done, info
+
+    def reset(self, *args, **kwargs):
+        return self._env.reset(*args, **kwargs)
+
+    def render(self, *args, **kwargs):
+        return self._env.render(*args, **kwargs)
