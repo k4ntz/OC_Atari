68b0a66ca09b95006d6dbe27f0831e99f9d3f7d7
Tyfor <timo@his4sports.de>
2022-12-05 14:40:48 +0100

    Object Position and type

--- extract_ram_info.py
+++ extract_ram_info.py
@@ -36,9 +36,12 @@ def _augment_info_skiing(info, ram_state):
     info["player_x"] = ram_state[25]        # can go up to 150 (170 and you are back to the left side)
     info["player_y"] = ram_state[26]        # constant 120
     info["score"] = _convert_number(ram_state[107])
-    info["speed"] = ram_state[14]
+    #info["speed"] = ram_state[14] or ram[20] both seem to have very similar behavior
     info["time"] = _time_skiing(ram_state)
-    info["objects_y"] = ram_state[86:93]    # you cannot assign to specific objects. they are random
+    info["object_y"] = ram_state[86:93]    # 93 is the newest object, 86 is the oldest
+    info["object_x"] = ram_state[62:69]    # 69 is the newest object, 62 is the oldest
+    info["object_type"] = ram_state[70:77] # 77 is the newest object, 70 is the oldest   | 85 for tree | 2 for flag | 5 for mogul
+    info["object_colour"] = ram_state[78:85] # 85 is the newest object, 78 is the oldest  |probably not important
     print(ram_state)
 
 
@@ -95,6 +98,7 @@ def _time_skiing(ram_state):
     # milliseconds
     time["milli_seconds"] = _convert_number(ram_state[106])
     return time
+
 
 
 def _convert_number(number):
