edfd1666ed51533526b7eefff55fbc8edc27e40f
christopher.schubert <christopher.schubert@stud.tu-darmstadt.de>
2022-12-07 19:54:58 +0100

    seaquest enemy variation, player direction

--- extract_ram_info.py
+++ extract_ram_info.py
@@ -45,7 +45,8 @@ def _augment_info_skiing(info, ram_state):
 # Seaquest
 def _augment_info_seaquest(info, ram_state):
     """
-    The game SEAQUEST displays the enemies and divers at specific lanes they move on. Thus there y-Position is fixed.
+    The game SEAQUEST displays the enemies and divers at specific lanes, where they move from the right side to the left
+    or from the left side to the right. Thus there y-Position is fixed.
     Illustration:
 
     x=0                             x=158
@@ -62,33 +63,42 @@ def _augment_info_seaquest(info, ram_state):
     _________________________________ (Underground)
 
     """
-    info["player_x"] = ram_state[70]  # starts at x = 76, rightmost reachable position is x = 134 and
-    # leftmost reachable position is x = 21
-    info["player_y"] = ram_state[97]  # starts at y = 13 the lowest it can go is y = 108
+    info["player_x"] = ram_state[70]  # start x = 76, rightmost pos. x = 134 and leftmost reachable pos. x = 21
+    info["player_y"] = ram_state[97]  # starts at y = 13, underground at y = 108
     info["oxygen"] = ram_state[102]  # 0-64: 64 is full oxygen
-    info["enemy_x"] = {"first lane (lowest)": ram_state[30],
-                       "second lane": _saturation_addition(ram_state[31], 31),
-                       "third lane": _saturation_addition(ram_state[32], 31),
-                       "fourth lane": _saturation_addition(ram_state[33], 31),
+    info["lives"] = ram_state[59]  # renders correctly up until 6 lives
+    info["score"] = (_convert_number(ram_state[57]) * 100) + _convert_number(ram_state[58])  # the game saves these
+    # numbers in 4 bit intervals (hexadecimal) but only displays the decimal numbers
+    info["level"] = ram_state[61]  # changes enemies, speed, ... the higher the value the harder the game currently is
+    info["divers_collected"] = ram_state[62]  # renders correctly up until 6 divers collected
+    info["player_missiles_x"] = ram_state[103]
+    info["player_direction"] = ram_state[86]  # 0: player faces to the right and 8: player faces to the left
+    info["enemy_x"] = {"first lane (lowest)": ram_state[30],  # the x-position of the left most enemy in that lane
+                       "second lane": ram_state[31],  # even when that enemy is not displayed
+                       "third lane": ram_state[32],
+                       "fourth lane": ram_state[33],
                        "fifth lane (highest)": ram_state[118]  # only moves if top_enemy_enabled is 2 or higher
                        }
-    info["divers_x_or_enemy_missiles"] = {"first lane (lowest)": ram_state[71], 
+    info["divers_x_or_enemy_missiles"] = {"first lane (lowest)": ram_state[71],
                                           "second lane": ram_state[72],
                                           "third lane": ram_state[73],
                                           "fourth lane": ram_state[74]
-                                          } # probably also bigger in later levels    
-    info["divers_collected"] = ram_state[62]  # renders correctly up until 6 divers collected
-    info["lives"] = ram_state[59]  # renders correctly up until 6 lives
-    info["score"] = (_convert_number(ram_state[57]) * 100) + _convert_number(ram_state[58])  # the game saves these numbers in 4 bit intervals (hexadecimal) but only displays the decimal numbers
-    info["player_missiles_x"] = ram_state[103]
-    info["level"] = ram_state[61]  # changes enemy types, speed, number... the higher the value the harder the game currently is
+                                          }  # divers and enemy missiles share these RAM positions
     info["top_enemy_enabled"] = ram_state[60]  # enables the top ship if higher/equal than 2
-    info["lane_y_position"] =  {"first lane (lowest)": 100, 
-                                "second lane": 75,
-                                "third lane": 50,
-                                "fourth lane": 25,
-                                "water surface": 13
-                                } #the lanes actual y-positions are not saved within the RAM, therefore these are educated guesses
+    info["lane_y_position"] = {"first lane (lowest)": 100,
+                               "second lane": 75,
+                               "third lane": 50,
+                               "fourth lane": 25,
+                               "water surface": 13
+                               }  # the lanes actual y-positions are not saved within the RAM, therefore these
+    # are educated guesses
+    info["enemy_variations"] = {"first lane (lowest)": ram_state[36] % 8,
+                                "second lane": ram_state[37] % 8,
+                                "third lane": ram_state[38] % 8,
+                                "fourth lane": ram_state[39] % 8}
+    # 0: no enemy; 1: only right enemy displayed; 2: only middle enemy ; 3: right and middle enemy;
+    # 4: only left enemy; 5: right and left enemy ; 6: middle and left enemy; 7: left, middle, right enemy;
+    # 8: same as 0; 9: same as 1 -> modulo 8
     print(ram_state)
 
 
