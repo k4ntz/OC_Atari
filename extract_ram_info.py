6a8b5cc8c06cc7111f766e7ad1fe26f10bacac17
Quentin Delfosse <q.delfosse@gmail.com>
2022-12-08 19:32:05 +0100

    moved breakout

--- extract_ram_info.py
+++ extract_ram_info.py
@@ -37,13 +37,6 @@ def augment_info_revised(info, ram_state, game_name):
         exit(1)
 
 
-# Breakout
-def _augment_info_breakout_raw(info, ram_state):
-    info["block_bitmap"] = _make_block_bitmap(ram_state)
-    info["ball"] = ram_state[99], ram_state[101]
-    info["player"] = ram_state[72] - 47, 189
-    print(ram_state)
-
 
 # Skiing
 def _augment_info_skiing_raw(info, ram_state):
@@ -119,35 +112,6 @@ def _augment_info_seaquest_raw(info, ram_state):
     # 4: only left enemy; 5: right and left enemy ; 6: middle and left enemy; 7: left, middle, right enemy;
     # 8: same as 0; 9: same as 1 -> modulo 8
     print(ram_state)
-
-
-def _make_block_bitmap(ram_state):
-    """
-    Create an ordered block bitmap of the game BREAKOUT from the ram state.
-
-    input ram
-    output ordered block bitmap
-    """
-    array = ram_state[:36].reshape(-1, 6)
-    global previous_array_str
-    blocks_str = ""
-    for row in np.array(array).T:
-        row_str = ""
-        for j, bitnumber in enumerate(row):
-            if j == 0:
-                row_str = '{0:06b}'.format(bitnumber)[::-2] + row_str
-            elif j == 5:
-                row_str = '{0:08b}'.format(bitnumber)[1::-2] + row_str
-            else:
-                row_str = '{0:08b}'.format(bitnumber)[::-2] + row_str
-        blocks_str = row_str + "\n" + blocks_str
-    # convert str to binary array
-    blocks_int = np.array([list(el) for el in blocks_str.split("\n") if el], dtype=int)
-    correct_order = [0, 4, 3, 2, 1, 5, 6, 7, 8, 11, 12, 16, 15, 14, 13, 17, 18, 19]
-    blocks_int = blocks_int.T[correct_order].T
-    # diff(previous_array_str, str(blocks_int))
-    # previous_array_str = str(blocks_int)
-    return blocks_int
 
 
 def _time_skiing(ram_state):
--- ram/__init__.py
+++ ram/__init__.py
@@ -1,5 +1,8 @@
+from .breakout import _augment_info_breakout_raw, _augment_info_breakout_revised
 from .pong import _augment_info_pong_raw, _augment_info_pong_revised
 
+
 __all__ = [
-    "_augment_info_pong_raw", "_augment_info_pong_revised"
+    "_augment_info_breakout_raw", "_augment_info_breakout_revised",
+    "_augment_info_pong_raw", "_augment_info_pong_revised",
     ]
