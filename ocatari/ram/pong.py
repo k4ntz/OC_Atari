548409ac457cc74c59ac810aa4cdb80941a87747
Quentin Delfosse <q.delfosse@gmail.com>
2023-01-09 16:44:19 +0100

    Finished Pong

--- ocatari/ram/pong.py
+++ ocatari/ram/pong.py
@@ -12,6 +12,8 @@ def _augment_info_pong_revised(info, ram_state):
     (x, y, w, h, r, g, b)
     """
     objects = {}
+    enemy_score = ram_state[13]
+    player_score = ram_state[14]
     # set defauld coord if object does not exist
     if ram_state[54] != 0:  # otherwise no ball
         objects["ball"] = ram_state[49]-49, ram_state[54]-14, 2, 3, 236, 236, 236
@@ -22,5 +24,15 @@ def _augment_info_pong_revised(info, ram_state):
         objects["enemy"] = 16, ram_state[50]-15, 4, 15, 213, 130, 74
     else:
         objects["enemy"] = (0, 0, 0, 0, 0, 0, 0)
+    enemy_score2 = (0, 0, 0, 0, 0, 0, 0)
+    player_score2 = (0, 0, 0, 0, 0, 0, 0)
+    if enemy_score > 10:
+        enemy_score2 = 20, 1, 12, 19, 213, 130, 74
+    if player_score > 10:
+        player_score2 = 100, 1, 12, 19, 92, 186, 92
+    objects['player_score'] = 116, 1, 12, 19, 92, 186, 92
+    objects['enemy_score'] = 36, 1, 12, 19, 213, 130, 74
+    objects['enemy_score2'] = enemy_score2
+    objects['player_score2'] = player_score2
     objects["player"] = 140, ram_state[51]-13, 4, 15, 92, 186, 92
     info["objects"] = objects
