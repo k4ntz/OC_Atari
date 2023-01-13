e1dfcd7d6ea87f768caf392eaae3e8f90937e370
testLab <belal.alkufri@gmail.com>
2023-01-13 17:54:31 +0100

    Update space_invaders.py

--- ocatari/vision/space_invaders.py
+++ ocatari/vision/space_invaders.py
@@ -1,79 +1,66 @@
-"""
-from .utils import bb_by_color, plot_bounding_boxes
+'''
+from .utils import bb_by_color
 
 objects_colors = {"player_green": [50, 132, 50], "score_green": [50, 132, 50], "player_yellow": [162, 134, 56],
-                  "score_yellow": [162, 134, 56], "aliens": [134, 134, 29], "walls": [181, 83, 40],
+                  "score_yellow": [162, 134, 56], "alien": [134, 134, 29],
+                  "wall_1": [181, 83, 40], "wall_2": [181, 83, 40], "wall_3": [181, 83, 40],
                   "satellite_dish": [151, 25, 122], "bullets": [142, 142, 142], "number_lives": [162, 134, 56],
                   "ground": [80, 89, 22], "background": [0, 0, 0]}
 
+detected = {}
+detected['bbs'] = []
 
 # NOT FINISHED
-def _detect_objects_skiing(info, obs):
-    detected = {}
-    detected["bbs"] = []
+def _detect_objects_space_invaders(info, obs):
+    # detected = {"bbs": []}
     # detection and filtering
     for k, v in objects_colors.items():
+
         bb_by_color(detected, obs, v, k)
 
         if k == "player_green":
             detected['bbs'] = [bb for bb in detected['bbs'] if
-                               bb[0] > 140]
+                               bb[5] != "player_green" or bb[0] > 140]
 
+        # dangerous. all digits get detected but its not expected because of separation. same for yellow
         elif k == "score_green":
             detected['bbs'] = [bb for bb in detected['bbs'] if
-                               bb[0] < 140]
+                               bb[5] != "score_green" or bb[0] < 140]
 
         elif k == "player_yellow":
             detected['bbs'] = [bb for bb in detected['bbs'] if
-                               bb[0] > 140]
+                               bb[5] != "player_yellow" or bb[0] > 140]
 
-        elif k == "player_yellow":
+        elif k == "score_yellow":
             detected['bbs'] = [bb for bb in detected['bbs'] if
-                               bb[0] < 140]
+                               bb[5] != "score_yellow" or bb[0] < 140]
 
-        # aliens -_-
-
-        # special case! not finished or correct
-        elif k == "walls":
+        # special case!
+        elif k == "wall_1":
             detected['bbs'] = [bb for bb in detected['bbs'] if
-                               bb[5] == "wall_1" and 0 < bb[1] < 60]
+                               bb[5] != "wall_1" or 0 < bb[1] < 60]
+        elif k == "wall_2":
             detected['bbs'] = [bb for bb in detected['bbs'] if
-                               bb[5] == "wall_2" and 60 < bb[1] < 95]
+                               bb[5] != "wall_2" or 60 < bb[1] < 95]
+        elif k == "wall_3":
             detected['bbs'] = [bb for bb in detected['bbs'] if
-                               bb[5] == "wall_3" and 95 < bb[1] < 150]
+                               bb[5] != "wall_3" or 95 < bb[1] < 150]
+        # bullets
+        # one of the problems for bullets is that they could be in same position
 
-    # detection and filtering
-    # if y < 70 then yellow is score
-    # lives could have the very similar x and y at same time
-
-    '''bb_by_color(detected, obs, objects_colors['player_green'], "player_green")
-    detected['bbs'] = [bb for bb in detected['bbs']]
-    bb_by_color(detected, obs, objects_colors['score_green'], "score_green")
-    detected['bbs'] = [bb for bb in detected['bbs']]
-    bb_by_color(detected, obs, objects_colors['player_yellow'], "player_yellow")
-    detected['bbs'] = [bb for bb in detected['bbs']]
-    bb_by_color(detected, obs, objects_colors['score_yellow'], "score_yellow")
-    detected['bbs'] = [bb for bb in detected['bbs']]
-    bb_by_color(detected, obs, objects_colors['aliens'], "aliens")
-    detected['bbs'] = [bb for bb in detected['bbs']]
-    bb_by_color(detected, obs, objects_colors['walls'], "wall_1")
-    detected['bbs'] = [bb for bb in detected['bbs']]
-    bb_by_color(detected, obs, objects_colors['walls'], "wall_2")
-    detected['bbs'] = [bb for bb in detected['bbs']]
-    bb_by_color(detected, obs, objects_colors['walls'], "wall_3")
-    detected['bbs'] = [bb for bb in detected['bbs']]
-
-    # bb_by_color(detected, obs, objects_colors['bullets'], "bullets")
-    # detected['bbs'] = [bb for bb in detected['bbs']] # bullets have same color!
-    bb_by_color(detected, obs, objects_colors['number_lives'], "number_lives")
-    detected['bbs'] = [bb for bb in detected['bbs']]'''
-
-    if False:
-        plot_bounding_boxes(obs, detected["bbs"], objects_colors)
     objects = {}
+    alien_x = 0
     for obj in detected["bbs"]:
+
         y, x, h, w, type, name = obj
         r, g, b = objects_colors[name]
-        objects[name] = (x, y, w, h, r, g, b)
+        if name == "alien":
+            alien_str = "alien_" + str(alien_x)
+            objects[alien_str] = (x, y, w, h, r, g, b)
+            alien_x += 1
+
+        else:
+            objects[name] = (x, y, w, h, r, g, b)
     info["objects"] = objects
-    """
+    '''
+
