0d692c43829611b231eb11cf418d9479851d2b19
BJenrajb <bj.nutzmail@gmail.com>
2023-01-16 16:28:58 +0100

    vision update to work with demo file

--- ocatari/vision/mspacman.py
+++ ocatari/vision/mspacman.py
@@ -1,114 +1,26 @@
 from .utils import find_objects
 
 
-objects_colors = {"player": [210, 164, 74], "ghost_orange": [180, 122, 48], "ghost_cyan": [84, 184, 153],
-                  "ghost_pink": [198, 89, 179], "ghost_red": [200, 72, 72], "life_1": [187, 187, 53],
-                  "life_2": [187, 187, 53], "life_3": [187, 187, 53],
-                  "cherry/strawberry/Apple": [184, 50, 50], "orange/banana": [198, 108, 58],
-                  "pretzel": [162, 162, 42], "pear": [110, 156, 66], "cherry/strawberry/Applein_in_play": [184, 50, 50],
-                  "orange/bananain_in_play": [198, 108, 58], "pretzel_in_play": [162, 162, 42],
-                  "pear_in_play": [110, 156, 66], "eatable_ghosts_1": [66, 114, 194],
-                  "eatable_ghosts_2": [66, 114, 194],
-                  "eatable_ghosts_3": [66, 114, 194], "eatable_ghosts_4": [66, 114, 194],
-                  "player_score": [195, 144, 61]}
+objects_colors = {"player": [210, 164, 74], "lives": [187, 187, 53], "player_score": [195, 144, 61],
+                  "ghost_orange": [180, 122, 48], "ghost_cyan": [84, 184, 153],
+                  "ghost_pink": [198, 89, 179], "ghost_red": [200, 72, 72], 
+                  "cherry/strawberry/Apple": [184, 50, 50], "pretzel": [162, 162, 42],
+                  "orange/banana": [198, 108, 58], "pear": [110, 156, 66],
+                  "eatable_ghosts": [66, 114, 194], "player_score": [195, 144, 61]}
 
+# fixed_objects_pos = { }
 
 def _detect_objects_mspacman(info, obs):
-    """
-        elif k == "eatable_ghosts_1":
-            detected['bbs'] = [bb for bb in detected['bbs'] if
-                               bb[5] != k]
-        elif k == "eatable_ghosts_2":
-            detected['bbs'] = [bb for bb in detected['bbs'] if
-                               bb[5] != k]
-        elif k == "eatable_ghosts_3":
-            detected['bbs'] = [bb for bb in detected['bbs'] if
-                               bb[5] != k]
-        elif k == "eatable_ghosts_4":
-            detected['bbs'] = [bb for bb in detected['bbs'] if
-                               bb[5] != k]
-    """
     objects = {}
 
-    player = [bb for bb in find_objects(obs, objects_colors["player"], min_distance=None)]
+    for object in objects_colors:
+        found_objects = find_objects(obs, objects_colors[object])
+        objects[object] = found_objects
 
-    if player:
-        objects["player"] = player[0]
-
-    ghost_orange = [bb for bb in find_objects(obs, objects_colors["ghost_orange"], min_distance=None)]
-
-    if ghost_orange:
-        objects["ghost_orange"] = ghost_orange[0]
-
-    ghost_cyan = [bb for bb in find_objects(obs, objects_colors["ghost_cyan"], min_distance=None)]
-
-    if ghost_cyan:
-        objects["ghost_cyan"] = ghost_cyan[0]
-
-    ghost_pink = [bb for bb in find_objects(obs, objects_colors["ghost_pink"], min_distance=None)]
-
-    if ghost_pink:
-        objects["ghost_pink"] = ghost_pink[0]
-
-    ghost_red = [bb for bb in find_objects(obs, objects_colors["ghost_red"], min_distance=None)]
-
-    if ghost_red:
-        objects["ghost_red"] = ghost_red[0]
-
-    life_1 = [bb for bb in find_objects(obs, objects_colors["life_1"], min_distance=None)
-              if bb[1] < 25]
-    if life_1:
-        objects["life_1"] = life_1[0]
-
-    life_2 = [bb for bb in find_objects(obs, objects_colors["life_2"], min_distance=None)
-              if 25 < bb[1] < 35]
-    if life_2:
-        objects["life_2"] = life_2[0]
-
-    life_3 = [bb for bb in find_objects(obs, objects_colors["life_3"], min_distance=None)
-              if 35 < bb[1]]
-    if life_3:
-        objects["life_3"] = life_3[0]
-
-    red_fruit = [bb for bb in find_objects(obs, objects_colors["cherry/strawberry/Apple"], min_distance=None)
-                 if 170 < bb[0]]
-    if red_fruit:
-        objects["cherry/strawberry/Apple"] = red_fruit[0]
-
-    orange_fruit = [bb for bb in find_objects(obs, objects_colors["orange/banana"], min_distance=None)
-                    if 170 < bb[0]]
-    if orange_fruit:
-        objects["orange/banana"] = orange_fruit[0]
-
-    pretzel = [bb for bb in find_objects(obs, objects_colors["pretzel"], min_distance=None)
-               if 170 < bb[0]]
-    if pretzel:
-        objects["pretzel"] = pretzel[0]
-
-    pear = [bb for bb in find_objects(obs, objects_colors["pear"], min_distance=None)
-            if 170 < bb[0]]
-    if pear:
-        objects["pear"] = pear[0]
-
-    red_fruit_in_play = [bb for bb in find_objects(obs, objects_colors["cherry/strawberry/Applein_in_play"],
-                                                   min_distance=None)
-                         if 170 > bb[0]]
-    if red_fruit_in_play:
-        objects["cherry/strawberry/Applein_in_play"] = red_fruit_in_play[0]
-
-    orange_fruit_in_play = [bb for bb in find_objects(obs, objects_colors["orange/bananain_in_play"], min_distance=None)
-                            if 170 > bb[0]]
-    if orange_fruit_in_play:
-        objects["orange/bananain_in_play"] = orange_fruit_in_play[0]
-
-    pretzel_in_play = [bb for bb in find_objects(obs, objects_colors["pretzel_in_play"], min_distance=None)
-                       if 170 > bb[0]]
-    if pretzel_in_play:
-        objects["pretzel_in_play"] = pretzel_in_play[0]
-
-    pear_in_play = [bb for bb in find_objects(obs, objects_colors["pear_in_play"], min_distance=None)
-                    if 170 > bb[0]]
-    if pear_in_play:
-        objects["pear_in_play"] = pear_in_play[0]
+    # fixed_objects_complete = {}
+    # for objn in fixed_objects_pos.keys():
+    #     fixed_objects_complete[objn] = [fixed_objects_pos[objn]]
+    # objects.update(fixed_objects_complete)
 
     info["objects"] = objects
+    info["objects_colors"] = objects_colors
