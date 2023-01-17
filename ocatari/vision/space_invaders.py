ff2b50763caf1553b82e0fc1c2dd31c2bb8956c9
testLab <belal.alkufri@gmail.com>
2023-01-18 00:22:33 +0100

    Update space_invaders.py

--- ocatari/vision/space_invaders.py
+++ ocatari/vision/space_invaders.py
@@ -1,5 +1,4 @@
-'''
-from .utils import bb_by_color
+from .utils import find_objects # noqa
 
 objects_colors = {"player_green": [50, 132, 50], "score_green": [50, 132, 50], "player_yellow": [162, 134, 56],
                   "score_yellow": [162, 134, 56], "alien": [134, 134, 29],
@@ -7,60 +6,11 @@ objects_colors = {"player_green": [50, 132, 50], "score_green": [50, 132, 50], "
                   "satellite_dish": [151, 25, 122], "bullets": [142, 142, 142], "number_lives": [162, 134, 56],
                   "ground": [80, 89, 22], "background": [0, 0, 0]}
 
-detected = {}
-detected['bbs'] = []
 
-# NOT FINISHED
 def _detect_objects_space_invaders(info, obs):
-    # detected = {"bbs": []}
-    # detection and filtering
-    for k, v in objects_colors.items():
-
-        bb_by_color(detected, obs, v, k)
-
-        if k == "player_green":
-            detected['bbs'] = [bb for bb in detected['bbs'] if
-                               bb[5] != "player_green" or bb[0] > 140]
-
-        # dangerous. all digits get detected but its not expected because of separation. same for yellow
-        elif k == "score_green":
-            detected['bbs'] = [bb for bb in detected['bbs'] if
-                               bb[5] != "score_green" or bb[0] < 140]
-
-        elif k == "player_yellow":
-            detected['bbs'] = [bb for bb in detected['bbs'] if
-                               bb[5] != "player_yellow" or bb[0] > 140]
-
-        elif k == "score_yellow":
-            detected['bbs'] = [bb for bb in detected['bbs'] if
-                               bb[5] != "score_yellow" or bb[0] < 140]
-
-        # special case!
-        elif k == "wall_1":
-            detected['bbs'] = [bb for bb in detected['bbs'] if
-                               bb[5] != "wall_1" or 0 < bb[1] < 60]
-        elif k == "wall_2":
-            detected['bbs'] = [bb for bb in detected['bbs'] if
-                               bb[5] != "wall_2" or 60 < bb[1] < 95]
-        elif k == "wall_3":
-            detected['bbs'] = [bb for bb in detected['bbs'] if
-                               bb[5] != "wall_3" or 95 < bb[1] < 150]
-        # bullets
-        # one of the problems for bullets is that they could be in same position
-
     objects = {}
-    alien_x = 0
-    for obj in detected["bbs"]:
-
-        y, x, h, w, type, name = obj
-        r, g, b = objects_colors[name]
-        if name == "alien":
-            alien_str = "alien_" + str(alien_x)
-            objects[alien_str] = (x, y, w, h, r, g, b)
-            alien_x += 1
-
-        else:
-            objects[name] = (x, y, w, h, r, g, b)
+    for object in objects_colors:
+        found_objects = find_objects(obs, objects_colors[object])
+        objects[object] = found_objects
     info["objects"] = objects
-    '''
-
+    info["objects_colors"] = objects_colors
