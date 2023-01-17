bd4191bc6fe4c1ffab64bfb2a3df64a469ed13a4
BJenrajb <bj.nutzmail@gmail.com>
2023-01-17 19:26:45 +0100

    created demo file and adjusted vision

--- ocatari/vision/kangaroo.py
+++ ocatari/vision/kangaroo.py
@@ -1,9 +1,19 @@
 from .utils import find_objects
 
-objects_colors = {"Kangaroo": [223, 183, 85], "Bell": [210, 164, 74], "Fruit": [214, 92, 92], "HUD": [160, 171, 79] }
+objects_colors = {"kangaroo": [223, 183, 85], "bell": [210, 164, 74],
+                  "fruit": [214, 92, 92], "hud": [160, 171, 79]
+                  }
 
 
 def _detect_objects_kangaroo(info, obs):
     objects = {}
-    
+    for object in objects_colors:
+        found_objects = find_objects(obs, objects_colors[object])
+        objects[object] = found_objects
+    """if fixed_objects:
+        fixed_objects_complete = {}
+        for objn in fixed_objects_pos.keys():
+            fixed_objects_complete[objn] = [fixed_objects_pos[objn]]
+        objects.update(fixed_objects_complete)"""
     info["objects"] = objects
+    info["objects_colors"] = objects_colors
--- scripts/demo/demo_kangaroo.py
+++ scripts/demo/demo_kangaroo.py
@@ -1,17 +1,14 @@
-# appends parent path to syspath to make ocatari importable
-# like it would have been installed as a package
 import sys
-sys.path.insert(0, '../../ocatari') # noqa
-from core import OCAtari
 import random
 import matplotlib.pyplot as plt
+sys.path.insert(0, '../../ocatari') # noqa
+from core import OCAtari
 from vision.utils import mark_bb, make_darker
-from vision.skiing import objects_colors
+from vision.kangaroo import objects_colors
 from utils import load_agent, parser
 
-game_name = "Skiing"
+game_name = "Kangaroo"
 MODE = "vision"
-# MODE = "raw"
 # MODE = "revised"
 env = OCAtari(game_name, mode=MODE, render_mode='rgb_array')
 observation, info = env.reset()
@@ -27,19 +24,17 @@ for i in range(1000):
     if opts.path is not None:
         action = agent.draw_action(env.dqn_obs)
     else:
-        action = random.randint(0, 2)
+        action = random.randint(0, 4)
     obs, reward, terminated, truncated, info = env.step(action)
     if info.get('frame_number') > 0 and i % 100 == 0:
-        for obj_name, o_list in info["objects"].items():
-            print(obj_name)
-            for oinfo in o_list:
-                opos = oinfo[:4]
-                ocol = oinfo[4:]
-                if MODE == "vision":
-                    ocol = objects_colors[obj_name]
-                sur_col = make_darker(ocol)
-                mark_bb(obs, opos, color=sur_col)
-                # mark_point(obs, *opos[:2], color=(255, 255, 0))
+        for obj_name, oinfo in info["objects"].items():
+            opos = oinfo[:4]
+            ocol = oinfo[4:]
+            if MODE == "vision":
+                ocol = objects_colors[obj_name]
+            sur_col = make_darker(ocol)
+            mark_bb(obs, opos, color=sur_col)
+            # mark_point(obs, *opos[:2], color=(255, 255, 0))
         plt.imshow(obs)
         plt.show()
 
