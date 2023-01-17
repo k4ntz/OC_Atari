5757c3db926d750fc65e02abdf1db7e62ac224a3
Quentin Delfosse <q.delfosse@gmail.com>
2023-01-17 20:46:23 +0100

    finished test scripts

--- ocatari/vision/extract_vision_info.py
+++ ocatari/vision/extract_vision_info.py
@@ -4,17 +4,17 @@ from .skiing import _detect_objects_skiing
 from .tennis import _detect_objects_tennis
 
 
-def detect_objects_vision(info, obs, game_name):
-    if game_name.lower() == "freeway":
-        return _detect_objects_freeway(info, obs)
-    elif game_name.lower() == "pong":
-        return _detect_objects_pong(info, obs)
-    elif game_name.lower() == "seaquest":
-        return _detect_objects_seaquest(info, obs)
+def detect_objects_vision(objects, obs, game_name, hud=False):
+    # if game_name.lower() == "freeway":
+    #     return _detect_objects_freeway(objects, obs, hud)
+    if game_name.lower() == "pong":
+        return _detect_objects_pong(objects, obs, hud)
+    # elif game_name.lower() == "seaquest":
+    #     return _detect_objects_seaquest(objects, obs, hud)
     elif game_name.lower() == "skiing":
-        return _detect_objects_skiing(info, obs)
+        return _detect_objects_skiing(objects, obs, hud)
     elif game_name.lower() == "tennis":
-        return _detect_objects_tennis(info, obs)
+        return _detect_objects_tennis(objects, obs, hud)
     else:
         print(colored("Uncovered game in vision mode", "red"))
         exit(1)
