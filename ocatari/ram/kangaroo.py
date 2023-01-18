91e7a7bd05954ada74071cc329189a5bb744bcf9
BJenrajb <bj.nutzmail@gmail.com>
2023-01-18 17:40:59 +0100

    flake8

--- ocatari/ram/kangaroo.py
+++ ocatari/ram/kangaroo.py
@@ -6,17 +6,21 @@ RAM extraction for the game KANGUROO. Supported modes: raw, revised.
 """
 
 
+
+
 def _augment_info_kangaroo_raw(info, ram_state):
     pass
 
 
 def _augment_info_kangaroo_revised(info, ram_state):
+
+
     """
     For all 3 objects:
     (x, y, w, h, r, g, b)
      """
     objects = {}
-    
+
     info["time"] = _convert_number(ram_state[59]) * 100
     info["score"] = _convert_number(ram_state[39]) * 10000 + _convert_number(ram_state[40]) * 100
     info["lives"] = ram_state[45]
@@ -44,19 +48,19 @@ def _augment_info_kangaroo_revised(info, ram_state):
     info["monkey_sprite"] = ram_state[3]  # or ram_state[7]
     info["player_sprite"] = ram_state[54]
     info["player_movement"] = ram_state[72]
-    info["ticker"] = ram_state[57] # Game starts when this state reaches 158
+    info["ticker"] = ram_state[57]  # Game starts when this state reaches 158
 
     info["objects"] = objects
 
 def _get_fruit_type_kangaroo(ram_state):
     if ram_state < 128:
-        if ram_state%4 == 0:
+        if ram_state % 4 == 0:
             return "Strawberry"
-        elif ram_state%4 == 1:
+        elif ram_state % 4 == 1:
             return "Apple"
-        elif ram_state%4 == 2:
+        elif ram_state % 4 == 2:
             return "Cherry"
-        elif ram_state%4 == 3:
+        elif ram_state % 4 == 3:
             return "Pineapple"
     else:
         return None
