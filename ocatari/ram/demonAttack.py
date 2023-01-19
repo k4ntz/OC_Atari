99fb7def2290ae3cb026fa0d6f40897c58b4f18e
BJenrajb <bj.nutzmail@gmail.com>
2023-01-19 18:32:41 +0100

    Changed Comment to not be a documentation anymore

--- ocatari/ram/demonAttack.py
+++ ocatari/ram/demonAttack.py
@@ -166,9 +166,9 @@ def _detect_objects_demon_attack_raw(info, ram_state):
     info["enemy_y"] = ram_state[69:71]  # 69 is topmost enemy 71 is lowest
     info["enemy_x"] = ram_state[13:15]  # 13 is topmost enemy 15 is lowest
     info["enemy_projectile_y"] = ram_state[37:46]
-    """ kind of like a bit map. If a value is 0 then there is no projectile
-    at that position the higher the value the thicker / more projectiles
-    at that position 46(ram) is highest possible enemy_position_y(lowest enemy)
-    37(ram) is player position_y"""
+    # kind of like a bit map. If a value is 0 then there is no projectile
+    # at that position the higher the value the thicker / more projectiles
+    # at that position 46(ram) is highest possible enemy_position_y(lowest enemy)
+    # 37(ram) is player position_y
     info["player_projectile_y"] = ram_state[21]
     info["player_projectile_x"] = ram_state[22]
