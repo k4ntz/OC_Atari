5cf2dcd516266a57925bc3c9b289fcc54f523ed4
Tyfor <timo@his4sports.de>
2023-01-21 14:09:21 +0100

    fixed freeway

--- ocatari/ram/extract_ram_info.py
+++ ocatari/ram/extract_ram_info.py
@@ -2,7 +2,7 @@ from termcolor import colored
 from .boxing import _detect_objects_boxing_raw, _detect_objects_boxing_revised, _init_objects_boxing_ram
 from .bowling import _detect_objects_bowling_raw, _detect_objects_bowling_revised
 from .breakout import _detect_objects_breakout_raw, _detect_objects_breakout_revised
-from .freeway import _detect_objects_freeway_raw, _detect_objects_freeway_revised
+from .freeway import _detect_objects_freeway_raw, _detect_objects_freeway_revised, _init_objects_freeway_ram
 from .pong import _detect_objects_pong_raw, _detect_objects_pong_revised, _init_objects_pong_ram
 from .seaquest import _detect_objects_seaquest_raw, _detect_objects_seaquest_revised
 from .skiing import _detect_objects_skiing_raw, _detect_objects_skiing_revised, _init_objects_skiing_ram
@@ -22,8 +22,8 @@ def init_objects(game_name, hud):
         return _init_objects_boxing_ram(hud)
     # elif game_name.lower() == "breakout":
     #     _detect_objects_breakout(objects, ram_state)
-    # elif game_name.lower() == "freeway":
-    #     _detect_objects_freeway(objects, ram_state)
+    elif game_name.lower() == "freeway":
+        return _init_objects_freeway_ram(hud)
     elif game_name.lower() == "pong":
         return _init_objects_pong_ram(hud)
     elif game_name.lower() == "skiing":
@@ -70,30 +70,30 @@ def detect_objects_raw(info, ram_state, game_name):
         exit(1)
 
 
-def detect_objects_revised(info, ram_state, game_name, hud):
+def detect_objects_revised(objects, ram_state, game_name, hud):
     """
     Augment the info dictionary with object centric information
     """
     if game_name.lower() == "boxing":
-        _detect_objects_boxing_revised(info, ram_state, hud)
+        _detect_objects_boxing_revised(objects, ram_state, hud)
     elif game_name.lower() == "breakout":
-        _detect_objects_breakout_revised(info, ram_state, hud)
+        _detect_objects_breakout_revised(objects, ram_state, hud)
     elif game_name.lower() == "freeway":
-        _detect_objects_freeway_revised(info, ram_state, hud)
+        _detect_objects_freeway_revised(objects, ram_state, hud)
     elif game_name.lower() == "pong":
-        _detect_objects_pong_revised(info, ram_state, hud)
+        _detect_objects_pong_revised(objects, ram_state, hud)
     elif game_name.lower() == "skiing":
-        _detect_objects_skiing_revised(info, ram_state, hud)
+        _detect_objects_skiing_revised(objects, ram_state, hud)
     elif game_name.lower() == "seaquest":
-        _detect_objects_seaquest_revised(info, ram_state, hud)
+        _detect_objects_seaquest_revised(objects, ram_state, hud)
     elif game_name.lower() == "spaceinvaders":
-        _detect_objects_space_invaders_revised(info, ram_state, hud)
+        _detect_objects_space_invaders_revised(objects, ram_state, hud)
     elif game_name.lower() == "tennis":
-        _detect_objects_tennis_revised(info, ram_state, hud)
+        _detect_objects_tennis_revised(objects, ram_state, hud)
     elif game_name.lower() == "bowling":
-        _detect_objects_bowling_revised(info, ram_state, hud)
+        _detect_objects_bowling_revised(objects, ram_state, hud)
     elif game_name.lower() == "demonattack":
-        _detect_objects_demon_attack_revised(info, ram_state, hud)
+        _detect_objects_demon_attack_revised(objects, ram_state, hud)
     else:
         print(colored("Uncovered game in revised mode", "red"))
         exit(1)
