47c9d7111f6bea329fa764789cf35a5a6d438d6a
testLab <belal.alkufri@gmail.com>
2023-01-23 21:30:08 +0100

    Update asterix.py

--- ocatari/vision/asterix.py
+++ ocatari/vision/asterix.py
@@ -1,13 +1,19 @@
 from .utils import find_objects
 from .game_objects import GameObject
 
-
-objects_colors = {}
+objects_colors = {"score": [187, 187, 53],
+                  "Cauldron": [184, 50, 50],
+                  "player": [187, 187, 53],
+                  "background": [0, 0, 0],
+                  "enemy": [227, 110, 110],
+                  "lives_1": [187, 187, 53],
+                  "lives_2": [187, 187, 53],
+                  }
 
 
 class Player(GameObject):
     pass
 
 
-def _detect_objects_demon_attack(objects, obs, hud=False):
+def _detect_objects_asterix(objects, obs, hud=False):
     pass
