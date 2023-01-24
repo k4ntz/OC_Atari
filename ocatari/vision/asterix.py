c4933a590e525dca19a08567177e2404b478f446
testLab <belal.alkufri@gmail.com>
2023-01-24 15:24:16 +0100

    Update asterix.py

--- ocatari/vision/asterix.py
+++ ocatari/vision/asterix.py
@@ -1,13 +1,13 @@
 from .utils import find_objects
 from .game_objects import GameObject
 
-objects_colors = {"score": [187, 187, 53],
-                  "Cauldron": [184, 50, 50],
-                  "player": [187, 187, 53],
-                  "background": [0, 0, 0],
-                  "enemy": [227, 110, 110],
-                  "lives_1": [187, 187, 53],
-                  "lives_2": [187, 187, 53],
+objects_colors = {'score': [187, 187, 53],
+                  'Cauldron': [184, 50, 50],
+                  'player': [187, 187, 53],
+                  'background': [0, 0, 0],
+                  'enemy': [227, 110, 110],
+                  'lives_1': [187, 187, 53],
+                  'lives_2': [187, 187, 53],
                   }
 
 
