e6e5968d47a6f5ea68f928f839c60c4e8a82a5e1
Quentin Delfosse <q.delfosse@gmail.com>
2023-01-25 11:31:24 +0100

    using value error to find the bug

--- scripts/demo/demo_pong_dict.py
+++ scripts/demo/demo_pong_dict.py
@@ -12,7 +12,7 @@ from ocatari.utils import load_agent, parser
 
 game_name = "Pong"
 # game_name = "SpaceInvaders"
-# game_name = "Tennis"
+game_name = "Skiing"
 MODE = "vision"
 MODE = "revised"
 env = OCAtari(game_name, mode=MODE, render_mode='rgb_array')
@@ -24,13 +24,15 @@ if opts.path:
     agent = load_agent(opts, env.action_space.n)
 
 fig, axes = plt.subplots(1, 2)
-for i in range(1000):
+for i in range(10000):
     if opts.path is not None:
         action = agent.draw_action(env.dqn_obs)
     else:
-        action = random.randint(0, 5)
-    obs, reward, terminated, truncated, info = env.step(action)
-    if i % 25 == 0:
+        action = random.randint(0, 2)
+    try:
+        obs, reward, terminated, truncated, info = env.step(action)
+    except ValueError:
+        import ipdb; ipdb.set_trace()
         # obse2 = deepcopy(obse)
         for ax, obs, objects_list, title in zip(axes, [obs],
                                                 [env.objects],
@@ -41,11 +43,11 @@ for i in range(1000):
                 sur_col = make_darker(ocol)
                 mark_bb(obs, opos, color=sur_col)
                 # mark_point(obs, *opos[:2], color=(255, 255, 0))
-            plt.imshow(obs)
-            plt.show()
         for ax in axes.flatten():
             ax.set_xticks([])
             ax.set_yticks([])
+        plt.imshow(obs)
+        plt.show()
 
     if terminated or truncated:
         observation, info = env.reset()
