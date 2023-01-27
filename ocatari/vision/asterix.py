42b505cb8261bdc19b065c1a40323766e61f4801
testLab <belal.alkufri@gmail.com>
2023-01-27 19:44:02 +0100

    Update asterix.py

--- ocatari/vision/asterix.py
+++ ocatari/vision/asterix.py
@@ -8,13 +8,13 @@ objects_colors = {'player': [187, 187, 53],
                   'score': [187, 187, 53],
                   'lives': [187, 187, 53],
 
-                    # new not edited objects:
+                  # new not edited objects:
                   'bounty': [198, 89, 179],  # bounty or prize or bonus
                   # make min_distance for bounty bigger cause of more than one color in this and you are
                   # taking the borders, so you have all the object in the rectangle
 
 
-                  }  # they still not all objects covered
+                  }  # it still not all objects covered
 
 
 class Player(GameObject):
@@ -32,7 +32,7 @@ class Cauldron(GameObject):
 class Enemy(GameObject):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.rgb = 227, 110, 110
+        self.rgb = 228, 111, 111
 
 
 class Score(GameObject):
