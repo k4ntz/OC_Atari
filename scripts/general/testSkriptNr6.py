c89ca4e3d3222d6200c9f996c943d0843e91e669
testLab <belal.alkufri@gmail.com>
2023-01-29 21:47:16 +0100

    Update testSkriptNr6.py

--- scripts/general/testSkriptNr6.py
+++ scripts/general/testSkriptNr6.py
@@ -3,10 +3,9 @@ from matplotlib import pyplot as plt
 from ocatari.core import OCAtari
 import time
 import random
-import ipdb
 
+# this test makes it easy to test several indices
 
-# env = OCAtari("Asterix-v4")
 env = OCAtari("Asterix-v4", mode="vision", render_mode='rgb_array')  # Skiing-v4, DemonAttack-v4, SpaceInvaders-v4
 observation, info = env.reset()
 prevRam = None
@@ -18,28 +17,38 @@ prevRam = None
 
 constant = []
 not_important = constant + []
-already_figured_out = not_important + list(range(42, 50))  # 50 is excluded
-                                            # x_lanes
+already_figured_out = not_important + list(range(41, 50))  # 50 is excluded
+                                            # x_objects
 value = 0
-ctr = 0
+index = 84
 
 for ROUND in range(1000000):
 
-    print('round =', ROUND, 'ctr =', ctr)
-    obs, reward, terminated, truncated, info = env.step(random.randint(-2, -1))
+    if ROUND > 0:
+        print('round =', ROUND, 'index =', index, 'ram[' + str(index) + '] =', ram[index])
+    else:
+        print('round =', ROUND, 'index =', index)
+    obs, reward, terminated, truncated, info = env.step(random.randint(-2, 2))
 
     if prevRam is not None:
-        value = (prevRam[ctr] + 31) % 256
+        # i = 7  # to test an individual index in all rounds
+        i = index
 
-        if ROUND % 8 == 0:
-            ctr = ctr + 1
+        value = (prevRam[i] + 31) % 256  # 31 = 00011111  (= more changes)
+        # value = -ram[i]
+
+        if ROUND % 1 == 0:
+            if index in [38, 86, 90]:  # this array is for asterix
+                index = index + 2
+            else:
+                index = index + 1
         if ROUND > 1:
-            env._env.unwrapped.ale.setRAM(ctr, value)
+            env._env.unwrapped.ale.setRAM(i, value)
 
         print(ram)
 
         for i in range(len(ram)):
-            if ram[i] != prevRam[i] and i not in already_figured_out or i == 26 or i == 16:
+            if ram[i] != prevRam[i] and i not in already_figured_out:
                 integ = ram[i] - prevRam[i]
 
                 if integ > 127:
@@ -54,19 +63,9 @@ for ROUND in range(1000000):
                           str(ram[i]), '\t',
                           format(ram[i], '08b')
                           )
-        if info.get('frame_number') > 150:
-            if ROUND > 80:
-
-                # if k % 3 == 0:  # round was k before
-                #     wert = 2^(wert + 1)
-
-                print("by index", value)
-                '''for l in range(8):
-                    wert = (prevRam[index] + 4) % 256
-                    env._env.unwrapped.ale.setRAM(index, wert) '''
 
         print("------------------------------------------")
-        # prevRam = ram
+
         if terminated or truncated:
             observation, info = env.reset()
         print(info)
@@ -76,7 +75,6 @@ for ROUND in range(1000000):
             rgb_array = env.render()
             plt.imshow(rgb_array)  # rgb_array stuff for fun
             plt.show()
-            print(rgb_array)
         time.sleep(0)
     if ROUND > 0:
         prevRam = ram
