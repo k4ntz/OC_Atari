f3778342a7bf67121285c315af4e80874dc93ef7
testLab <belal.alkufri@gmail.com>
2023-02-03 03:04:29 +0100

    Update asterix.py

--- ocatari/vision/asterix.py
+++ ocatari/vision/asterix.py
@@ -128,28 +128,29 @@ def _detect_objects_asterix(objects, obs, hud=False):
     for instance in cauldron:
         objects.append(Cauldron(*instance))
 
-    enemy = find_objects(obs, objects_colors["enemy"], closing_dist=6)
+    enemy = find_objects(obs, objects_colors["enemy"], closing_dist=2)
     for instance in enemy:
         objects.append(Enemy(*instance))
 
     ctr = 1
     for x in ["reward1", "reward2", "reward3", "reward4", "reward5", "reward6"]:
-        reward = find_mc_objects(obs, objects_colors[x], min_distance=3, closing_dist=2, miny=24, maxy=151)
+        reward = find_mc_objects(obs, objects_colors[x], min_distance=3, closing_dist=2, miny=24, maxy=151, )
+        # size=(6, 11), tol_s=(4, 2))
         if reward is not None:
             for num in range(1, 7):
-                reward = find_mc_objects(obs, objects_colors[x], min_distance=3, closing_dist=2, miny=24, maxy=151)
+                reward = find_mc_objects(obs, objects_colors[x], min_distance=3, closing_dist=2, miny=24, maxy=151, )
+                # size=(6, 11), tol_s=(4, 2))
             for instance in reward:
                 objects.append(Reward(ctr, *instance))
             break
         ctr += 1
 
-
-    helmet = find_mc_objects(obs, objects_colors["helmet"], closing_dist=6)
+    helmet = find_mc_objects(obs, objects_colors["helmet"], closing_dist=3)
     for instance in helmet:
         objects.append(Helmet(*instance))
 
-    shield = find_objects(obs, objects_colors["shield"], closing_dist=1, size=(5, 11), min_distance=2)
-    for instance in shield:
+    shield = find_objects(obs, objects_colors["shield"], closing_dist=1, size=(5, 11), tol_s=1, min_distance=2)
+    for instance in shield:  # specify size(make it work)
         objects.append(Shield(*instance))
 
     lamp = find_mc_objects(obs, objects_colors["lamp"], closing_dist=3)
@@ -160,15 +161,15 @@ def _detect_objects_asterix(objects, obs, hud=False):
     for instance in apple:
         objects.append(Apple(*instance))
 
-    fish = find_objects(obs, objects_colors["fish"], size=(8, 5), tol_s=2, closing_dist=1, min_distance=1)
+    fish = find_objects(obs, objects_colors["fish"],closing_dist=1, min_distance=1)  # size=(8, 5), tol_s=2,
     for instance in fish:
         objects.append(Fish(*instance))
 
-    meat = find_mc_objects(obs, objects_colors["meat"], closing_dist=1, min_distance=2)
+    meat = find_mc_objects(obs, objects_colors["meat"], closing_dist=1, min_distance=1)
     for instance in meat:
         objects.append(Meat(*instance))
 
-    mug = find_mc_objects(obs, objects_colors["mug"], closing_dist=1, min_distance=2)
+    mug = find_mc_objects(obs, objects_colors["mug"], closing_dist=2, min_distance=2)
     for instance in mug:
         objects.append(Mug(*instance))
 
@@ -181,6 +182,6 @@ def _detect_objects_asterix(objects, obs, hud=False):
         for instance in score:
             objects.append(Score(*instance))
 
-    print("\nobjects:")
-    print(*objects, sep="\n")
-    print("\n")
+    # print("\nobjects:")
+    # print(*objects, sep="\n")
+    # print("\n")
