d6bf9f522c6c6420d45a6e8eea282fa0cec81b7d
testLab <belal.alkufri@gmail.com>
2023-02-03 16:59:43 +0100

    sizes classes color names
    sizes completely specified
    all classes created
    renamed colors

--- ocatari/vision/asterix.py
+++ ocatari/vision/asterix.py
@@ -7,17 +7,16 @@ objects_colors = {'player': [187, 187, 53],
                   'lives': [187, 187, 53],
 
                   # next color is inner one
-                  'reward1': [[198, 89, 179], [127, 92, 213], [170, 170, 170]],  # value 50  cauldron
                   'reward_50': [[198, 89, 179], [127, 92, 213], [170, 170, 170]],
-                  'reward2': [[135, 183, 84], [213, 130, 74], [170, 170, 170]],  # value 100 helmet. same as reward5 :(
-                  'reward3': [[195, 144, 61], [84, 138, 210], [170, 170, 170]],  # value 200 shield
-                  'reward4': [[213, 130, 74], [84, 92, 214], [170, 170, 170]],  # value 300 lamp
-                  'reward5': [[135, 183, 84], [214, 92, 92], [170, 170, 170]],  # value 400 apple
-                  'reward6': [[163, 57, 21], [164, 89, 208], [170, 170, 170]],  # value 500 fish, meat and mug
+                  'reward_100': [[135, 183, 84], [213, 130, 74], [170, 170, 170]],  # helmet
+                  'reward_200': [[195, 144, 61], [84, 138, 210], [170, 170, 170]],  # shield
+                  'reward_300': [[213, 130, 74], [84, 92, 214], [170, 170, 170]],  # lamp
+                  'reward_400': [[135, 183, 84], [214, 92, 92], [170, 170, 170]],  # apple
+                  'reward_500': [[163, 57, 21], [164, 89, 208], [170, 170, 170]],  # fish, meat and mug
 
                   # next objects are which meant with reward
                   'cauldron': [[167, 26, 26], [184, 50, 50]],
-                  'helmet': [[240, 128, 128], [236, 236, 236]],  # got after 13600 frames
+                  'helmet': [[240, 128, 128], [236, 236, 236], [214, 214, 214], [192, 192, 192], [170, 170, 170]],
                   'shield': [214, 214, 214],
                   'lamp': [[187, 53, 53], [214, 214, 214]],
                   'apple': [[184, 50, 50], [110, 156, 66]],  # red and green. 110, 156, 66 is for green
@@ -38,18 +37,18 @@ class Player(GameObject):  # player could be shown over all enemies/other object
 
 # code for not much classes reward
 # cause they are not important for the agent
-class Reward(GameObject):
-    def __init__(self, num, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.rgb = (
-            (198, 89, 179),  # , [127, 92, 213]],
-            (135, 183, 84),  # , [213, 130, 74]],
-            (195, 144, 61),  # [84, 138, 210]],
-            (213, 130, 74),  # [84, 92, 214]],
-            (135, 183, 84),  # [214, 92, 92]],
-            (163, 57, 21)  # [164, 89, 208]]
-        )[num - 1]
-        # self.value = num
+# class Reward(GameObject):
+#     def __init__(self, num, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.rgb = [
+#             (198, 89, 179),  # , [127, 92, 213]],
+#             (135, 183, 84),  # , [213, 130, 74]],
+#             (195, 144, 61),  # [84, 138, 210]],
+#             (213, 130, 74),  # [84, 92, 214]],
+#             (135, 183, 84),  # [214, 92, 92]],
+#             (163, 57, 21)  # [164, 89, 208]]
+#         ][num - 1]
+#         # self.value = num
 
 
 class Cauldron(GameObject):
@@ -118,10 +117,40 @@ class Mug(GameObject):
         self.rgb = 184, 50, 50  # [[184, 50, 50], [214, 214, 214]]
 
 
-class Reward_50(GameObject):
+class Reward50(GameObject):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.rgb = 198, 89, 179
+
+
+class Reward100(GameObject):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.rgb = 135, 183, 84
+
+
+class Reward200(GameObject):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.rgb = 195, 144, 61
+
+
+class Reward300(GameObject):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.rgb = 213, 130, 74
+
+
+class Reward400(GameObject):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.rgb = 135, 183, 84
+
+
+class Reward500(GameObject):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.rgb = 163, 57, 21
 
 
 def _detect_objects_asterix(objects, obs, hud=False):
@@ -141,11 +170,38 @@ def _detect_objects_asterix(objects, obs, hud=False):
     for instance in enemy:
         objects.append(Enemy(*instance))
 
-    reward_50 = find_mc_objects(obs, objects_colors["reward_50"], min_distance=3, closing_dist=5, miny=24, maxy=151,
-                                size=(6, 11), tol_s=0
+    reward50 = find_mc_objects(obs, objects_colors["reward_50"], min_distance=3, closing_dist=5, miny=24, maxy=151)
+    # size=(6, 11), tol_s=0
+    for instance in reward50:
+        objects.append(Reward50(*instance))
+
+    reward100 = find_mc_objects(obs, objects_colors["reward_100"], min_distance=3, closing_dist=5, miny=24, maxy=151,
                                 )
-    for instance in reward_50:
-        objects.append(Reward_50(*instance))
+    for instance in reward100:
+        objects.append(Reward100(*instance))
+
+    reward200 = find_mc_objects(obs, objects_colors["reward_200"], min_distance=3, closing_dist=5, miny=24, maxy=151,
+
+                                )
+    for instance in reward200:
+        objects.append(Reward200(*instance))
+
+    reward300 = find_mc_objects(obs, objects_colors["reward_300"], min_distance=3, closing_dist=5, miny=24, maxy=151,
+                                )
+    for instance in reward300:
+        objects.append(Reward300(*instance))
+
+    reward400 = find_mc_objects(obs, objects_colors["reward_400"], min_distance=3, closing_dist=5, miny=24, maxy=151,
+
+                                )
+    for instance in reward400:
+        objects.append(Reward400(*instance))
+
+    reward500 = find_mc_objects(obs, objects_colors["reward_500"], min_distance=3, closing_dist=5, miny=24, maxy=151,
+
+                                )
+    for instance in reward500:
+        objects.append(Reward500(*instance))
 
     # code for not much classes from rewards
     # ctr = 1
@@ -154,14 +210,14 @@ def _detect_objects_asterix(objects, obs, hud=False):
     #                              size=(6, 11), tol_s=4)
     #     if reward is not None:
     #         for _ in range(1, 7):
-    #             reward = find_mc_objects(obs, objects_colors[x], min_distance=3, closing_dist=2, miny=24, maxy=151,  # )
+    #             reward = find_mc_objects(obs, objects_colors[x], min_distance=3, closing_dist=2, miny=24, maxy=151,
     #                                      size=(6, 11), tol_s=5)
     #         for instance in reward:
     #             objects.append(Reward(ctr, *instance))
     #         break
     #     ctr += 1
 
-    helmet = find_mc_objects(obs, objects_colors["helmet"], closing_dist=3, min_distance=4)
+    helmet = find_mc_objects(obs, objects_colors["helmet"], closing_dist=1, min_distance=1, size=(7, 11))
     for instance in helmet:
         objects.append(Helmet(*instance))
 
@@ -169,24 +225,24 @@ def _detect_objects_asterix(objects, obs, hud=False):
     for instance in shield:  # specify size(make it work)
         objects.append(Shield(*instance))
 
-    lamp = find_mc_objects(obs, objects_colors["lamp"], closing_dist=3)
+    lamp = find_mc_objects(obs, objects_colors["lamp"], closing_dist=3, size=(8, 11))
     for instance in lamp:
         objects.append(Lamp(*instance))
 
-    apple = find_mc_objects(obs, objects_colors["apple"], closing_dist=2, min_distance=2)
+    apple = find_mc_objects(obs, objects_colors["apple"], closing_dist=2, min_distance=2, size=(8, 11))
     for instance in apple:
         objects.append(Apple(*instance))
 
     fish = find_objects(obs, objects_colors["fish"], closing_dist=1, min_distance=1,
-                        size=(8, 5), tol_s=2, )  # size=(8, 5), tol_s=2,
+                        size=(8, 5), tol_s=2, )  # size=(8, 5), tol_s=2,  does it work?
     for instance in fish:
         objects.append(Fish(*instance))
 
-    meat = find_mc_objects(obs, objects_colors["meat"], closing_dist=1, min_distance=1)
+    meat = find_mc_objects(obs, objects_colors["meat"], closing_dist=1, min_distance=1, size=(5, 11))
     for instance in meat:
         objects.append(Meat(*instance))
 
-    mug = find_mc_objects(obs, objects_colors["mug"], closing_dist=2, min_distance=2)
+    mug = find_mc_objects(obs, objects_colors["mug"], closing_dist=2, min_distance=2, size=(7, 11))
     for instance in mug:
         objects.append(Mug(*instance))
 
@@ -201,5 +257,5 @@ def _detect_objects_asterix(objects, obs, hud=False):
 
     # do pars min max y for all
     # print("\nobjects:")
-    # print(*objects, sep="\n")
-    # print("\n")
+    print(*objects, sep="\n")
+    print("\n")
