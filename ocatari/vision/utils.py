9e1b112ffbf5743e44bd12883ad7884b432daef5
testLab <belal.alkufri@gmail.com>
2023-02-03 17:03:38 +0100

    small edition in utils
    edited the colors recognition that it has at least x-1 from all x colors

--- ocatari/vision/utils.py
+++ ocatari/vision/utils.py
@@ -129,7 +129,8 @@ def find_mc_objects(image, colors, closing_active=True, size=None, tol_s=10,
 
     masks = [cv2.inRange(image, np.array(color), np.array(color)) for color in colors]
     for mask in masks:
-        if mask.max() == 0:
+        # if mask.max() == 0:
+        if mask.max() < len(colors) - 1:  # changed by belal
             return []
     mask = sum(masks)
     if closing_active:
