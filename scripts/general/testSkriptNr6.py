6d615dfa8bd3691bd7df8c2ebfec1ec0c54702fc
testLab <belal.alkufri@gmail.com>
2023-02-04 18:57:08 +0100

    Update testSkriptNr6.py

--- scripts/general/testSkriptNr6.py
+++ scripts/general/testSkriptNr6.py
@@ -1,5 +1,4 @@
 from matplotlib import pyplot as plt
-
 from ocatari.core import OCAtari
 import time
 import random
@@ -11,33 +10,30 @@ observation, info = env.reset()
 prevRam = None
 
 constant = []
-not_important = constant + []
-already_figured_out = not_important + list(range(41, 50)) + list(range(94, 97))\
-                      + list(range(29, 37)) + list(range(19, 27))
+not_important = constant + list(range(55, 57))
+already_figured_out = not_important  + list(range(94, 97))\
+                      + list(range(29, 37))  # + list(range(19, 27)) + list(range(41, 50))
 
-value = 0
-index = 61
-for ROUND in range(1000000):
+value = 85
+index = 70
 
-    # if ROUND > 0:
-    #     print('round =', ROUND, 'ram[' + str(index-1) + '] =', ram[index-1])
-        # for k in [6, 38, 54, 83, 88, 89, 120]:  # checking specific values
-        #     print( 'ram[' + str(k) + '] =', ram[k])
-    # else:
-    #     print('round =', ROUND, 'index =', index)
+for ROUND in range(10000000):
+
+
     obs, reward, terminated, truncated, info = env.step(random.randint(-2, 2))
 
     if prevRam is not None:
-        i = 39  # to test an individual index in all rounds (where the index keeps constant)
+        i = 71  # to test an individual index in all rounds (where the index keeps constant)
         # i = index  # here the index meant to be incremented
 
-        # value = (prevRam[i] + 31) % 256  # 31 = 00011111
+        # value = (prevRam[i] + 31) % 256  # 31 = 00001111
+        value = (prevRam[i] + 33) % 256  # 33 = 00010001
         # value = (prevRam[i] + 3) % 256
-        # value = (prevRam[i] + 51) % 256  # 51 = 00110011 for efficient changes (= more and fast)
+        # value = (prevRam[i] + 85) % 256  # 85 = 01010101 for efficient changes (= more and fast)
         # value = (prevRam[i] + 1) % 256
         # value = (prevRam[i] + 16) % 256
         # value = (prevRam[i] * 2) % 256
-        # value = prevRam[i] + ( -1 ^ ( ROUND % 2 ))
+        # value = prevRam[i] + ( -1 ^ ( ROUND % 2 ))  # to alternate
         # if ROUND % 2 == 0:
         #     value = prevRam[i] -1
         # else:
@@ -46,49 +42,57 @@ for ROUND in range(1000000):
         # value = 86
 
         if ROUND % 3 == 0:
+            index += 1
             while index in already_figured_out:
-                index = index + 1
-            index = index + 1
+                index += 1
 
         # if ROUND > 1:
         #     env._env.unwrapped.ale.setRAM(i, value)  # DON'T CHANGE. CHANGE ABOVE
 
-        # print(ram)
-
         env._env.unwrapped.ale.setRAM(83, 2)
 
-        for k in range(len(ram)):
-            if ram[k] != prevRam[k] and k not in already_figured_out:
-                if ram[k] > prevRam[k]:
-                    integ = ram[k] - prevRam[k]
-                else:
-                    integ = prevRam[k] - ram[k]
+        print(ram)
 
-                if integ > 127:
-                    print(str(k), '\t', -integ, '\t',
-                          format(-integ, '08b'), '\t',
-                          str(ram[k]), '\t',
-                          format(ram[k], '08b'),
-                          "negativ")
-                else:
-                    print(str(k), '\t', integ, '\t',
-                          format(integ, '08b'), '\t',
-                          str(ram[k]), '\t',
-                          format(ram[k], '08b')
-                          )
+        if ROUND % 25 == 0:
+            for k in range(len(ram)):
+                if ram[k] != prevRam[k] and k not in already_figured_out:
+                    if ram[k] > prevRam[k]:
+                        integ = ram[k] - prevRam[k]
+                    else:
+                        integ = prevRam[k] - ram[k]
 
-        print("------------------------------------------")
+                    if integ > 127:
+                        print(str(k), '\t', -integ, '\t',
+                              format(-integ, '08b'), '\t',
+                              str(ram[k]), '\t',
+                              format(ram[k], '08b'),
+                              "negativ")
+                    else:
+                        print(str(k), '\t', integ, '\t',
+                              format(integ, '08b'), '\t',
+                              str(ram[k]), '\t',
+                              format(ram[k], '08b')
+                              )
 
-        if terminated or truncated:
-            observation, info = env.reset()
-        print(info)
-
-        env.render()
-        if info.get("frame_number") % 100 == 0:
+            print("------------------------------------------")
             rgb_array = env.render()
             plt.imshow(rgb_array)  # rgb_array stuff for fun
             plt.show()
-        time.sleep(0)
+
+        if terminated or truncated:
+            observation, info = env.reset()
+        # print(info)
+
+        if ROUND > 1:
+            print('round =', ROUND, 'ram[' + str(i) + '] =', ram[i])
+            # for k in [6, 38, 54, 83, 88, 89, 120]:  # checking specific values
+            #     print('ram[' + str(k) + '] =', ram[k])
+        else:
+            print('round =', ROUND, 'i =', i)
+
+        env.render()
+
+        # time.sleep(0)
     if ROUND > 0:
         prevRam = ram
     ram = env._env.unwrapped.ale.getRAM()
