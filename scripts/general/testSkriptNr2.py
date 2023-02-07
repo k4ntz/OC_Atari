c809051869db7143ef7efb40ed39b710005a29d5
BJenrajb <bj.nutzmail@gmail.com>
2023-02-07 16:47:36 +0100

    update vision and ram

--- scripts/general/testSkriptNr2.py
+++ scripts/general/testSkriptNr2.py
@@ -7,22 +7,24 @@ created by timo to set the ram and see whats changed
 """
 
 
-env = gym.make("Kangaroo", render_mode="human")
+env = gym.make("BeamRider", render_mode="human")
 env.metadata['render_fps'] = 60
 observation, info = env.reset(seed=42)
 observation, reward, terminated, truncated, info = env.step(0)
+
+# env.unwrapped.ale.setRAM(0, 5)
 
 for _ in range(1000):
     # action = policy(observation)  # User-defined policy function
 
     # -------------------manipulate ram----------------------------------
     ram = env.unwrapped.ale.getRAM()
-    target_ram_position = 19
+    target_ram_position = 74
     previous_ram_at_position = ram[target_ram_position]
-    new_ram_value = 19
+    new_ram_value = 99
     # print(new_ram_value)
-    print(ram)
-    # print(ram[57])
+    # print(ram)
+    print(ram[target_ram_position])
     if new_ram_value > 255 or new_ram_value < 0:
         print("ram out of bounds")
         new_ram_value = 0
@@ -30,7 +32,7 @@ for _ in range(1000):
     # env.unwrapped.ale.setRAM(11, 10)
     # -------------------------------------------------------------------
     terminated, truncated = False, False
-    observation, reward, terminated, truncated, info = env.step(6)
+    observation, reward, terminated, truncated, info = env.step(1)  # (random.randint(1,8))
     if terminated or truncated:
         observation, info = env.reset()
 
