fb7591836ef157ad38c81cd172e734a0f8a191ad
testLab <belal.alkufri@gmail.com>
2023-02-13 11:42:45 +0100

    score corrected
    now score is being detected as a whole
    closing_dist for shields optimized

--- scripts/demo/demo_space_invaders.py
+++ scripts/demo/demo_space_invaders.py
@@ -1,47 +1,44 @@
 # appends parent path to syspath to make ocatari importable
 # like it would have been installed as a package
-import sys
-sys.path.insert(0, '../../ocatari') # noqa
-from ocatari.core import OCAtari
 import random
+import sys
 import matplotlib.pyplot as plt
+import ipdb
+sys.path.insert(0, '../../')  # noqa
+
+from ocatari.core import OCAtari
 from ocatari.vision.utils import mark_bb, make_darker
-from ocatari.vision.space_invaders import objects_colors
-from ocatari.utils import load_agent, parser
+from ocatari.ram.demonAttack import ProjectileHostile
 
 game_name = "SpaceInvaders-v4"
 MODE = "vision"
-# MODE = "raw"
-# MODE = "revised"
-env = OCAtari(game_name, mode=MODE, render_mode='rgb_array')
+HUD = True
+env = OCAtari(game_name, mode=MODE, hud=HUD, render_mode='human')
 observation, info = env.reset()
-prevRam = None
-already_figured_out = []
 
-opts = parser.parse_args()
+for i in range(10000000):
+    # obs, reward, terminated, truncated, info = env.step(-2)
+    obs, reward, terminated, truncated, info = env.step(random.randint(-2, 2))
+    # env.step(env.action_space.sample())
+    env._env.unwrapped.ale.setRAM(83, 2)
 
-if opts.path:
-    agent = load_agent(opts, env.action_space.n)
-
-for i in range(1000):
-    if opts.path is not None:
-        action = agent.draw_action(env.dqn_obs)
-    else:
-        action = random.randint(0, 2)
-    obs, reward, terminated, truncated, info = env.step(action)
-    if info.get('frame_number') > 0 and i % 100 == 0:
-        for obj_name, o_list in info["objects"].items():
-            print(obj_name)
-            for oinfo in o_list:
-                opos = oinfo[:4]
-                ocol = oinfo[4:]
-                if MODE == "vision":
-                    ocol = objects_colors[obj_name]
+    if i % 1 == 0:
+        print(env.objects)
+        for obj in env.objects:
+            x, y = obj.xy
+            if x < 160 and y < 210:
+                opos = obj.xywh
+                ocol = obj.rgb
                 sur_col = make_darker(ocol)
+                if isinstance(obj, ProjectileHostile):
+                    sur_col = 255, 255, 255
                 mark_bb(obs, opos, color=sur_col)
-                # mark_point(obs, *opos[:2], color=(255, 255, 0))
+            # mark_point(obs, *opos[:2], color=(255, 255, 0))
+
         plt.imshow(obs)
         plt.show()
+
+        # ipdb.set_trace()
 
     if terminated or truncated:
         observation, info = env.reset()
