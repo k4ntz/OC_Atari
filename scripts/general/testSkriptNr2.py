d7dfdf6e4f910d104d2431b2d20e36837f5d1d48
BJenrajb <bj.nutzmail@gmail.com>
2023-02-16 17:27:05 +0100

    qbert setup

--- scripts/general/testSkriptNr2.py
+++ scripts/general/testSkriptNr2.py
@@ -7,7 +7,7 @@ created by timo to set the ram and see whats changed
 """
 
 
-env = gym.make("Kangaroo", render_mode="human")
+env = gym.make("Qbert", render_mode="human")
 env.metadata['render_fps'] = 60
 observation, info = env.reset(seed=42)
 observation, reward, terminated, truncated, info = env.step(0)
@@ -30,7 +30,7 @@ for _ in range(1000):
     # env.unwrapped.ale.setRAM(11, 10)
     # -------------------------------------------------------------------
     terminated, truncated = False, False
-    observation, reward, terminated, truncated, info = env.step(6)
+    observation, reward, terminated, truncated, info = env.step(random.randint(0, 5))
     if terminated or truncated:
         observation, info = env.reset()
 
