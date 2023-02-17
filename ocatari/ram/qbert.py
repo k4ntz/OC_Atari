99578a0a2af7999fae32ef7987aa8af95dca7e22
BJenrajb <bj.nutzmail@gmail.com>
2023-02-17 18:12:12 +0100

    player, score, lives position

--- ocatari/ram/qbert.py
+++ ocatari/ram/qbert.py
@@ -1,7 +1,7 @@
 from .game_objects import GameObject
 
 """
-RAM extraction for the game KANGUROO. Supported modes: raw, revised.
+RAM extraction for the game Q*BERT. Supported modes: raw, revised.
 
 """
 
@@ -10,7 +10,7 @@ class Player(GameObject):
     def __init__(self):
         super(Player, self).__init__()
         self._xy = 78, 103
-        self.wh = 8, 24
+        self.wh = 8, 19
         self.rgb = 181, 83, 40
         self.hud = False
 
@@ -72,8 +72,8 @@ class FlyingDiscs(GameObject):
 class Score(GameObject):
     def __init__(self):
         super(Score, self).__init__()
-        self._xy = 78, 103
-        self.wh = 8, 24
+        self._xy = 34, 6
+        self.wh = 37, 7
         self.rgb = 210, 210, 64
         self.hud = False
 
@@ -81,8 +81,8 @@ class Score(GameObject):
 class Lives(GameObject):
     def __init__(self):
         super(Lives, self).__init__()
-        self._xy = 78, 103
-        self.wh = 8, 24
+        self._xy = 33, 16
+        self.wh = 24, 12
         self.rgb = 210, 210, 64
         self.hud = False
 
@@ -101,8 +101,21 @@ def _init_objects_qbert_ram(hud=True):
 def _detect_objects_qbert_revised(objects, ram_state, hud=True):
     objects.clear()
 
+    if ram_state[68] != 0 and ram_state[43] != 0:
+        # oder ram_state[33], ram_state[67] für y
+        player = Player()
+        player.xy = ram_state[43] - 3, ram_state[68] + 17
+        objects.append(player)
+
+    if hud:
+        objects.append(Score())
+        objects.append(Lives())
+
     return objects
 
 
 def _detect_objects_qbert_raw(info, ram_state):
+
+    # ram_state[126] maybe sprite 171 = jump and 201 normal
+
     pass
