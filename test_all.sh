f8701d8a90fad6f1130d445f4745f2d03819ff08
Quentin Delfosse <q.delfosse@gmail.com>
2023-02-28 19:38:03 +0100

    added the test_all script

---
+++ test_all.sh
@@ -0,0 +1,4 @@
+for game in Asterix Berzerk Bowling Boxing Breakout Carnival DemonAttack Freeway Kangaroo MsPacman Pong Seaquest Skiing SpaceInvaders Tennis
+do
+    python3 tests/test_alignment.py -g $game
+done
