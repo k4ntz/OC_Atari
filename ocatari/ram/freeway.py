26c31ebc9c47b7fd1f74c8ccbed721c8851eedb9
Sebastian Szt <szt.sebastian@googlemail.com>
2023-03-01 21:16:03 +0100

    added method

--- ocatari/ram/freeway.py
+++ ocatari/ram/freeway.py
@@ -38,6 +38,22 @@ car_colors = {"car1": [167, 26, 26], "car2": [180, 231, 117], "car3": [105, 105,
               }
 
 
+# parses MAX_NB* dicts, returns default init list of objects
+def _get_max_objects(hud=False):
+
+    def fromdict(max_obj_dict):
+        objects = []
+        mod = sys.modules[__name__]
+        for k, v in max_obj_dict.items():
+            for _ in range(0, v):
+                objects.append(getattr(mod, k)())    
+        return objects
+
+    if hud:
+        return fromdict(MAX_NB_OBJECTS_HUD)
+    return fromdict(MAX_NB_OBJECTS)
+
+
 def _init_objects_freeway_ram(hud=False):
     """
     (Re)Initialize the objects
