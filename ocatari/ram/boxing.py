468a2fad09cb7522a9fcabe88684b14973323d2f
Sebastian Szt <szt.sebastian@googlemail.com>
2023-03-10 14:19:26 +0100

    boxing max objects

--- ocatari/ram/boxing.py
+++ ocatari/ram/boxing.py
@@ -1,4 +1,5 @@
 from .game_objects import GameObject
+import sys 
 
 
 MAX_NB_OBJECTS =  {'Player': 1, 'Enemy': 1}
@@ -89,6 +90,22 @@ class EnemyScore(GameObject):
             self._xy = 111, 5
             self.wh = 6, 7
             self._ten = False
+
+
+# parses MAX_NB* dicts, returns default init list of objects
+def _get_max_objects(hud=False):
+
+    def fromdict(max_obj_dict):
+        objects = []
+        mod = sys.modules[__name__]
+        for k, v in max_obj_dict.items():
+            for _ in range(0, v):
+                objects.append(getattr(mod, k)())    
+        return objects
+
+    if hud:
+        return fromdict(MAX_NB_OBJECTS_HUD)
+    return fromdict(MAX_NB_OBJECTS)
 
 
 def _init_objects_boxing_ram(hud=False):
