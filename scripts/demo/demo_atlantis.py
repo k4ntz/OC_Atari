66819028b7d09baba450999043c926b4d42a3ee2
christopher.schubert <christopher.schubert@stud.tu-darmstadt.de>
2023-03-13 20:35:01 +0100

    fix merge conflicts

--- scripts/demo/demo_atlantis.py
+++ scripts/demo/demo_atlantis.py
@@ -12,23 +12,12 @@ from copy import deepcopy
 
 game_name = "Atlantis"
 MODE = "vision"
-# MODE = "revised"
+MODE = "revised"
 HUD = True
 env = OCAtari(game_name, mode=MODE, hud=HUD, render_mode='rgb_array')
 observation, info = env.reset()
 prev_ram = None
 
-# ram_code_review = [0,31,0,31,30,31,40,31,10,31,100,31,255,0,1,0,255,255
-# ,29,126,29,128,112,152,152,152,24,152,152,0,152,29,162,0,67,1
-# ,0,78,40,0,0,255,0,96,4,192,2,96,0,96,0,96,5,48
-# ,2,224,0,96,91,160,75,52,0,67,64,0,253,96,2,91,17,7
-# ,7,130,201,254,2,2,2,208,48,48,176,1,0,0,5,5,5,0
-# ,5,0,2,0,3,0,0,0,0,7,186,198,192,48,224,42,29,0
-# ,0,8,3,0,13,2,0,0,16,3,3,2,0,1,24,20,255,77
-# ,115,21]
-
-# for i in range(128):
-#     env._env.unwrapped.ale.setRAM(i, ram_code_review[i])
 
 def tamper_state(env, ram_poses=[50, 51, 54], lambda_func=lambda x: x+20, to_exclude=[]):
     # import ipdb;ipdb.set_trace()
