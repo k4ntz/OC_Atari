e990f9d33e41e00ac4469425d6a8affa1d5b11ee
Quentin Delfosse <q.delfosse@gmail.com>
2023-03-25 19:53:32 +0100

    Added orientation to skiing

--- ocatari/ram/choppercommand.py
+++ ocatari/ram/choppercommand.py
@@ -2,9 +2,28 @@ from dataclasses import dataclass
 
 from ._helper_methods import number_to_bitfield, bitfield_to_number
 from .game_objects import GameObject
+import sys
+
 
 # RAM
+MAX_NB_OBJECTS =  {'Player': 2, 'Truck': 3, 'MiniPlayer': 1, 'MiniEnemy': 12, 'MiniTruck': 9, 'Shot': 2, 'EnemyPlane': 3, 'EnemyHelicopter': 3, 'Bomb': 3}
+MAX_NB_OBJECTS_HUD =  {'Score': 4, 'Life': 2, 'Player': 2, 'Truck': 3, 'MiniPlayer': 1, 'MiniEnemy': 12, 'MiniTruck': 9, 'Shot': 2, 'EnemyPlane': 3, 'EnemyHelicopter': 3, 'Bomb': 3}
 
+
+# parses MAX_NB* dicts, returns default init list of objects
+def _get_max_objects(hud=False):
+
+    def fromdict(max_obj_dict):
+        objects = []
+        mod = sys.modules[__name__]
+        for k, v in max_obj_dict.items():
+            for _ in range(0, v):
+                objects.append(getattr(mod, k)())    
+        return objects
+
+    if hud:
+        return fromdict(MAX_NB_OBJECTS_HUD)
+    return fromdict(MAX_NB_OBJECTS)
 
 class Player(GameObject):
     def __init__(self):
