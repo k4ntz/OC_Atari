8722e1ea2302ab68c33a7557b9ca43cf7bcb8f73
BJenrajb <bj.nutzmail@gmail.com>
2023-05-06 12:18:20 +0200

    ChopperCommand shots fixed

--- scripts/demo/demo_choppercommand.py
+++ scripts/demo/demo_choppercommand.py
@@ -8,25 +8,22 @@ sys.path.insert(0, '../../')  # noqa
 
 from ocatari.core import OCAtari
 from ocatari.vision.utils import mark_bb, make_darker
-from ocatari.ram.demonAttack import ProjectileHostile
+from ocatari.ram.demonattack import ProjectileHostile
 
 game_name = "ChopperCommand-v4"
-MODE = "vision"
+MODE = "revised"
 HUD = True
 env = OCAtari(game_name, mode=MODE, hud=HUD, render_mode='human')
 observation, info = env.reset()
 
-for i in range(10000000):
-    # obs, reward, terminated, truncated, info = env.step(-2)
-    obs, reward, terminated, truncated, info = env.step(random.randint(-2, 2))
+for i in range(10000):
+    obs, reward, terminated, truncated, info = env.step(1)
+    # obs, reward, terminated, truncated, info = env.step(random.randint(-2, 2))
     # env.step(env.action_space.sample())
-    env._env.unwrapped.ale.setRAM(83, 2)
-
-    if i % 1000 == 0:
-        print(i)
     if i % 10 == 0:
         print(env.objects)
-        env._env.unwrapped.ale.setRAM(54, i//10 + 4)
+        ram = env._env.unwrapped.ale.getRAM()
+        # env._env.unwrapped.ale.setRAM(54, i//10 + 4)
         for obj in env.objects:
             x, y = obj.xy
             if x < 160 and y < 210:
