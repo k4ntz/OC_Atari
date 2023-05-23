bde534c882f66210e8f5d6916d606e43105a518f
Quentin Delfosse <q.delfosse@gmail.com>
2023-05-23 19:22:33 +0200

    Readded the changed named files

---
+++ ocatari/vision/riverraid.py
@@ -0,0 +1,125 @@
+from .utils import find_objects
+from .utils import find_mc_objects
+from .game_objects import GameObject
+
+objects_colors = {"player": [232, 232, 74], "logo": [232, 232, 74], "background_water": [45, 50, 184],
+                  "background_grass": [110, 156, 66], "fuel_bar": [0, 0, 0], "lives": [232, 232, 74],
+                  "score": [232, 232, 74], "player_missile": [232, 232, 74],
+                  "helicopter": [[0, 64, 48], [0, 0, 148], [210, 164, 74]],
+                  "house": [[214, 214, 214], [0, 0, 0]],
+                  "tanker": [[84, 160, 197], [163, 57, 21], [0, 0, 0]],
+                  "fuel_depot": [[214, 92, 92], [214, 214, 214]],
+                  "jet": [[117, 204, 235], [117, 181, 239], [117, 128, 240]],
+                  "bridge": [[187, 187, 53], [105, 105, 15], [134, 134, 29], [124, 44, 0]]}
+
+
+class Player(GameObject):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.rgb = 232, 232, 74
+
+
+class PlayerMissile(GameObject):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.rgb = 232, 232, 74
+
+
+class Helicopter(GameObject):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.rgb = 0, 64, 48
+
+
+class Tanker(GameObject):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.rgb = 84, 160, 197
+
+
+class FuelDepot(GameObject):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.rgb = 210, 91, 94
+
+
+class Bridge(GameObject):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.rgb = 134, 134, 29
+
+
+class Jet(GameObject):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.rgb = 117, 181, 239
+
+
+class PlayerScore(GameObject):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.rgb = 232, 232, 74
+
+
+class Lives(GameObject):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.rgb = 232, 232, 74
+
+
+class Logo(GameObject):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.rgb = 232, 232, 74
+
+
+def _detect_objects_riverraid(objects, obs, hud=False):
+    objects.clear()
+
+    player = find_objects(obs, objects_colors["player"], min_distance=1)
+    for p in player:
+        if p[1] < 160 and p[2] > 2 and p[3] > 10:
+            objects.append(Player(*p))
+
+    player_missile = find_objects(obs, objects_colors["player_missile"], min_distance=1)
+    for missile in player_missile:
+        if missile[2] < 2 and 1 < missile[3] < 10 and missile[1] < 160:
+            objects.append(PlayerMissile(*missile))
+
+    helicopter = find_mc_objects(obs, objects_colors["helicopter"], min_distance=1)
+    for heli in helicopter:
+        objects.append(Helicopter(*heli))
+
+    tanker = find_mc_objects(obs, objects_colors["tanker"], min_distance=1)
+    for tank in tanker:
+        if tank[0] > 0 and tank[1] < 175:
+            objects.append(Tanker(*tank))
+
+    jet = find_mc_objects(obs, objects_colors["jet"], min_distance=1)
+    for j in jet:
+        objects.append(Jet(*j))
+
+    fuel_depot = find_mc_objects(obs, objects_colors["fuel_depot"], min_distance=1)
+    for fuel in fuel_depot:
+        if fuel[3] > fuel[2]:
+            objects.append(FuelDepot(*fuel))
+
+    bridge = find_mc_objects(obs, objects_colors["bridge"], min_distance=1)
+    for br in bridge:
+        objects.append(Bridge(*br))
+
+    if hud:
+        logo = find_objects(obs, objects_colors["logo"], min_distance=1)
+        for log in logo:
+            if log[1] > 190 and log[0] > 70:
+                objects.append(Logo(*log))
+
+        lives = find_objects(obs, objects_colors["lives"], min_distance=1)
+        for liv in lives:
+            if liv[1] > 190 and liv[0] < 62:
+                objects.append(Lives(*liv))
+
+        score = find_objects(obs, objects_colors["score"], min_distance=1, closing_dist=1)
+        for sc in score:
+            if 160 < sc[1] < 190:
+                objects.append(PlayerScore(*sc))
---
+++ ocatari/vision/spaceinvaders.py
@@ -0,0 +1,93 @@
+from .utils import find_objects
+from .game_objects import GameObject
+
+objects_colors = {"player": [50, 132, 50], "score": [50, 132, 50],
+                  "player2": [162, 134, 56], "score2": [162, 134, 56],
+                  "alien": [134, 134, 29], "shield": [181, 83, 40],
+                  "satellite": [151, 25, 122], "bullet": [142, 142, 142],
+                  "lives": [162, 134, 56]
+                  }
+
+
+class Player(GameObject):
+    def __init__(self, x, y, w, h, num, *args):
+        super().__init__(x, y, w, h, *args)
+        if num == 1:
+            self.rgb = 92, 186, 92
+        else:
+            self.rgb = 162, 134, 56  # yellow
+        self.player_num = num
+
+
+class Alien(GameObject):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.rgb = 134, 134, 29
+
+
+class Satellite(GameObject):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.rgb = 151, 25, 122
+
+
+class Shield(GameObject):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.rgb = 181, 83, 40
+
+
+class Bullet(GameObject):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.rgb = 142, 142, 142
+
+
+class Score(GameObject):
+    def __init__(self, x, y, w, h, num, *args):
+        super().__init__(x, y, w, h, *args)
+        if num == 1:
+            self.rgb = 92, 186, 92
+        else:
+            self.rgb = 162, 134, 56
+
+
+class Lives(GameObject):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.rgb = 162, 134, 56
+
+
+def _detect_objects_spaceinvaders(objects, obs, hud):
+    objects.clear()
+
+    for i, obj in enumerate(["player", "player2"]):
+        players = find_objects(obs, objects_colors[obj], closing_active=False,
+                               miny=180, maxy=195)
+        for instance in players:
+            if instance[2] < 10:  # width
+                objects.append(Player(*instance, i + 1))
+            elif hud and instance[2] > 10:
+                objects.append(Lives(*instance))
+
+    if hud:
+        for i, obj in enumerate(["score", "score2"]):
+            scores = find_objects(obs, objects_colors[obj], closing_dist=12, maxy=30)
+            for instance in scores:
+                objects.append(Score(*instance, i + 1))
+
+    aliens = find_objects(obs, objects_colors["alien"])
+    for instance in aliens:
+        objects.append(Alien(*instance))
+
+    shields = find_objects(obs, objects_colors["shield"], closing_dist=17, min_distance=20)
+    for instance in shields:
+        objects.append(Shield(*instance))
+
+    satellites = find_objects(obs, objects_colors["satellite"])
+    for instance in satellites:
+        objects.append(Satellite(*instance))
+
+    bullets = find_objects(obs, objects_colors["bullet"])
+    for instance in bullets:
+        objects.append(Bullet(*instance))
