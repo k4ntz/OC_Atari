5e3a3016a36c938fe64ad87b6b4357d338dee12f
Kévin-Lâm Quesnel <kevin-lam@hotmail.fr>
2023-05-26 01:15:58 +0200

    finish correction on fishingDerby vision detection

--- ocatari/vision/fishingDerby.py
+++ ocatari/vision/fishingDerby.py
@@ -11,18 +11,21 @@ class Shark(GameObject):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.rgb = 0, 0, 0
+        self.direction = True  # is the shark going from right to left
 
 
 class Fish(GameObject):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.rgb = 232, 232, 74
+        self.hooked = False
 
 
 class PlayerOneFishingHook(GameObject):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.rgb = 232, 232, 74
+        self.hook_position = 0, 0
 
 
 class ScorePlayerTwo(GameObject):
@@ -35,6 +38,7 @@ class PlayerTwoFishingHook(GameObject):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.rgb = 0, 0, 0
+        self.hook_position = 0, 0
 
 
 class ScorePlayerOne(GameObject):
@@ -46,33 +50,36 @@ class ScorePlayerOne(GameObject):
 def _detect_objects_fishingDerby(objects, obs, hud=True):
     objects.clear()
 
-    for shark in find_objects(obs, objects_colors["shark"], closing_dist=1, minx=28):
+    count_shark = 0
+    for shark in find_objects(obs, objects_colors["shark"], closing_dist=1, minx=30, maxx=126, miny=75, maxy=96):
         shark_instance = Shark(*shark)
-        if shark_instance.wh[1] < 20 and shark_instance.wh[1] > 5 and shark_instance.y > 50:
-            objects.append(Shark(*shark))
+        if 82 > shark_instance.y > 78 and 11 < shark_instance.h < 14:
+            objects.append(shark_instance)
+            count_shark += 1
+    if count_shark == 0:
+        for shark in find_objects(obs, objects_colors["shark"], closing_dist=1, minx=30, maxx=126, miny=75, maxy=96):
+            shark_instance = Shark(*shark)
+            objects.append(shark_instance)
 
     for fish in find_objects(obs, objects_colors["fish"], closing_dist=8):
         fish_instance = Fish(*fish)
-        if fish_instance.y > 90 and fish_instance.y < 190:
+        if 90 < fish_instance.y < 190:
             objects.append(fish_instance)
 
     for p1_fish_hook in find_objects(obs, objects_colors["player 1 fishing hook"], closing_dist=1):
-        p1_fish_hook_instance = PlayerOneFishingHook(*p1_fish_hook)
-        if p1_fish_hook_instance.y < 80 and p1_fish_hook_instance.x > 25:
-            objects.append(p1_fish_hook_instance)
+        p1_fish_hook = PlayerOneFishingHook(*p1_fish_hook)
+        if p1_fish_hook.y < 80 and p1_fish_hook.x > 25:
+            p1_fish_hook.hook_position = p1_fish_hook.x + p1_fish_hook.w, \
+                                         p1_fish_hook.y + p1_fish_hook.wh[1]
+            objects.append(p1_fish_hook)
 
-    notp2FishingPole = [[90, 80], [70, 130], [90, 77], [80, 77]]
-    for p2_fishing_pole in find_objects(obs, objects_colors["player 2 fishing hook"]):
-        player_two_fishing_pole = PlayerTwoFishingHook(*p2_fishing_pole)
-        if player_two_fishing_pole.y > 75 and player_two_fishing_pole.x > 80:
-            fishing_pole = True
-            for coordinates in notp2FishingPole:
-                if player_two_fishing_pole.x == coordinates[0] and player_two_fishing_pole.y == coordinates[1]:
-                    fishing_pole = False
-                    break
-
-            if fishing_pole:
-                objects.append(player_two_fishing_pole)
+    for p2_fishing_pole in find_objects(obs, objects_colors["player 2 fishing hook"], miny=75, minx=30, maxx=130,
+                                        maxy=188, closing_dist=1):
+        p2_fish_hook = PlayerTwoFishingHook(*p2_fishing_pole)
+        if 80 > p2_fish_hook.y > 75:
+            p2_fish_hook.hook_position = p2_fish_hook.x + p2_fish_hook.w, \
+                                                    p2_fish_hook.y + p2_fish_hook.wh[1]
+            objects.append(p2_fish_hook)
 
     if hud:
         for score in find_objects(obs, objects_colors["score"], closing_dist=1):
