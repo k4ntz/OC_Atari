2ccab9486bccb59204ade6818fe0960a63619478
Kévin-Lâm Quesnel <kevin-lam@hotmail.fr>
2023-05-26 02:26:20 +0200

    [TO BE TESTED] implement RAM objects recognition

--- ocatari/ram/fishingderby.py
+++ ocatari/ram/fishingderby.py
@@ -1,8 +1,8 @@
 from .game_objects import GameObject
 import sys
 
-MAX_NB_OBJECTS = {"Player1FishingString": 1, "Player2FishingString": 1, "Fish": 50, "Shark": 1}
-MAX_NB_OBJECTS_HUD = {"Player1FishingString": 1, "Player2FishingString": 1, "Fish": 50, "Shark": 1, "ScoreP1": 1,
+MAX_NB_OBJECTS = {"Player1FishingString": 1, "Player2FishingString": 1, "Fish": 6, "Shark": 1}
+MAX_NB_OBJECTS_HUD = {"Player1FishingString": 1, "Player2FishingString": 1, "Fish": 6, "Shark": 1, "ScoreP1": 1,
                       "ScoreP2": 1}
 
 
@@ -10,6 +10,8 @@ class Fish(GameObject):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.rgb = 232, 232, 74
+        self.xy = 0, 0
+        self.wh = 8, 6
         self.hooked: bool = False
 
 
@@ -17,7 +19,9 @@ class Shark(GameObject):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.rgb = 232, 232, 74
-        self.is_going_right_to_left = True  # is the shark going from left to right
+        self.xy = 0, 0
+        self.wh = 35, 19
+        self.is_going_left_to_right = True  # is the shark going from left to right
         self.previous_pos = 0
 
 
@@ -25,24 +29,7 @@ class PlayerOneFishString(GameObject):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.rgb = 232, 232, 74
-        self.xy = 0, 0
-        self.wh = 0, 0
-        self.hooking: bool = False
-        self.hook_position = 0, 0
-
-
-class ScorePlayerTwo(GameObject):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.rgb = 167, 26, 26
-        self.value = 0
-
-
-class PlayerTwoFishingString(GameObject):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.rgb = 0, 0, 0
-        self.xy = 0, 0
+        self.xy = 0, 31
         self.wh = 0, 0
         self.hooking: bool = False
         self.hook_position = 0, 0
@@ -52,6 +39,27 @@ class ScorePlayerOne(GameObject):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.rgb = 167, 26, 26
+        self.xy = 49, 8
+        self.wh = 6, 8
+        self.value = 0
+
+
+class PlayerTwoFishingString(GameObject):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.rgb = 0, 0, 0
+        self.xy = 0, 31
+        self.wh = 0, 0
+        self.hooking: bool = False
+        self.hook_position = 0, 0
+
+
+class ScorePlayerTwo(GameObject):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.rgb = 167, 26, 26
+        self.xy = 113, 8
+        self.wh = 6, 8
         self.value = 0
 
 
@@ -60,38 +68,57 @@ def _get_max_objects(hud=False):
 
 
 def _init_objects_fishingDerby_ram(hud=False):
-    objects = [PlayerOneFishString(), PlayerTwoFishingString(), Fish(), Fish(), Fish(), Fish(), Fish(), Fish(), Shark(),
-               ScorePlayerOne(), ScorePlayerTwo()]
+    if hud:
+        objects = [PlayerOneFishString(), PlayerTwoFishingString(), Fish(), Fish(), Fish(), Fish(), Fish(), Fish(),
+                   Shark(),
+                   ScorePlayerOne(), ScorePlayerTwo()]
+    else:
+        objects = [PlayerOneFishString(), PlayerTwoFishingString(), Fish(), Fish(), Fish(), Fish(), Fish(), Fish(),
+                   Shark()]
     return objects
 
 
 def _detect_objects_fishingDerby_revised(objects, ram_state, hud=False):
-
-    #Ram state of the fishing poles
-    p1h, p2h = objects[0:2]
-    p1h.xy = ram_state[32] - 1, p1h.xy[1]
-    p2h.xy = ram_state[33] - 1, p2h.xy[1]
-
+    # Ram state of the fishing poles
+    p1s, p2s = objects[0:2]
+    coeff_1 = 1
+    coeff_2 = 1
+    if ram_state[30] == 16:
+        coeff_1 = -1
+    if ram_state[31] == 16:
+        coeff_2 = -1
+    p1s.xy = min(31 + ram_state[21] * 4 + coeff_1 * ram_state[34], 30 + ram_state[21] * 4), p1s.xy[1]
+    p2s.xy = min(126 - ram_state[22] * 4 + coeff_2 * ram_state[35], 127 - ram_state[21] * 4), p2s.xy[1]
+    p1s.hook_position = 31 + ram_state[21] * 4 + coeff_1 * ram_state[35], ram_state[65] / 16 * 72 + 83
+    p2s.hook_position = 126 - ram_state[22] * 4 + coeff_2 * ram_state[35], ram_state[66] / 16 * 72 + 83
+    p1s.wh = abs(p1s.xy[0] - p1s.hook_position[0]), abs(p1s.xy[1] - p1s.hook_position[1])
+    p2s.wh = abs(p2s.xy[0] - p2s.hook_position[0]), abs(p2s.xy[1] - p2s.hook_position[1])
     # Considering that the first fish is the one at the top layer and
     fishes_hooked = []
     if ram_state[112] != 0:
-        fishes_hooked.append(5 - ram_state[112])
+        fishes_hooked.append([5 - ram_state[112]])
     if ram_state[113] != 0:
         fishes_hooked.append(5 - ram_state[113])
+
     for i in range(6):
         if i in [fishes_hooked]:
             objects[2 + i].hooked = True
-        if not objects[6 - i].hooked:
+        if not objects[2 + i].hooked:
             objects[2 + i].xy = ram_state[74 - i], 97 + 16 * i
         else:
-            objects[2 + i].xy = ram_state[74 - i], objects[2 + i].xy[1] - 6.8
-
+            if 5-ram_state[112] == i:
+                # it means the fish was caught by the player 1 thus its position is
+                # the position of p1's hook + half the height of a fish
+                objects[2 + i].xy = ram_state[74 - i], p1s.hook_position[1] + 2
+            else:
+                objects[2 + i].xy = ram_state[74 - i], p2s.hook_position[1] + 2
 
     objects[8].previous_pos = objects[8].xy[0]
     objects[8].xy = ram_state[75], 79
     objects[8].is_right_to_left = objects[8].xy[0] - objects[8].previous_pos > 0
-    objects[9].value = ram_state[61]
-    objects[9].value = ram_state[62]
+    if hud:
+        objects[9].value = ram_state[61]
+        objects[9].value = ram_state[62]
 
 
 def _detect_objects_fishingDerby_raw(info, ram_state):
