97e60645bd8efd1a8dfe87429d1790bf9d50b158
bluemlJ <me@jblue.ml>
2023-05-30 17:24:41 +0200

    updated dataset_generation scripts

---
+++ dataset_generation/datasets_on_all.sh
@@ -0,0 +1,13 @@
+# +
+# BOTH
+for game in Assault Asterix Atlantis Berzerk Bowling Boxing Breakout Carnival Freeway Kangaroo MsPacman Pong Seaquest Skiing SpaceInvaders Tennis
+do
+    python3 generate_dataset.py -g $game
+done
+
+# VISION ONLY
+for game in Alien Asteroids BeamRider ChopperCommand DemonAttack FishingDerby Frostbite MontezumaRevenge Qbert Riverraid RoadRunner 
+# for game in Breakout Pong
+do
+    python3 generate_dataset_vision.py -g $game
+done
