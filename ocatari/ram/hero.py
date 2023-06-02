c072a405e1d63a9eafef932cf952bce4ee56e260
Kévin-Lâm Quesnel <kevin-lam@hotmail.fr>
2023-06-02 17:24:14 +0200

    correct initialisation hero files

--- ocatari/ram/hero.py
+++ ocatari/ram/hero.py
@@ -2,14 +2,14 @@ MAX_NB_OBJECTS = {}
 MAX_NB_OBJECTS_HUD = {}
 
 
-def _init_objects_fishingDerby_ram(hud=False):
+def _init_objects_hero_ram(hud=False):
     objects = []
     return objects
 
 
-def _detect_objects_fishingDerby_revised(objects, ram_state, hud=False):
+def _detect_objects_hero_revised(objects, ram_state, hud=False):
     return
 
 
-def _detect_objects_fishingDerby_raw(info, ram_state):
+def _detect_objects_hero_raw(info, ram_state):
     return
