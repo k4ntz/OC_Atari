d0734ed303b6dd473e9e8d7a461137dd8da3ba7d
Quentin Delfosse <quentin.delfosse@tu-darmstadt.de>
2023-07-05 12:07:16 +0200

    Fixed pip install, currently at 0.1.2

--- setup.py
+++ setup.py
@@ -4,13 +4,10 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-with open("requirements.txt", "r", encoding="utf-8") as fh:
-    requirements = fh.readlines()
-
 
 setup(
     name='ocatari',
-    version='0.1.0',
+    version='0.1.2',
     author='Quentin Delfosse',
     author_email='quentin.delfosse@cs.tu-darmstadt.de',
     packages=find_packages(),
@@ -21,5 +18,20 @@ setup(
     description='Object Centric Atari 2600',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    install_requires=requirements
+    install_requires=[
+        "gymnasium",
+        "matplotlib",
+        "numpy",
+        "opencv_python",
+        "scikit_image",
+        "termcolor",
+        "seaborn",
+        "pandas",
+        "scikit-learn",
+        "tqdm",
+        "pyfiglet",
+    ]
 )
+
+print("Please install gymnasium atari dependencies, using:\n", 
+      "pip install gymnasium[atari, accept-rom-license]")
