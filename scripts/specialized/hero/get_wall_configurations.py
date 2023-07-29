a63116ca79213ee13d2c53c181ba75e45d863fd8
Kévin-Lâm Quesnel <kevin-lam@hotmail.fr>
2023-07-29 06:17:30 +0200

    [WIP] advance in implementation of the script that gets the wall position for each level

--- scripts/specialized/hero/get_wall_configurations.py
+++ scripts/specialized/hero/get_wall_configurations.py
@@ -29,8 +29,6 @@ RENDER_MODE = "rgb_array"
 env = OCAtari(opts.game + "NoFrameskip", mode=MODE, render_mode=RENDER_MODE)
 random.seed(0)
 
-initial_ram_n = 0
-
 make_deterministic(0, env)
 
 get_bin = lambda x: format(int(x), 'b').zfill(8)
@@ -52,59 +50,66 @@ snapshot = None
 current_directory = os.path.dirname(os.path.abspath(__file__))
 file_name = "walls_configurations.txt"
 file_path = os.path.join(current_directory, file_name)
-code_file = open("walls_configurations.txt", "a")
+code_file = open(file_path, "a")
 code = ""
 # snapshot = pickle.load(open("kangstarted.pkl", "rb"))
 # env._env.env.env.ale.restoreState(snapshot)
 
-if snapshot is None:
-    for _ in range(20):
-        resulting_obs, _, _, _, _ = env.step(random.randint(0, env.nb_actions - 1))
-        snapshot = env._env.env.env.ale.cloneState()
-
-base_next_obs, _, _, _, _ = env.step(0)
-base_objects = deepcopy(env.objects)
-binary_mode = False
 
 level_ram = 117
 config_ram = 28
-
+code += "def add_walls_to_object_list(ram_state, objects, objects_map):\n"
 for level_number in range(0, 20):
-    indentation = ""
-    code += f"elif ram_state[117] == {level_number}:\n"
+    indentation = "    "
+    code += indentation + f"elif ram_state[117] == {level_number}:\n"
     env.set_ram(level_ram, level_number)
+    if level_number == 0:
+        for i in range(50):
+            env.step(3)
+            env.step(4)
+    for i in range(30):
+        env.step(0)
     env.set_ram(32, 0)
+    env.set_ram(42, 81)
     for configuration_number in range(number_of_configuration_per_level[level_number]):
+        indentation = "    "
         # destroy destructible walls
         env.set_ram(32, 0)
-        indentation += "  "
+        indentation += "    "
         if configuration_number == 0:
-            code += indentation + f"if ram_state[28] == {config_ram}:\n"
+            code += indentation + f"if ram_state[28] == {configuration_number}:\n"
         else:
-            code += indentation + f"elif ram_state[28] == {config_ram}:\n"
-        env._env.env.env.ale.restoreState(snapshot)
+            code += indentation + f"elif ram_state[28] == {configuration_number}:\n"
+        # env._env.env.env.ale.restoreState(snapshot)
         env.set_ram(config_ram, configuration_number)
-        resulting_obs, _, _, _, _ = env.step(15)
+        resulting_obs, _, _, _, _ = env.step(0)
+        env.set_ram(31, 60)
+        if configuration_number == number_of_configuration_per_level[level_number] - 1:
+            print(env.get_ram()[41])
+            env.set_ram(27, env.get_ram()[41])
+            print(env.get_ram()[27], env.get_ram()[31])
+            for i in range(30):
+                env.step(0)
         print(f"level {level_number} configuration number {configuration_number}")
 
         walls = detect_Walls(resulting_obs)
-        indentation += "   "
-        show_ims(resulting_obs, level_number, configuration_number)
+        indentation += "    "
+        # show_ims(resulting_obs, level_number, configuration_number)
         for i in range(len(walls)):
             wall = walls[i]
             code += indentation + "wall_instance = Wall()\n"
             code += indentation + f"wall_instance.xy = {wall.xy}\n"
             code += indentation + f"wall_instance.wh = {wall.wh}\n"
             code += indentation + "objects.append(wall_instance)\n"
-            code += indentation + f"objects_map[fixed_wall {i}] = wall_instance\n"
+            code += indentation + f"objects_map['fixed_wall {i}'] = wall_instance\n"
         code += indentation + f"number_of_walls = {len(walls)}\n"
         code += indentation + "i = 0\n"
-        code += indentation + """while(objects_map.get(f"fixed wall {number_of_walls + i}"!= none):\n"""
+        code += indentation + """while(objects_map.get(f"fixed wall {number_of_walls + i}")!= None):\n"""
         indentation += "    "
         code += indentation + """objects_map.pop(f"fixed wall {number_of_walls + i}")\n"""
         code += indentation + "i += 1\n"
 
     code += "\n"
-
+code += "    " + "return objects, objects_map"
 code_file.write(code)
 code_file.close()
