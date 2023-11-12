ee3b363805c3218f2a3162f8295f7131fefb06fa
Quentin Delfosse <q.delfosse@gmail.com>
2023-11-12 18:14:26 +0100

    Added riverraid

--- ocatari/core.py
+++ ocatari/core.py
@@ -34,7 +34,7 @@ except ModuleNotFoundError:
         "Try `pip install pygame`.\n",
     )
 
-DEVICE = "gpu" if torch.cuda.is_available() else "cpu"
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 AVAILABLE_GAMES = ["Alien", "Assault", "Asterix", "Asteroids", "Atlantis", "BeamRider", "Berzerk", "Bowling", "Boxing",
                    "Breakout", "Carnival", "Centipede", "ChoppperCommand", "DemonAttack", "Enduro", "FishingDerby", "Freeway",
