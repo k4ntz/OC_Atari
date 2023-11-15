6c3c4343b939b1d164dd4571631a0f0e547ee73c
BJenrajb <bj.nutzmail@gmail.com>
2023-11-15 17:31:56 +0100

    Amidar update

--- ocatari/vision/amidar.py
+++ ocatari/vision/amidar.py
@@ -2,43 +2,69 @@ from .game_objects import GameObject
 from .utils import find_objects, find_mc_objects, most_common_color
 import numpy as np
 
-objects_colors = {"green": [111, 210, 111], "red": [240, 128, 128], "blue": [0, 0, 148], "dark_blue": [0, 48, 100],
-                  "life_green": [72, 160, 72]}
+objects_colors = {"yellow": [252, 252, 84], "green": [135, 183, 84], "red": [214, 92, 92]}
 
 
 class Player(GameObject):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.rgb = [111, 210, 111]
+        self.rgb = [252, 252, 84]
+
+
+class Monster_green(GameObject):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.rgb = [135, 183, 84]
+
+
+class Monster_yellow(GameObject):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.rgb = [252, 252, 84]
+
+
+class Monster_red(GameObject):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.rgb = [214, 92, 92]
 
 
 #  ---- HUD -----
 class Score(GameObject):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.rgb = [111, 210, 111]
+        self.rgb = [252, 252, 84]
 
 
 class Life(GameObject):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.rgb = [72, 160, 72]
+        self.rgb = [252, 252, 84]
 
 
 def _detect_objects_amidar(objects, obs, hud=False):
     objects.clear()
 
-    player = find_mc_objects(obs, [objects_colors["green"], objects_colors["red"]], miny=40)
-    for bb in player:
-            objects.append(Player(*bb))
-    
-            
+    yellow = find_objects(obs, objects_colors["yellow"], maxy=170)
+    for bb in yellow:
+            # print(np.shape(obs))
+            if obs[bb[1]][bb[0]][0] == 252:
+                objects.append(Monster_yellow(*bb))
+            else:
+                objects.append(Player(*bb))
+
+    green = find_objects(obs, objects_colors["green"])
+    for bb in green:
+            objects.append(Monster_green(*bb))
+
+    red = find_objects(obs, objects_colors["red"])
+    for bb in red:
+            objects.append(Monster_red(*bb))
 
     if hud:
-        score = find_objects(obs, objects_colors["green"], maxy=40, closing_dist=4, min_distance=1)
+        score = find_objects(obs, objects_colors["yellow"], miny=175, closing_dist=4, min_distance=1)
         for bb in score:
-                objects.append(Score(*bb))
-        
-        lives = find_mc_objects(obs, [objects_colors["life_green"], objects_colors["red"]], maxy=40)
-        for bb in lives:
-                objects.append(Life(*bb))
+                if bb[2] > 4:
+                    objects.append(Score(*bb))
+                else:
+                    objects.append(Life(*bb))
