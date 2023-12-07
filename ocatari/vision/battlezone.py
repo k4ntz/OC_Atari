62da6bc74ff00c496588ddad8c6f7ed838db4262
BJenrajb <bj.nutzmail@gmail.com>
2023-12-07 15:30:04 +0100

    bankheist + battlezone

--- ocatari/vision/battlezone.py
+++ ocatari/vision/battlezone.py
@@ -3,8 +3,8 @@ from .utils import find_objects, find_mc_objects, most_common_color
 import numpy as np
 
 objects_colors = {"player": [[26, 102, 26], [111, 111, 111], [74, 74, 74]], "radar":[111, 210, 111], "radar_hud": [236, 236, 236], "tank_blue1": [24, 80, 128],
-                  "tank_blue2": [66, 136, 176], "tank_grey": [142, 142, 142], "crosshair1": [0, 0, 0], "crosshair2": [236, 200, 96],
-                  "red": [[228, 111, 111], [200, 72, 72], [148, 0, 0]]}
+                  "tank_blue2": [66, 136, 176], "tank_grey": [142, 142, 142], "tank_grey2": [170, 170, 170], "tank_yellow": [195, 144, 61],
+                  "crosshair1": [0, 0, 0], "crosshair2": [236, 200, 96], "red": [[228, 111, 111], [200, 72, 72], [148, 0, 0]], "hud_green": [45, 129, 105]}
                   #"red": [[228, 111, 111], [214, 92, 92], [200, 72, 72], [184, 50, 50], [167, 26, 26], [148, 0, 0]]}
 
 
@@ -41,7 +41,19 @@ class Radar_Content(GameObject):
 class Blue_Tank(GameObject):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.rgb = [24, 80, 128]
+        self.rgb = [66, 136, 176]
+
+
+class Yellow_Blue_Tank(GameObject):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.rgb = [195, 144, 61]
+
+
+class Red_Thing(GameObject):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.rgb = [200, 72, 72]
 
 
 #  ---- HUD -----
@@ -89,8 +101,20 @@ def _detect_objects_battlezone(objects, obs, hud=False):
     blue = find_mc_objects(obs, [objects_colors["tank_blue1"], objects_colors["tank_blue2"],objects_colors["tank_grey"]], miny=78, maxy=175)
     for bb in blue:
             objects.append(Blue_Tank(*bb))
+
+    yellow = find_mc_objects(obs, [objects_colors["tank_blue1"], objects_colors["tank_yellow"],objects_colors["tank_grey2"]], miny=78, maxy=175)
+    for bb in yellow:
+            objects.append(Yellow_Blue_Tank(*bb))
     
     red = find_mc_objects(obs, objects_colors["red"], miny=78, maxy=175, closing_dist=5)
+    for bb in red:
+            objects.append(Red_Thing(*bb))
 
     if hud:
-         pass
+        score = find_objects(obs, objects_colors["hud_green"], closing_dist=4, miny=178, maxy=188)
+        for bb in score:
+                objects.append(Score(*bb))
+
+        lives = find_objects(obs, objects_colors["hud_green"], miny=188, closing_dist=1)
+        for bb in lives:
+                objects.append(Life(*bb))
