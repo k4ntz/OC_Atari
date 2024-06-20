0f0f3bddb052c683f487d8fcea45d14a9b4d150f
bluemlJ <bluemlJ@gmx.net>
2024-06-20 15:41:20 +0200

    enabling adding additional information like orientation to the obj representation

--- ocatari/ram/extract_ram_info.py
+++ ocatari/ram/extract_ram_info.py
@@ -58,6 +58,23 @@ def detect_objects_ram(objects, ram_state, game_name, hud):
         print(colored(f"_detect_objects_ram not implemented for game: {game_name}", "red"))
         raise err
 
+def get_object_state_size(game_name,hud):
+    p_module = __name__.split('.')[:-1] + [game_name.lower()]
+    game_module = '.'.join(p_module)
+
+    try:
+        mod = sys.modules[game_module]
+        return mod._get_object_state_size(hud)
+    except KeyError as err:
+        print(colored(f"Game module does not exist: {game_module}", "red"))
+        raise err
+    except AttributeError as err:
+        try:
+            return mod._get_max_objects(hud)
+        except AssertionError as err:
+            raise err
+    
+
 def get_object_state(reference_list, objects, game_name):
     p_module = __name__.split('.')[:-1] + [game_name.lower()]
     game_module = '.'.join(p_module)
@@ -73,7 +90,6 @@ def get_object_state(reference_list, objects, game_name):
         #print(colored(f"_get_object_state not implemented for game: {game_name}", "red"))
         #print(colored(f"Try Default get_object_state", "red"))
         try:
-            import ipdb;ipdb.set_trace()
             temp_ref_list = reference_list.copy()
             state = reference_list.copy()
             for o in objects: # populate out_vector with object instance
@@ -86,34 +102,5 @@ def get_object_state(reference_list, objects, game_name):
                 if d != "": #fill not populated category instances wiht 0.0's
                     state[i] = [0.0, 0.0, 0.0, 0.0]
             return state
-        except _ as err:
+        except AssertionError as err:
             raise err
-    
-# def get_object_state(reference_list, objects, game_name):
-#     p_module = __name__.split('.')[:-1] + [game_name.lower()]
-#     game_module = '.'.join(p_module)
-
-#     try:
-#         mod = sys.modules[game_module]
-#         state = mod._get_object_state(reference_list, objects)
-#         return state
-#     except KeyError as err:
-#         print(colored(f"Game module does not exist: {game_module}", "red"))
-#         raise err
-#     except AttributeError as err:
-#         #print(colored(f"_get_object_state not implemented for game: {game_name}", "red"))
-#         #print(colored(f"Try Default get_object_state", "red"))
-#         try:
-#             temp_ref_list = reference_list.copy()
-#             state = reference_list.copy()
-#             for o in objects: # populate out_vector with object instance
-#                 idx = temp_ref_list.index(o.category) #at position of first category occurance
-#                 flat = [item for sublist in o.h_coords for item in sublist]
-#                 state[idx] = flat #write the slice
-#                 temp_ref_list[idx] = "" #remove reference from reference list
-#             for i, d in enumerate(temp_ref_list):
-#                 if d != "": #fill not populated category instances wiht 0.0's
-#                     state[i] = [0.0, 0.0, 0.0, 0.0]
-#             return state
-#         except _ as err:
-#             raise err
