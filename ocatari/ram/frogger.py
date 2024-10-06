067e3921fe4921862cbd96479a92f2f6f92bf8fc
Elisabeth Dillies <elisabeth.dillies@gmail.com>
2024-10-06 11:56:18 +0200

    Continue Frogger's vision (cars)

--- ocatari/ram/frogger.py
+++ ocatari/ram/frogger.py
@@ -1,12 +1,12 @@
-from .game_objects import GameObject
+from .game_objects import GameObject, NoObject
 
 """
 RAM extraction for the game Frogger.
 
 """
 
-MAX_NB_OBJECTS = {'Frog': 1}
-MAX_NB_OBJECTS_HUD = {'Frog': 1}
+MAX_NB_OBJECTS = {'Frog': 1, 'Car': 12}
+MAX_NB_OBJECTS_HUD = {'Frog': 1, 'Car': 12}
 
 class Frog(GameObject):
     """
@@ -24,18 +24,18 @@ def _init_objects_ram(hud=False):
     """
     (Re)Initialize the objects
     """
-    return [Frog()]
+    return [Frog()] + [NoObject()] * 12
 
 def _detect_objects_ram(objects, ram_state, hud=False):
     """
     For all objects:
     (x, y, w, h, r, g, b)
     """
-    f = objects[0]
-    f.x = ram_state[48] - 1
+    frog = objects[0]
+    frog.x = ram_state[48] - 1
     if ram_state[44] == 255:
-        f.y = 171
+        frog.y = 171
     elif ram_state[44] == 5:
-        f.y = 95
+        frog.y = 95
     else:
-        f.y = - 13 * ram_state[44] + 161
+        frog.y = - 13 * ram_state[44] + 161
--- ocatari/vision/frogger.py
+++ ocatari/vision/frogger.py
@@ -1,16 +1,29 @@
-from .utils import find_objects
+from .utils import find_objects, match_objects
 from .game_objects import GameObject
 
 objects_colors = {"frog": [110, 156, 66]}
+
+car_colors = {"car1": [195, 144, 61], "car2": [164, 89, 208], "car3": [82, 126, 45],
+              "car4": [198, 89, 179], "car5": [236, 236, 236]}
 
 class Frog(GameObject):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.rgb = 110, 156, 66
 
-def _detect_objects(objects, obs, hud=False):
-    objects.clear()
+class Car(GameObject):
+    def __init__(self, x, y, w, h, rgb):
+        super().__init__(x, y, w, h)
+        self.rgb = rgb
 
-    frog = find_objects(obs, objects_colors["frog"], size=(7, 7), tol_s=2)
-    for el in frog:
-        objects.append(Frog(*el))
+def _detect_objects(objects, obs, hud=False):
+    # objects.clear()
+
+    frog = objects[0]
+    frog_bb = find_objects(obs, objects_colors["frog"], size=(7, 7), tol_s=2)[0]
+    frog.xywh = frog_bb
+
+    cars_bb, max_cars = [], 0
+    for color in car_colors.values():
+        cars_bb.extend([list(bb) + [color] for bb in find_objects(obs, color, minx=8, maxx=152, miny=104, maxy=170)])
+    match_objects(objects, cars_bb, 1, 12, Car)
