851681d5bf1bde8807a5dd7c5432b2f8ae3c5590
Elisabeth Dillies <elisabeth.dillies@gmail.com>
2024-10-30 16:25:40 +0100

    Work on PySR regression script

--- scripts/find_regression.py
+++ scripts/find_regression.py
@@ -4,6 +4,7 @@ Uses the vision detection of objects for the regression, performed with PySR.
 """
 
 import math
+import random
 import numpy as np
 import pandas as pd
 from os import makedirs
@@ -13,41 +14,51 @@ from ocatari.core import OCAtari
 from ocatari.utils import parser, make_deterministic, load_agent
 from ocatari.vision.utils import find_objects
 
-game = "PongDeterministic-v4"
-slots = {'enemy': [213, 130, 74], 'player': [92, 186, 92], 'ball': [236, 236, 236]} # objects to track and their color
-properties = []
-for object in slots.keys():
-    properties.extend([object + '_x', object + '_y', object + '_w', object + '_h'])
+GAME = "PongNoFrameskip-v4"
+SLOTS = {'enemy': [213, 130, 74], 'player': [92, 186, 92], 'ball': [236, 236, 236]} # objects to track and their color
+AGENT = "random" # "c51"
 
-def generate_dataset():
+def get_properties():
+    properties = []
+    for object in SLOTS.keys():
+        properties.extend([object + '_x', object + '_y', object + '_w', object + '_h'])
+    return properties
+
+def generate_dataset(nframes):
     MODE = "both"
     # RENDER_MODE = "rgb_array"
     RENDER_MODE = "human"
-    env = OCAtari(game, mode=MODE, render_mode=RENDER_MODE)
+    env = OCAtari(GAME, mode=MODE, render_mode=RENDER_MODE)
     # env = OCAtari(game, mode=MODE, render_mode=RENDER_MODE, frameskip=1)
     observation, info = env.reset()
 
-    agent = 'c51'
-    path = f"models/{game}/{agent}.gz" # game will be cleaned by load_agent
-    dqn_agent = load_agent(path, env.action_space.n)
+    if AGENT != "random":
+        path = f"models/{GAME}/{AGENT}.gz" # game will be cleaned by load_agent
+        dqn_agent = load_agent(path, env.action_space.n)
 
     make_deterministic(0, env)
 
     # skip first frames
     for _ in range(50):
-        action = dqn_agent.draw_action(env.dqn_obs)
+        if AGENT == "random":
+            action = random.randint(0, env.nb_actions-1)
+        else:
+            action = dqn_agent.draw_action(env.dqn_obs)
         _, _, _, _, _ = env.step(action)
 
-    N_FRAMES = 200
+    properties = get_properties()
     columns = properties + ['ram_states']
     data = pd.DataFrame(columns=columns)
                                     
-    for i in range(N_FRAMES):
-        action = dqn_agent.draw_action(env.dqn_obs)
+    for i in range(nframes):
+        if AGENT == "random":
+            action = random.randint(0, env.nb_actions-1)
+        else:
+            action = dqn_agent.draw_action(env.dqn_obs)
         obs, _, terminated, truncated, _ = env.step(action)
 
         row = []
-        for color in slots.values():
+        for color in SLOTS.values():
             obj = find_objects(obs, color, size=15, tol_s=15) # TODO: add a maxsize
             if obj == []:
                 row.extend([None] * 4)
@@ -60,11 +71,14 @@ def generate_dataset():
             break
 
     makedirs("data/datasets/", exist_ok=True)
-    filename = f"data/datasets/{game}_{agent}_ram_and_objects_{N_FRAMES}_frames.csv"
+    filename = f"data/datasets/{GAME}_{AGENT}_ram_and_objects_{nframes}_frames.csv"
     data.to_csv(filename)
 
-# generate_dataset()
-dataset = pd.read_csv('data/datasets/PongDeterministic-v4_c51_ram_and_objects_3000_frames.csv')
+def get_dataset(nframes):
+    # generate_dataset(nframes)
+    filename = f"data/datasets/{GAME}_{AGENT}_ram_and_objects_{nframes}_frames.csv"
+    dataset = pd.read_csv(filename)
+    return dataset
 
 def get_ram_states(dataset):
     ram_states_raw = dataset['ram_states']
@@ -85,27 +99,38 @@ def clean_data(ram_states, data):
             clean_data.append(val)
     return clean_ram, clean_data
 
-model = PySRRegressor(
-    niterations = 100,  # < Increase me for better results
-    binary_operators = ["+", "-", "max", "min", "mod", "cond", "greater"],
-    elementwise_loss = "loss(prediction, target) = (prediction - target)^2",
-    # ^ Custom loss function (julia syntax)
-    # constraints={
-    #     "*": {"constants": 1},  # Ensure one constant in multiplication, i.e., only integer factors
-    #     "+": {"constants": 1},  # Ensure one constant in addition, only integers
-    # }, # ^ Constraints to limit the constants to integers
-    # extra_sympy_mappings={
-    #     "IntConstant": lambda: np.random.randint(-50, 50),  # Random integers between -50 and 50
-    # }, # ^ Function to generate integer constants only
-)
+def get_model(eq_file=None):
+    model = PySRRegressor(
+        niterations = 150,  # < Increase me for better results
+        maxsize = 15,
+        binary_operators = ["+", "-", "*", "/", "mod", "cond", "greater"],
+        elementwise_loss = "loss(prediction, target) = (prediction - target)^2",
+        # ^ Custom loss function (julia syntax)
+        # constraints={
+        #     "*": {"constants": 1},  # Ensure one constant in multiplication, i.e., only integer factors
+        #     "+": {"constants": 1},  # Ensure one constant in addition, only integers
+        # }, # ^ Constraints to limit the constants to integers
+        # extra_sympy_mappings={
+        #     "IntConstant": lambda: np.random.randint(-50, 50),  # Random integers between -50 and 50
+        # }, # ^ Function to generate integer constants only
+        # equation_file = f'pysr/{eq_file}'
+    )
+    return model
 
-ram_states = get_ram_states(dataset)
-for prop in properties:
-    print("Property  :", prop)
-    prop_data = dataset[prop].to_numpy()
-    cl_ram, cl_data = clean_data(ram_states, prop_data)
-    model.fit(cl_ram, cl_data)
-    equations = model.get_hof()
-    print(equations)
-    print()
-    # print("Sympy equation:", model.sympy())
+def compute_properties_regression():
+    properties = get_properties()
+    dataset = get_dataset(200)
+    ram_states = get_ram_states(dataset)
+    model = get_model()
+    for prop in properties:
+        print("Property  :", prop)
+        prop_data = dataset[prop].to_numpy()
+        cl_ram, cl_data = clean_data(ram_states, prop_data)
+        model.fit(cl_ram, cl_data)
+        equations = model.get_hof()
+        print(equations)
+        print()
+        # print("Sympy equation:", model.sympy())
+
+# generate_dataset(2000)
+compute_properties_regression()
