9cc33d730b27884d9870a566b21968c187c3591d
BJenrajb <bj.nutzmail@gmail.com>
2024-11-19 15:56:24 +0100

    ns_impl for games
    fishingderby, mspacman, seaquest, breakout vision

--- ocatari/vision/kangaroo.py
+++ ocatari/vision/kangaroo.py
@@ -1,5 +1,5 @@
-from .utils import find_objects
-from .game_objects import GameObject
+from .utils import find_objects, match_objects
+from .game_objects import GameObject, NoObject
 
 objects_colors = {"kangaroo": [223, 183, 85], "bell": [210, 164, 74],
                   "fruit": {"red": [214, 92, 92], "yellow": [195, 144, 61]},
@@ -52,13 +52,13 @@ class Platform(GameObject):
         self.hud = False
 
 # The color of this object matches with the walls, floors and ladders, it can therefore not be detected Properly
-class Projectile_top(GameObject):
+class FallingCoconut(GameObject):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.rgb = 162, 98, 33
 
 
-class Projectile_enemy(GameObject):
+class ThrownCoconut(GameObject):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.rgb = 227, 159, 89
@@ -76,7 +76,7 @@ class Score(GameObject):
         self.rgb = 160, 171, 79
 
 
-class Life(GameObject):
+class Lives(GameObject):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.rgb = 160, 171, 79
@@ -88,28 +88,24 @@ class Time(GameObject):
         self.rgb = 160, 171, 79
 
 
-def _detect_objects(objects, obs, hud=True):
-    objects.clear()
+def _detect_objects(objects, obs, hud=False):
 
     player = find_objects(obs, objects_colors["kangaroo"], min_distance=1, miny=28)
-    for bb in player:
-        objects.append(Player(*bb))
+    if player:
+        objects[0].xywh = player[0]
 
     child = find_objects(obs, objects_colors["kangaroo"], min_distance=1, maxy=27)
-    for bb in child:
-        objects.append(Child(*bb))
+    if child:
+        objects[1].xywh = child[0]
 
+    fruit = []
     for i in objects_colors["fruit"]:
-        fruit = find_objects(obs, objects_colors["fruit"][i], min_distance=1)
-        for bb in fruit:
-            if bb[1] < 170:
-                fru = Fruit(*bb)
-                fru.rgb = objects_colors["fruit"][i]
-                objects.append(fru)
+        fruit.extend(find_objects(obs, objects_colors["fruit"][i], min_distance=1))
+    match_objects(objects, fruit, 10, 3, Fruit)
 
     bell = find_objects(obs, objects_colors["bell"], min_distance=1)
-    for bb in bell:
-        objects.append(Bell(*bb))
+    if bell:
+        objects[13].xywh = bell[0]
 
     # In this game both the enemy and their Projectile have the same color.
     # Both of them can be positioned at varios spots in the level,
@@ -117,62 +113,41 @@ def _detect_objects(objects, obs, hud=True):
     enemy = find_objects(obs, objects_colors["enemy"], min_distance=1)
 
     for bb in enemy:
-        if bb[2] > 5:
-            en = Enemy(*bb)
-            objects.append(en)
+        if bb[2] <= 5:
+            enemy.remove(bb)
+    match_objects(objects, enemy, 2, 4, Enemy)
 
     p_enemy = find_objects(obs, objects_colors["projectile_enemy"], min_distance=1, size=(2, 3), tol_s=2)
 
     for bb in p_enemy:
-        if bb[2] < 5:
-            en = Projectile_enemy(*bb)
-            objects.append(en)
+        if bb[2] >= 5:
+            p_enemy.remove(bb)
+    match_objects(objects, p_enemy, 7, 3, ThrownCoconut)
 
     proj = find_objects(obs, objects_colors["projectile_top"], min_distance=1, minx=16, maxx=140, miny=3, maxy=27,
                         size=(2, 3), tol_s=2)
+    proj.extend(find_objects(obs, objects_colors["projectile_top"], min_distance=1, minx=16, maxx=140, miny=35, maxy=70,
+                        size=(2, 3), tol_s=2))
+    proj.extend(find_objects(obs, objects_colors["projectile_top"], min_distance=1, minx=16, maxx=140, miny=80, maxy=123,
+                        size=(2, 3), tol_s=2))
+    proj.extend(find_objects(obs, objects_colors["projectile_top"], min_distance=1, minx=16, maxx=140, miny=128, maxy=171,
+                        size=(2, 3), tol_s=2))
 
-    for bb in proj:
-        p = Projectile_top(*bb)
-        objects.append(p)
 
-    proj = find_objects(obs, objects_colors["projectile_top"], min_distance=1, minx=16, maxx=140, miny=35, maxy=70,
-                        size=(2, 3), tol_s=2)
-
-    for bb in proj:
-        p = Projectile_top(*bb)
-        objects.append(p)
-
-    proj = find_objects(obs, objects_colors["projectile_top"], min_distance=1, minx=16, maxx=140, miny=80, maxy=123,
-                        size=(2, 3), tol_s=2)
-
-    for bb in proj:
-        p = Projectile_top(*bb)
-        objects.append(p)
-
-    proj = find_objects(obs, objects_colors["projectile_top"], min_distance=1, minx=16, maxx=140, miny=128, maxy=171,
-                        size=(2, 3), tol_s=2)
-
-    for bb in proj:
-        p = Projectile_top(*bb)
-        objects.append(p)
-    
-    objects.append(Platform(16, 172, w=128))  # base platform
-    objects.append(Platform(16, 28, w=128))  # top platform
-    objects.append(Ladder(132, 132))
-    objects.append(Platform(16, 76, w=128))
-    objects.append(Ladder(20, 85))
-    objects.append(Platform(16, 124, w=128))
-    objects.append(Ladder(132, 37))
+    if proj:
+        if type(objects[6]) is NoObject:
+            objects[6] = FallingCoconut(*proj[0])
+        else:
+            objects[6].xywh = proj[0]
+    else:
+        objects[6] = NoObject()
 
     if hud:
-        life = find_objects(obs, objects_colors["hud"], min_distance=1, minx=10, maxx=40)
-        for bb in life:
-            objects.append(Life(*bb))
+        life = find_objects(obs, objects_colors["hud"], closing_dist=8, minx=10, maxx=40)
+        objects[-1].xywh = life[0]
 
         time = find_objects(obs, objects_colors["hud"], min_distance=1, minx=70, maxx=100)
-        for bb in time:
-            objects.append(Time(*bb))
+        objects[-2].xywh = time[0]
 
         score = find_objects(obs, objects_colors["hud"], closing_dist=6, minx=100, maxx=150)
-        for bb in score:
-            objects.append(Score(*bb))
+        objects[-3].xywh = score[0]
