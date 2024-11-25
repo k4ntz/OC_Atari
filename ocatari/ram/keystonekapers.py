39cdbd3cc5d228bce31e6d3e95b577403216eb83
melikaslz <mel.salim@yahoo.com>
2024-11-25 14:20:46 +0100

    Finishing Keystone Kapers

--- ocatari/ram/keystonekapers.py
+++ ocatari/ram/keystonekapers.py
@@ -5,21 +5,17 @@ RAM extraction for the game Keystone Kapers.
 
 """
 
-MAX_NB_OBJECTS = {'Kop': 1, 'Krook': 1, 'Ball': 1, 'Moneybag': 2, 'Suitcase':2, 'Elevator':3, 'Escalator':2, 'SecuritySystem':1,'Radio': 4, 'Cart': 3, 'Biplane': 2}
-MAX_NB_OBJECTS_HUD = {'Kop': 1, 'Krook': 1, 'Ball':1, 'Moneybag': 2, 'Suitcase':2,'Elevator':3, 'Escalator':2, 'SecuritySystem':1 ,'Radio':4, 'Cart': 3, 'Biplane': 2}
+MAX_NB_OBJECTS = {'Kop': 1, 'Krook': 1, 'Ball': 1, 'Moneybag': 2, 'Suitcase':2, 'Elevator':3, 'Escalator':2, 'SecuritySystem':1,'Radio': 4, 'Cart': 4, 'Biplane': 2}
+MAX_NB_OBJECTS_HUD = {'Kop': 1, 'Krook': 1, 'Ball':1, 'Moneybag': 2, 'Suitcase':2,'Elevator':3, 'Escalator':2, 'SecuritySystem':1 ,'Radio':4, 'Cart': 4, 'Biplane': 2, 'BonusKops': 1, 'Score':1, 'Timer':1 }
 
 class Kop(GameObject):
     """
     The player figure i.e., the Kop. 
-    """
-    
+    """  
     def __init__(self):
         super().__init__()
         self.rgb = 220,175,111
         self.hud = False
-        # self.wh = 6,20
-        # self.y-=5
-        # self.h+=5
 
 class Krook(GameObject):
     """
@@ -29,10 +25,6 @@ class Krook(GameObject):
         super().__init__()
         self.rgb = 220,175,11
         self.hud = False
-        # self._xy = 0, 0
-        # self.wh = 7,20
-        # self.y-=1
-        # self.h+=1
 
 
 class Ball(GameObject):
@@ -83,8 +75,26 @@ class Biplane(GameObject):
      def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.rgb = 238,209,128
-        self.h+=4
-        self.y-=1
+        
+#HUD:
+class BonusKops(GameObject):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.rgb = 0, 0, 0
+        self.hud = True
+        
+class Score(GameObject):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.rgb = 236,236,236
+        self.hud = True
+
+class Timer(GameObject):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.rgb = 0, 0, 0
+        #self.xywh = (55, 43, 14, 7)
+        self.hud = True
         
 def _init_objects_ram(hud=False):
     """
@@ -95,9 +105,9 @@ def _init_objects_ram(hud=False):
     elevators = [NoObject()]*3
     escalators = [NoObject()]*2
     radios = [NoObject()]*4
-    carts = [NoObject()]*3
+    carts = [NoObject()]*4
     biplanes = [NoObject()]*2
-    return [Kop()]+ [Krook()]+ [Ball()] + bags + suitcases + elevators + escalators + [SecuritySystem()]+ radios + carts + biplanes
+    return [Kop()]+ [Krook()]+ [Ball()] + bags + suitcases + elevators + escalators + [SecuritySystem()]+ radios + carts + biplanes+ [BonusKops()] +[Score()] + [Timer()] 
 
 def _detect_objects_ram(objects, ram_state, hud=False):
     pass
--- ocatari/vision/keystonekapers.py
+++ ocatari/vision/keystonekapers.py
@@ -2,19 +2,23 @@ from .utils import find_objects, find_mc_objects, find_objects_in_color_range, m
 from .game_objects import GameObject,  NoObject
 
 objects_colors = {
-                "Kop": [[220,175,111],[25,63,137]], "Krook": [[220,175,111],[43,43,43]], "Ball": [[137,26,53],[160,50,82],[182,72,110]],
-                "Security System": [[50,152,82],[207,175,92],[171,135,50]], "Elevator":[[72,164,164],[52,0,128]] ,
-                "Moneybag":[[128,88,0],[171,135,50]], "Suitcase":[[128,88,0]], "Escalator":[52,0,128],"Shopping carts":[[210,210,210],[0,0,0]],
-                "Biplane":[238,209,128],"Radio":[[[52,0,128],[236,236,236]],[[210,210,210],[52,0,128]]],
-                "Timer":[[0,0,0],[236,236,236]], "Score":[236,236,236], "Bonus Kops":[0,0,0]
+                "Kop": [[220,175,111],[25,63,137]],
+                "Krook": [[220,175,111],[43,43,43]],
+                "Ball": [[137,26,53],[160,50,82],[182,72,110]],
+                "Security System": [[50,152,82],[207,175,92],[171,135,50]],
+                "Elevator":[[72,164,164],[52,0,128]] ,
+                "Moneybag":[[128,88,0],[171,135,50]],
+                "Suitcase":[[128,88,0]],
+                "Escalator":[52,0,128],
+                "Shopping carts":[[210,210,210],[0,0,0]],
+                "Biplane":[238,209,128],
+                "Radio":[[[52,0,128],[236,236,236]],[[210,210,210],[52,0,128]]],
+                "Timer":[0,0,0],
+                "Score":[236,236,236], "Bonus Kops":[0,0,0]
                     }
-# objects_colors = {
-#                 "Kop": [[220,175,111],[25,63,137]], "Ball": [[137,26,53],[160,50,82],[182,72,110]],"Krook": [[220,175,111],[43,43,43]],
-#                 "Security System": [[0,0,0],[50,152,82],[207,175,92],[171,135,50],[236,236,236]], "Elevator":[[72,164,164],[52,0,128]] ,"Moneybag":[[128,88,0],[171,135,50]],
-#                 "Suitcase":[[128,88,0]], "Escalator":[52,0,128],"Shopping carts":[[210,210,210],[0,0,0]],"Biplane":[238,209,128],"Radio":[[[52,0,128],[236,236,236]],[[210,210,210],[52,0,128]]],
-#                 "Timer":[[0,0,0],[236,236,236]], "Score":[236,236,236], "Bonus Kops":[0,0,0]
-#                     }
+
 floors =[[91,116], [123,148],[155,180]]
+max_carts = [1,1,1,1]
 elevators_per_line = [1,1,1]
 class Kop(GameObject):
     def __init__(self, *args, **kwargs):
@@ -102,13 +106,14 @@ def _detect_objects(objects, obs, hud=False):
     # detection and filtering
     #objects.clear()
     
+    #Player(Kop)
     player = objects[0]
     player_bb = find_mc_objects(obs, objects_colors["Kop"], size=(6, 20), tol_s =9)
     if player_bb:
         player.xywh = tuple(a + b for a, b in zip(player_bb[0], (0,-5,0,5)))
-    
     start_idx = 1
     
+    #Thief(Krook)
     thief_bb = find_mc_objects(obs, objects_colors["Krook"], size =(7, 20), closing_dist= 2)
     if thief_bb:  
         thief_bb[0] = tuple(a + b for a, b in zip(thief_bb[0], (0,-1,0,1)))
@@ -116,13 +121,14 @@ def _detect_objects(objects, obs, hud=False):
     else:
         objects[start_idx] = NoObject()
     #match_objects(objects, thief_bb, start_idx, 1, Krook)
-    
     start_idx+=1
-
+    
+    #Balls
     balls_bb = [bb for bb in find_mc_objects(obs, objects_colors["Ball"], size= (6,6), tol_s = 1)]
     match_objects(objects, balls_bb, start_idx, 1, Ball) 
     start_idx+=1
     
+    #Money bags
     bags_bb = [bb for bb in find_mc_objects(obs,objects_colors["Moneybag"], closing_dist=8, size=(7,12), tol_s=3)]
     if bags_bb:
         for i in range(len(bags_bb)):
@@ -130,23 +136,15 @@ def _detect_objects(objects, obs, hud=False):
     match_objects(objects, bags_bb, start_idx, 2, Moneybag) 
     start_idx+=2
     
+    #Suitcases
     suitcases_bb = [bb for bb in find_mc_objects(obs,objects_colors["Suitcase"], size=(8,1),tol_s=0)]
     if suitcases_bb:
         for i in range(len(suitcases_bb)):
             suitcases_bb[i] = tuple(a + b for a, b in zip(suitcases_bb[i], (0,-9,0,9)))
     match_objects(objects, suitcases_bb, start_idx, 2, Suitcase) 
-    start_idx+=1
-    
-    # #Issue: Open property?
-    # for (miny, maxy) in floors:
-    #     elv_bb = []
-    #     for color in objects_colors["Elevator"]:
-    #         elv_bb += [list(bb) + [color] for bb in find_objects(obs,color, size=(8,23), tol_s = 2, miny= miny, maxy= maxy)]
-    #     match_objects(objects, elv_bb, start_idx, 1, Elevator)        
-    #     start_idx += 1
-    #     print(elv_bb)
-    
-    #with open property
+    start_idx+=2
+
+    #Elevators (Open and Close)
     for (min_y,max_y) in floors:
         elvs_bb=[]
         for color in objects_colors["Elevator"]:
@@ -161,10 +159,12 @@ def _detect_objects(objects, obs, hud=False):
             objects[start_idx]=NoObject()
         start_idx+=1
     
+    #Escalators
     escalators_bb = [bb for bb in find_objects(obs,objects_colors["Escalator"], closing_dist=8, size =(47,40) , tol_s = 0)]
     match_objects(objects, escalators_bb,start_idx, 2, Escalator)
     start_idx+=2
 
+    #Security System
     security_bb = find_mc_objects(obs,objects_colors["Security System"], size = (80, 13), tol_s = 2)
     if security_bb:
         objects[start_idx] = SecuritySystem(*security_bb[0])
@@ -172,47 +172,40 @@ def _detect_objects(objects, obs, hud=False):
         objects[start_idx] = NoObject()
     start_idx+=1
     
+    #Radios
     radios_bb = []
     for color in objects_colors["Radio"]:    
         radios_bb += find_mc_objects(obs,color,size =(8,12), tol_s=2, closing_dist=4)
     match_objects(objects, radios_bb,start_idx, 4, Radio)
     start_idx +=4
     
-    #Why cart does not fix
-    shoppingcarts = find_mc_objects(obs,objects_colors["Shopping carts"], size = (8,10), tol_s = 0)
-    for el in shoppingcarts:
-            objects.append(Cart(*el))
-    start_idx+=3
-    
-    biplanes_bb = find_objects(obs,objects_colors["Biplane"], size =(8,5), tol_s=1)
+    #Shopping Carts
+    carts_bb = find_mc_objects(obs,objects_colors["Shopping carts"], size = (8,10), tol_s = 0, closing_active= False)
+    match_objects(objects, carts_bb,start_idx, 4, Cart)
+    start_idx+=4
+
+    #Biplanes
+    biplanes_bb = find_objects(obs,objects_colors["Biplane"], size =(8,5), tol_s=1, closing_active= False)
     if biplanes_bb:
         for i in range(len(biplanes_bb)):
             biplanes_bb[i] = tuple(a + b for a, b in zip(biplanes_bb[i], (0,-1,0,4)))
     match_objects(objects, biplanes_bb, start_idx, 2, Biplane) 
     start_idx+=2
-            
     
-'''      
-    shoppingcarts = find_mc_objects(obs,objects_colors["Shopping carts"], size = (8,10), tol_s = 0)
-    for el in shoppingcarts:
-            objects.append(Cart(*el))
-            
-            
-    for color in objects_colors["Radio"]:    
-        rads = find_mc_objects(obs,color,size =(8,12), tol_s=2, closing_dist=4)
-        for el in rads:
-                objects.append(Radio(*el))
+    #Hud
     if hud:
-        score = find_objects(obs,objects_colors["Score"], maxy = 40, maxx=70, closing_dist=5)
-        for el in score:
-            objects.append(Score(*el))
-        bonus = find_objects(obs,objects_colors["Bonus Kops"],minx=10, maxx = 50, miny = 40, maxy = 50)
-        for el in bonus:
-            objects.append(BonusKops(*el))
-        for color in objects_colors["Timer"]:
-            timer = find_objects(obs,color,minx=50, maxx = 80, miny = 40, maxy = 50, closing_dist=5)
-            for el in timer:
-                objects.append(Timer(*el))
-        
-    
-    '''
+        #Score
+        score_bb = find_objects(obs,objects_colors["Score"], maxy = 40, maxx=70, closing_dist=5)
+        objects[start_idx] = Score(*score_bb[0]) if score_bb else NoObject()
+        start_idx+=1
+
+        #Bonus Kops
+        bonus_bb = find_objects(obs,objects_colors["Bonus Kops"],minx=10, maxx = 50, miny = 40, maxy = 50)
+        if bonus_bb:
+            objects[start_idx] = BonusKops(*bonus_bb[0])
+        start_idx+=1
+
+        timer_bb = find_objects(obs,objects_colors["Timer"],minx=50, maxx = 75, miny = 40, maxy = 51, closing_dist=5)
+        if timer_bb:
+            objects[start_idx] = Timer(*timer_bb[0]) 
+        start_idx+=1
