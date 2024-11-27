3cc0502778ba2a0f8fe6592ed5be06c4808685a5
BJenrajb <bj.nutzmail@gmail.com>
2024-11-27 16:55:07 +0100

    enduro progress

--- ocatari/ram/donkeykong.py
+++ ocatari/ram/donkeykong.py
@@ -171,8 +171,8 @@ def _get_max_objects(hud=False):
         return objects
 
     if hud:
-        return fromdict(MAX_ALL_OBJECTS)
-    return fromdict(MAX_ESSENTIAL_OBJECTS)
+        return fromdict(MAX_NB_OBJECTS_HUD)
+    return fromdict(MAX_NB_OBJECTS)
 
 
 # def _init_all_objects(hud=False):
@@ -191,8 +191,7 @@ def _init_objects_ram(hud=True):
     (Re)Initialize the objects
     """
     # objects[Time][0] = Time()
-    objects = [Player(), DonkeyKong(), Girlfriend(), Hammer()] + [None] * 4 + \
-        [Ladder(*xy) for xy in ladders]
+    objects = [Player(), DonkeyKong(), Girlfriend(), Hammer()] + [None] * 4 + [Ladder(*xy) for xy in ladders]
     if hud:
         objects.extend([Life(), Score()])
     return objects
