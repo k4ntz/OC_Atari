0cb8bba726d6491f88f49535291d2df867ea530e
Quentin Delfosse <q.delfosse@gmail.com>
2024-11-27 23:33:09 +0100

    Fixed Bowling

--- ocatari/core.py
+++ ocatari/core.py
@@ -67,7 +67,7 @@ OBJv2_SUPPORTED = [
 ]
 
 # Constant to control the upscaling factor for rendering
-UPSCALE_FACTOR = 3
+UPSCALE_FACTOR = 5
 
 
 # The OCAtari environment provides an interface to interact with Atari 2600 games through Gymnasium, enabling object tracking and analysis. This environment extends the functionality of traditional Atari environments by incorporating different object detection modes (RAM, vision, or both) and supports enhanced observation spaces for advanced tasks like reinforcement learning.  
