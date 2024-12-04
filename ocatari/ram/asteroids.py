3133bfe01be47a8716127e3e14cbfa143d7097d8
bluemlJ <bluemlJ@gmx.net>
2024-12-04 10:28:01 +0100

    repair ci.yml

--- ocatari/ram/asteroids.py
+++ ocatari/ram/asteroids.py
@@ -191,7 +191,6 @@ def _detect_objects_ram(objects, ram_state, hud=False):
         else:
             objects[0] = NoObjectPlayer()
         
-
     def update_asteroids():
         ast_list = [3, 4, 5, 6, 7, 8, 9, 12, 13, 14, 15, 16, 17, 18, 19]
         ast_bb = []
