64d9d4e40273f5274996f6fd997f55e53938934e
bluemlJ <bluemlJ@gmx.net>
2024-12-16 11:12:23 +0100

    repair ciruclar imports

--- ocatari/vision/__init__.py
+++ ocatari/vision/__init__.py
@@ -11,3 +11,6 @@ modules = [mod for mod in modules if not "_old" in mod]
 
 __all__ = [basename(f)[:-3] for f in modules if isfile(f)
            and not f.endswith('__init__.py')]
+
+for module in __all__:
+    __import__(f"{__name__}.{module}")
