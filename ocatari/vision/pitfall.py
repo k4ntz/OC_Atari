6a28fd36417481ceaf9c0fed58eb45a70e079978
LWcoder02 <larsw02@gmail.com>
2025-01-28 17:36:05 +0100

    Added first vision implementation for pitfall, not fully tested

--- ocatari/vision/pitfall.py
+++ ocatari/vision/pitfall.py
@@ -1,5 +1,5 @@
-from .utils import find_objects, find_mc_objects, find_rope_segments
-from .game_objects import GameObject
+from .utils import find_objects, find_mc_objects, find_rope_segments, match_objects
+from .game_objects import GameObject, NoObject
 import numpy as np
 import matplotlib as plt
 
@@ -45,6 +45,18 @@ class StairPit(GameObject):
         super().__init__(*args, **kwargs)
         self.rgb = 0, 0, 0
         self.hud = False
+
+
+class Stair(GameObject):
+    """
+    The escape shafts from the underground.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.rgb = 134, 134, 29
+        self.hud = False
+
 
 
 class Pit(GameObject):
@@ -131,6 +143,21 @@ class DiamondRing(GameObject):
         self.hud = False
 
 
+class Platform(GameObject):
+    """
+    Permanent platforms.
+    """
+    
+    def __init__(self, *args, **kwargs):
+        super(Platform, self).__init__(*args, **kwargs)
+        # x=0, y=0, w=8, h=4, 
+        # self._xy = x, y
+        # self._prev_xy = x, y
+        # self.wh = w, h
+        self.rgb = 167, 26, 26
+        self.hud = False
+
+
 class LifeCount(GameObject):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -154,30 +181,54 @@ class Timer(GameObject):
 
 def _detect_objects(objects, obs, hud=False):
     # detection and filtering
-    objects.clear()
+    # objects.clear()
     # Rope and wall is not working
+    player = objects[0]
 
-    player = find_mc_objects(obs, playercolors, size=(7, 20), tol_s=4)
-    if player:
-        objects.append(Player(*player[0]))
+    player_bb = find_mc_objects(obs, playercolors, size=(7, 20), tol_s=4)
+    # player_rope_bb = find_mc_objects(obs, playercolors_on_rope, size=(7, 20), tol_s=4)
+    # print(player_rope_bb)
+    if player_bb:
+        player.xywh = player_bb[0]
+    # elif player_rope_bb:
+    #     player.xywh = player_rope_bb[0]
 
-    wall = find_mc_objects(obs, wallcolors, size=(
+
+    wall = objects[1]
+    wall_bb = find_mc_objects(obs, wallcolors, size=(
         7, 35), tol_s=5, closing_dist=5)  # ,miny=140,maxy=190,
-    for w in wall:
-        objects.append(Wall(*w))
-    logs = find_objects(obs, objects_colors["logs"], size=(
-        6, 14), tol_s=2, maxy=132, miny=114)
-    for l in logs:
-        objects.append(Logs(*l))
-    sp = find_objects(obs, objects_colors["smallpit"], size=(
-        8, 6), tol_s=2, maxy=130, miny=114)
-    for s in sp:
-        objects.append(StairPit(*s))
+    if wall_bb:
+        if not wall:
+            wall = Wall(*wall_bb[0])
+            objects[1] = wall
+        wall.xywh = wall_bb[0]
+    else:
+        objects[1] = NoObject()
+    # for w in wall_bb:
+    #     objects.append(Wall(*w))
 
-    sc = find_objects(obs, objects_colors["scorpion"], size=(
+    logs_bb = find_objects(obs, objects_colors["logs"],
+                           size=(6, 14), tol_s=2, maxy=132, miny=114)
+    match_objects(objects, logs_bb, 2, 3, Logs)
+
+
+    # for l in logs:
+    #     objects.append(Logs(*l))
+
+    
+    sp_bb = find_objects(obs, objects_colors["smallpit"], size=(
+        8, 6), tol_s=1, maxy=130, miny=114)
+    
+    match_objects(objects, sp_bb, 5, 1, StairPit)
+    # for s in sp:
+    #     objects.append(StairPit(*s))
+
+
+    sc_bb = find_objects(obs, objects_colors["scorpion"], size=(
         7, 10), tol_s=3, maxy=178, miny=160)
-    for s in sc:
-        objects.append(Scorpion(*s))
+    match_objects(objects, sc_bb, 9, 1, Scorpion)
+    # for s in sc:
+    #     objects.append(Scorpion(*s))
 
     rope_slices = find_rope_segments(obs, objects_colors["rope"], seg_height=(
         1, 45), maxy=120, miny=70, minx=40, maxx=112)
@@ -186,58 +237,89 @@ def _detect_objects(objects, obs, hud=False):
         if lowest[3] > 1:
             lowest[1] = lowest[1]+lowest[3]-1
             lowest[3] = 1
-        objects.append(Rope(*lowest))
+        lowest = [tuple(lowest)]
+        match_objects(objects, lowest, 10, 1, Rope)
+    # if rope_slices:
+    #     lowest = max(rope_slices, key=lambda x: x[1])
+    #     if lowest[3] > 1:
+    #         lowest[1] = lowest[1]+lowest[3]-1
+    #         lowest[3] = 1
+    #     objects.append(Rope(*lowest))
+       
 
-    snake = find_mc_objects(obs, snakecolors, size=(
-        8, 14), tol_s=2, maxy=132, miny=114)
-    for s in snake:
-        objects.append(Snake(*s))
+    snake_bb = find_mc_objects(obs, snakecolors, size=(
+        8, 14), tol_s=5, maxy=132, miny=114)
+    match_objects(objects, snake_bb, 11, 1, Snake)
+    # for s in snake:
+    #     objects.append(Snake(*s))
 
-    tp = find_objects(obs, objects_colors["smallpit"], size=(
+    
+    tp_bb = find_objects(obs, objects_colors["smallpit"], size=(
         64, 10), tol_s=10, maxy=130, miny=114)  # same color as small pit
-    for s in tp:
-        objects.append(Tarpit(*s))
+    match_objects(objects, tp_bb, 12, 1, Tarpit)
+    # for s in tp:
+    #     objects.append(Tarpit(*s))
 
-    pp = find_objects(obs, objects_colors["smallpit"], size=(
+    
+    pp_bb = find_objects(obs, objects_colors["smallpit"], size=(
         12, 6), tol_s=1, maxy=130, miny=114)  # same color as small pit
-    for p in pp:
-        objects.append(Pit(*p))
+    match_objects(objects, pp_bb, 7, 2, Pit)
+    # for p in pp:
+    #     objects.append(Pit(*p))
 
-    wh = find_objects(obs, objects_colors["waterhole"], size=(
+    
+    wh_bb = find_objects(obs, objects_colors["waterhole"], size=(
         64, 10), tol_s=10, maxy=130, miny=114)
-    for w in wh:
-        objects.append(Waterhole(*w))
+    match_objects(objects, wh_bb, 12, 1, Waterhole)
+    # for w in wh:
+    #     objects.append(Waterhole(*w))
 
-    gold = find_mc_objects(obs, goldenbarcolors, size=(
+    gold_bb = find_mc_objects(obs, goldenbarcolors, size=(
         7, 13), tol_s=3, maxy=132, miny=114, closing_dist=4)
-    for g in gold:
-        objects.append(GoldenBar(*g))
+    match_objects(objects, gold_bb, 20, 1, GoldenBar)
+    # for g in gold:
+    #     objects.append(GoldenBar(*g))
 
-    sc = find_objects(obs, objects_colors["crocodile"], size=(
+    sc_bb = find_objects(obs, objects_colors["crocodile"], size=(
         8, 8), tol_s=2, maxy=132, miny=114)
-    for c in sc:
-        objects.append(Crocodile(*c))
+    match_objects(objects, sc_bb, 13, 3, Crocodile)
+    # for c in sc:
+    #     objects.append(Crocodile(*c))
 
-    fire = find_mc_objects(obs, firecolors, size=(
+    fire_bb = find_mc_objects(obs, firecolors, size=(
         8, 14), tol_s=3, maxy=132, miny=114, closing_dist=4)
-    for f in fire:
-        objects.append(Fire(*f))
+    match_objects(objects, fire_bb, 17, 1, Fire)
+    # for f in fire:
+    #     objects.append(Fire(*f))
 
-    mb = find_mc_objects(obs, moneybagcolors, size=(
+    mb_bb = find_mc_objects(obs, moneybagcolors, size=(
         7, 14), tol_s=3, maxy=132, miny=114, closing_dist=4)
-    for b in mb:
-        objects.append(MoneyBag(*b))
+    match_objects(objects, mb_bb, 18, 1, MoneyBag)
+    # for b in mb:
+    #     objects.append(MoneyBag(*b))
 
-    gold = find_mc_objects(obs, silverbarcolors, size=(
+    gold_bb = find_mc_objects(obs, silverbarcolors, size=(
         7, 13), tol_s=3, maxy=132, miny=114, closing_dist=4)
-    for g in gold:
-        objects.append(SilverBar(*g))
+    match_objects(objects, gold_bb, 19, 1, SilverBar)
+    # for g in gold:
+    #     objects.append(SilverBar(*g))
 
-    gold = find_mc_objects(obs, diamondringcolors, size=(
+    gold_bb = find_mc_objects(obs, diamondringcolors, size=(
         7, 13), tol_s=3, maxy=132, miny=114, closing_dist=4)
-    for g in gold:
-        objects.append(DiamondRing(*g))
+    match_objects(objects, gold_bb, 20, 1, DiamondRing)
+    # for g in gold:
+    #     objects.append(DiamondRing(*g))
 
+    if hud:
+        lc = find_objects(obs, objects_colors['hud_objs'], size=(1, 8), tol_s=1, maxy=30, miny=20, minx=15, maxx=26, closing_active=False)
+        match_objects(objects, lc, 23, 3, LifeCount)
+
+        ps = find_objects(obs, objects_colors["hud_objs"], maxy=18, miny=7, minx=16, maxx=70, closing_dist=8)
+        match_objects(objects, ps, 26, 2, PlayerScore)
+
+        ts = find_objects(obs, objects_colors["hud_objs"], maxy=30, miny=20, minx=28, maxx=69, closing_dist=8)
+        match_objects(objects, ps, 28, 1, Timer)
+    """
     if hud:
         lc = find_objects(obs, objects_colors["hud_objs"], size=(1, 8), tol_s=1, maxy=30, miny=20, minx=15, maxx=26,
                           closing_active=False)
@@ -251,3 +333,5 @@ def _detect_objects(objects, obs, hud=False):
             obs, objects_colors["hud_objs"], maxy=30, miny=20, minx=28, maxx=69, closing_dist=8)
         for t in ts:
             objects.append(Timer(*t))
+
+        """
