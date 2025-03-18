b662f9cd51b99f57258a2f6d2656e42a4e4f7f7f
BJenrajb <bj.nutzmail@gmail.com>
2025-03-18 18:10:31 +0100

    Robotank ram+ vision full implementation

--- ocatari/ram/robotank.py
+++ ocatari/ram/robotank.py
@@ -3,15 +3,16 @@ from .utils import match_objects
 from ._helper_methods import _convert_number
 import sys
 import numpy as np
+import math
 
 """
 RAM extraction for the game Road Runner.
 """
 
-MAX_NB_OBJECTS = {"Player_Crosshair": 1, "PlayerShot": 1, "EnemyTank": 1, "EnemyShot": 1, 
-                  "VisionSensor": 1, "RadarSensor": 1, "CanonSensor": 1, "TreadSensor": 1,
+MAX_NB_OBJECTS = {"Player_Crosshair": 1, "PlayerShot": 1, "EnemyTank": 1, "EnemyShot": 1, "EnemyRadarPosition": 1, 
+                  "VisionSensor": 1, "RadarSensor": 1, "CanonSensor": 1, "TreadSensor": 1
                   }
-MAX_NB_OBJECTS_HUD = {"Player_Crosshair": 1, "PlayerShot": 1, "EnemyTank": 1, "EnemyShot": 1, 
+MAX_NB_OBJECTS_HUD = {"Player_Crosshair": 1, "PlayerShot": 1, "EnemyTank": 1, "EnemyShot": 1, "EnemyRadarPosition": 1,
                       "VisionSensor": 1, "RadarSensor": 1, "CanonSensor": 1, "TreadSensor": 1,
                       "Score": 1, "Lives": 1, "Clock": 1}
 
@@ -65,6 +66,19 @@ class EnemyShot(GameObject):
         self._xy = 0, 0
         self.wh = (16, 11)
         self.rgb = 1, 1, 1
+        self.hud = False
+
+
+class EnemyRadarPosition(GameObject):
+    """
+    Opposing tanks position on the radar.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self._xy = 0, 0
+        self.wh = (1, 1)
+        self.rgb = 146, 70, 192
         self.hud = False
 
 class Sensor(GameObject):
@@ -163,6 +177,13 @@ class Clock(ValueObject):
         self.hud = True
         self.value = 0
 
+def degree_to_xy(degree, radius=10):
+    
+    angle = 0.017453292519943295 * degree
+
+    return (radius*math.cos(angle), radius*math.sin(angle))
+
+
 # parses MAX_NB* dicts, returns default init list of objects
 def _get_max_objects(hud=False):
 
@@ -185,7 +206,7 @@ def _init_objects_ram(hud=False):
     """
     objects = [Player_Crosshair()]
     objects.extend([NoObject()] * 3)
-    objects.extend([VisionSensor(), RadarSensor(), CanonSensor(), TreadSensor()])
+    objects.extend([EnemyRadarPosition(), VisionSensor(), RadarSensor(), CanonSensor(), TreadSensor()])
     if hud:
         objects.extend([NoObject(), Lives(), Clock()])
     return objects
@@ -206,73 +227,98 @@ def _detect_objects_ram(objects, ram_state, hud=False):
 
     objects[0].xy = 72, 82 + offset
 
-    # base y == 125
-    if ram_state[64] > 4:
-        if type(objects[1]) is NoObject:
-            objects[1] = PlayerShot()
-        size = ram_state[64] - 6
-        objects[1].xy = 77 - (size>>1), 101 + size*5 + ram_state[65] + offset
-        objects[1].wh = size+4, size+3
+    if ram_state[83]:
+        # base y == 125
+        if ram_state[64] > 4:
+            if type(objects[1]) is NoObject:
+                objects[1] = PlayerShot()
+            size = ram_state[64] - 6
+            objects[1].xy = 77 - (size>>1), 101 + size*5 + ram_state[65] + offset
+            objects[1].wh = size+4, size+3
+        else:
+            objects[1] = NoObject()
+
+
+        # ram 37 == wh
+        if 63 < ram_state[37] and ram_state[35] and not ram_state[77]:
+            if type(objects[2]) is NoObject:
+                objects[2] = EnemyTank()
+            size = (ram_state[37]>>4) - 3
+            x, y = ram_state[32] - 4 - size, 87 + offset
+            w, h = 4 + size, size
+            if size < 3:
+                y +=2
+            elif size == 3:
+                y +=1
+
+            if 0 < ram_state[69] < 15:
+                w = w>>1
+                x+=w
+            elif ram_state[69]==15:
+                x+= 4 + (size>>2)
+
+            objects[2].xy = x, y
+            objects[2].wh = w, h
+
+        else:
+            objects[2] = NoObject()
+
+        if ram_state[67]:
+            if type(objects[3]) is NoObject:
+                objects[3] = EnemyShot()
+            size = 32 - ram_state[67]
+            y = 89 + offset
+
+            objects[3].xy = ram_state[62] - 1, y
+            objects[3].wh = (size>>1)+1, size - 1
+        else:
+            objects[3] = NoObject()
     else:
-        objects[1] = NoObject()
-
-
-    # ram 37 == wh
-    if 63 < ram_state[37] and ram_state[35] and not ram_state[77]:
-        if type(objects[2]) is NoObject:
-            objects[2] = EnemyTank()
-        size = (ram_state[37]>>4) - 3
-        x, y = ram_state[32] - 4 - size, 87 + offset
-        w, h = 4 + size, size
-        if size < 3:
-            y +=2
-        elif size == 3:
-            y +=1
-
-        if 0 < ram_state[69] < 15:
-            w = w>>1
-            x+=w
-        elif ram_state[69]==15:
-            x+= 4 + (size>>2)
-
-        objects[2].xy = x, y
-        objects[2].wh = w, h
-    else:
-        objects[2] = NoObject()
-
-    if ram_state[67]:
-        if type(objects[3]) is NoObject:
-            objects[3] = EnemyShot()
-        size = 32 - ram_state[67]
-        y = 89 + offset
-
-        objects[3].xy = ram_state[62] - 1, y
-        objects[3].wh = (size>>1)+1, size - 1
-    else:
-        objects[3] = NoObject()
+        for i in range(1, 4):
+            objects[i] = NoObject()
     
-    vision, radar, canon, tread = objects[4: 8]
+    if not ram_state[118]&4:
+        if not ram_state[77]:
+            if type(objects[4]) is NoObject:
+                objects[4] = EnemyRadarPosition()
+
+            radius = 12 - (ram_state[37]>>5)
+            degree = ram_state[32] + ram_state[33]*255
+
+            x, y = 83, 157
+            off_x, off_y = degree_to_xy(degree, radius)
+
+            x = int(x - off_x)
+            y = int(y - off_y)
+
+            objects[4].xy = x, y
+        else:
+            objects[4] = NoObject()
+    else:
+        objects[4] = NoObject()
+
+    vision, radar, canon, tread = objects[5: 9]
     for i, sensor in enumerate([tread, canon, radar, vision]):
-        if ram_state[118] >> i:
+        if ram_state[118]&(2**i):
             sensor.damaged = True
         else:
             sensor.damaged = False
     if hud:
         if ram_state[53]:
-            if type(objects[8]) is NoObject:
-                objects[8] = Score()
-            objects[8].wh = 6 + (8 * (ram_state[53] - 1)), 8
-            objects[8].value = ram_state[53]
-        else:
-            objects[8] = NoObject()
-
-        if ram_state[40]:
             if type(objects[9]) is NoObject:
-                objects[9] = Lives()
-            objects[9].wh = 6 + (8 * (ram_state[40] - 1)), 8
-            objects[9].value = ram_state[40]
+                objects[9] = Score()
+            objects[9].wh = 6 + (8 * (ram_state[53] - 1)), 8
+            objects[9].value = ram_state[53]
         else:
             objects[9] = NoObject()
+
+        if ram_state[40]:
+            if type(objects[10]) is NoObject:
+                objects[10] = Lives()
+            objects[10].wh = 6 + (8 * (ram_state[40] - 1)), 8
+            objects[10].value = ram_state[40]
+        else:
+            objects[10] = NoObject()
         
-        objects[10].value = _convert_number(ram_state[49])
+        objects[11].value = _convert_number(ram_state[49])
         
