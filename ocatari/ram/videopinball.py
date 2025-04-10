94d68640375ee9311d3eba8295ee4a2cf49c1d94
BJenrajb <bj.nutzmail@gmail.com>
2025-04-10 17:25:48 +0200

    VideoPinball ns_rework

--- ocatari/ram/videopinball.py
+++ ocatari/ram/videopinball.py
@@ -1,28 +1,27 @@
-from .game_objects import GameObject
+from .game_objects import GameObject, NoObject
 
 """
 RAM extraction for the game Fishing Derby.
 """
 
-MAX_NB_OBJECTS = {"Player1FishingString": 1,
-                  "Player2FishingString": 1, "Fish": 6, "Shark": 1}
-MAX_NB_OBJECTS_HUD = {"Player1FishingString": 1, "Player2FishingString": 1, "Fish": 6, "Shark": 1, "ScoreP1": 1,
-                      "ScoreP2": 1}
+MAX_NB_OBJECTS = {"Flipper": 2, "Ball": 1, "Spinner": 2, "DropTarget": 12, "Bumper": 8, "Plunger": 1}
+MAX_NB_OBJECTS_HUD = {"Flipper": 2, "Ball": 1, "Spinner": 2, "DropTarget": 12, "Bumper": 8, "Plunger": 1, "Score": 1,
+                      "LifeUsed": 1, "DifficultyLevel": 1}
 
 bumpers = (((104, 112), (16, 32)), ((104, 48), (4, 32)), ((116, 48), (4, 32)), ((40, 112), (16, 32)),
            ((40, 48), (4, 32)), ((52, 48), (4, 32)), ((72, 104), (16, 8)), ((72, 48), (16, 32)))
 droptgts = (((60, 24), (7, 14)), ((76, 24), (7, 14)), ((92, 24), (7, 14)),
             ((47, 58), (3, 12)), ((79, 58), (3, 12)), ((109, 58), (7, 12)),
-            ((47, 122), (3, 12)), ((77, 122), (7, 12)), ((111, 122), (3, 12)),
+            ((47, 122), (3, 12)), ((77, 120), (7, 14)), ((111, 122), (3, 12)),
             ((60, 154), (7, 12)), ((76, 154), (7, 12)), ((92, 154), (7, 12)))
 dp_follow_list = [72, 73, 74,
                   75, 76, 77,
                   81, 82, 83,
                   84, 85, 86]
-left_flip_map = {0: (64, 184, 13, 6), 16: (64, 181, 13, 9),
-                 32: (64, 181, 13, 9), 48: (64, 181, 13, 9)}
-right_flip_map = {128: (83, 184, 13, 6), 144: (
-    83, 181, 13, 9), 160: (83, 181, 13, 9), 176: (83, 181, 13, 9)}
+left_flip_map = {0: (64, 184, 13, 8), 16: (64, 184, 13, 6),
+                 32: (64, 181, 13, 9), 48: (64, 177, 13, 13)}
+right_flip_map = {128: (83, 184, 13, 8), 144: (
+    83, 184, 13, 6), 160: (83, 181, 13, 9), 176: (84, 177, 13, 13)}
 
 
 def compute_score(ram):
@@ -38,11 +37,13 @@ def manage_dptgts(dptgts, ram):
     for i, (tg, pos) in enumerate(zip(dptgts, dp_follow_list)):
         st = ram[pos]
         if st == 0:
-            if tg is not None:
-                dptgts[i] = None
+            if type(tg) is not NoObject:
+                dptgts[i] = NoObject()
         else:
-            if tg is None:
-                tg = DropTarget(*droptgts[i])
+            if type(tg) is NoObject:
+                tg = DropTarget()
+                tg.xy = droptgts[i][0]
+                tg.wh = droptgts[i][1]
                 dptgts[i] = tg
             if st == 80:  # diamond
                 tg.value = 20
@@ -53,10 +54,10 @@ def manage_dptgts(dptgts, ram):
 
 
 class Flipper(GameObject):
-    def __init__(self, left, *args, **kwargs):
+    def __init__(self, left=True, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.xy = (64, 184) if left else (83, 184)
-        self.wh = 13, 6
+        self.wh = 13, 7
         self.rgb = 236, 236, 236
         self.left = left
         self.hud = False
@@ -72,7 +73,7 @@ class Ball(GameObject):
 
 
 class Spinner(GameObject):
-    def __init__(self, left, *args, **kwargs):
+    def __init__(self, left=True, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.xy = (30, 91) if left else (126, 91)
         self.wh = 6, 10
@@ -81,28 +82,30 @@ class Spinner(GameObject):
 
 
 class DropTarget(GameObject):
-    def __init__(self, xy, wh, *args, **kwargs):
+    def __init__(self, x=0, y=0, w=0, h=0, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.xy = xy
-        self.wh = wh
+        self.xy = x, y
+        self.wh = w, h
         self.rgb = 210, 164, 74
         self.hud = False
         self.value = 1
 
 
-class Plunger(GameObject):
-    def __init__(self, *args, **kwargs):
+class Bumper(GameObject):
+    def __init__(self, x=0, y=0, w=0, h=0, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.rgb = 187, 159, 71
+        self.xy = x, y
+        self.wh = w, h
+        self.rgb = 104, 72, 198
         self.hud = False
 
 
-class Bumper(GameObject):
-    def __init__(self, xy, wh, *args, **kwargs):
+class Plunger(GameObject):
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.xy = xy
-        self.wh = wh
-        self.rgb = 104, 72, 198
+        self.xy = 149, 133
+        self.wh = 2, 41
+        self.rgb = 187, 159, 71
         self.hud = False
 
 
@@ -141,12 +144,12 @@ def _get_max_objects(hud=False):
 
 
 def _init_objects_ram(hud=False):
-    objects = [Flipper(True), Flipper(False), Ball(),
-               Spinner(True), Spinner(False)]
-    objects.extend([None for _ in dp_follow_list])
+    objects = [Flipper(left=True), Flipper(left=False), Ball(),
+               Spinner(left=True), Spinner(left=False)]
+    objects.extend([NoObject() for _ in dp_follow_list])
     for b_xy, b_wh in bumpers:
-        objects.append(Bumper(b_xy, b_wh))
-    objects.append(None)
+        objects.append(Bumper(b_xy[0], b_xy[1], b_wh[0], b_wh[1]))
+    objects.append(Plunger())
     if hud:
         objects.extend([Score(), LifeUsed(), DifficultyLevel()])
     return objects
@@ -159,9 +162,13 @@ def _detect_objects_ram(objects, ram_state, hud=False):
     flipr.xy = right_flip_map[ram_state[102]][:2]
     flipr.wh = right_flip_map[ram_state[102]][2:]
     ball.xy = ram_state[67], ram_state[68]-3
-    drop_targets = objects[5:5+12]
+    drop_targets = objects[5:17]
     manage_dptgts(drop_targets, ram_state)
-    objects[5:5+12] = drop_targets
+    objects[5:17] = drop_targets
+
+    # plunger
+    objects[25].xywh = 149, ram_state[70]*2 - 1, 2, (88-ram_state[70])*2 - 1
+
     if hud:
         score, lifeu, difflvl = objects[-3:]
         score.value = compute_score(ram_state)
--- ocatari/vision/videopinball.py
+++ ocatari/vision/videopinball.py
@@ -1,5 +1,5 @@
-from .game_objects import GameObject
-from .utils import find_objects
+from .game_objects import GameObject, NoObject
+from .utils import find_objects, match_objects
 
 objects_colors = {"ball": [104, 72, 198], "spinner": [236, 236, 236], "flipper": [236, 236, 236],
                   "target": [210, 164, 74], "plunger": [187, 159, 71], "bumper": [104, 72, 198], "hud": [187, 159, 71]}
@@ -75,41 +75,41 @@ class DifficultyLevel(GameObject):
 
 
 def _detect_objects(objects, obs, hud=True):
-    objects.clear()
     ball_h = 6
     ball_w = 4
     x_special_target = 0
-    for flipper in find_objects(obs, objects_colors["flipper"], miny=165, maxy=193, minx=X_MIN_GAMEZONE,
-                                maxx=145):
-        objects.append(Flipper(*flipper))
+
+    for flipper in find_objects(obs, objects_colors["flipper"], miny=165, maxy=193, minx=X_MIN_GAMEZONE, maxx=145):
+        if flipper[0] < 80:
+            objects[0].xywh = flipper
+        else:
+            objects[1].xywh = flipper
+
     for ball in find_objects(obs, objects_colors["ball"], minx=X_MIN_GAMEZONE,
                              maxx=X_MAX_GAMEZONE-1, closing_active=False):
         if ball[3] < ball_h and ball[2] < ball_w:
-            objects.append(Ball(*ball))
-    for drop_target in find_objects(obs, objects_colors["target"], minx=X_MIN_GAMEZONE,
-                                    maxx=X_MAX_GAMEZONE, miny=24, maxy=170):
-        objects.append(DropTarget(*drop_target))
+            objects[2].xywh = ball
+
     for spinner in find_objects(obs, objects_colors["spinner"], minx=X_MIN_GAMEZONE,
                                 maxx=X_MAX_GAMEZONE, miny=88, maxy=102, closing_dist=12):
-        objects.append(Spinner(*spinner))
-    for plunger in find_objects(obs, objects_colors["plunger"], minx=147,
-                                maxx=152, miny=88, maxy=102):
-        objects.append(Plunger(*plunger))
-    for bumper in find_objects(obs, objects_colors["bumper"], minx=26,
-                               maxx=131, miny=42, maxy=147):
-        if bumper[3] > ball_h and bumper[2] >= ball_w:
-            objects.append(Bumper(*bumper))
+        if spinner[0] < 80:
+            objects[3].xywh = spinner
+        else:
+            objects[4].xywh = spinner
+
+    drop_target = find_objects(obs, objects_colors["target"], minx=X_MIN_GAMEZONE,
+                                    maxx=X_MAX_GAMEZONE, miny=24, maxy=170)
+    match_objects(objects, drop_target, 5, 11, DropTarget)
+
     for special_target in find_objects(obs, objects_colors["target"], minx=69,
                                        maxx=140, miny=117, maxy=137):
-        x_special_target = special_target[0]
-        objects.append(DropTarget(*special_target))
-    if hud:
-        for score in find_objects(obs, objects_colors["hud"], minx=63, miny=0, maxy=16, closing_dist=6):
-            objects.append(Score(*score))
-        for life_used in find_objects(obs, objects_colors["hud"], minx=36,
-                                      maxx=60, miny=0, maxy=16):
-            objects.append(LifeUsed(*life_used))
-        for difficulty_level in find_objects(obs, objects_colors["hud"], minx=0,
-                                             maxx=35, miny=0, maxy=16):
-            objects.append(DifficultyLevel(*difficulty_level))
-    return
+        if type(objects[16]) is NoObject:
+            objects[16] = DropTarget(*special_target)
+        else:
+            objects[16].xywh = special_target
+
+    plunger = find_objects(obs, objects_colors["plunger"], minx=145,
+                                maxx=152, miny=88, maxy=180)
+    if plunger:
+        objects[25].xywh = plunger[0]
+    # objects.append(Plunger(*plunger))
