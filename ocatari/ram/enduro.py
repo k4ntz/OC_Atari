b7f45c749e0b85f60d6a8d49d2e1abaa0d21ed9c
azim <azimpour10221@gmail.com>
2025-04-27 18:10:41 +0200

    Added location extraction for opponent cars

--- ocatari/ram/enduro.py
+++ ocatari/ram/enduro.py
@@ -1,5 +1,6 @@
 from .game_objects import GameObject, NoObject
 from .utils import match_objects
+from ._helper_methods import _convert_number
 import sys
 import math
 
@@ -8,8 +9,8 @@ import math
 RAM extraction for the game Enduro.
 """
 
-MAX_NB_OBJECTS = {"Player": 1, "Car": 27}
-MAX_NB_OBJECTS_HUD = {"Player": 1, "Car": 27,
+MAX_NB_OBJECTS = {"Player": 1, "Car": 7}
+MAX_NB_OBJECTS_HUD = {"Player": 1, "Car": 7,
                       "NumberOfCars": 4, "PlayerScore": 6, "Level": 1}
 
 
@@ -81,9 +82,39 @@ def _init_objects_ram(hud=False):
     """
     objects = [Player()] + [NoObject()]*7
     if hud:
-        objects.extend([NoObject]*12)
+        objects.extend([NoObject()]*12)
     return objects
 
+def get_road_edges(lc, lr, rc, rr, y, td):
+    ldx = math.sqrt(lr**2 - (y - lc[1])**2)
+    if td >= 0:
+        lx = lc[0] - ldx
+    else:
+        lx = lc[0] + ldx
+    
+    rdx = math.sqrt(rr**2 - (y - rc[1])**2)
+    if td > 0:
+        rx = rc[0] - rdx
+    else:
+        rx = rc[0] + rdx
+    
+    return lx, rx
+
+def find_center(a, b, r, right_turn):
+    m = (a[0] + b[0]) / 2, (a[1] + b[1]) / 2
+    d = math.sqrt((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2)
+    h = math.sqrt(r ** 2 - (d / 2) ** 2)
+
+    s = (a[1] - b[1]) / (math.copysign(max(1, abs(a[0] - b[0])), a[0] - b[0]))
+
+    dy = math.sqrt(h**2 / (1 + s**2))
+    dx = 0 - math.sqrt((s*h)**2 / (1 + s**2))
+    if right_turn:
+        c = round(m[0] - dx), round(m[1] + dy)
+    else:
+        c = round(m[0] + dx), round(m[1] + dy)
+    
+    return c
 
 def _detect_objects_ram(objects, ram_state, hud=False):
     """
@@ -92,46 +123,68 @@ def _detect_objects_ram(objects, ram_state, hud=False):
     """
     # 106 ram_state is somewhat controlling the y of the player when it's dying by sinking
     # ram 54 was the previous x position migh be important for turns
+
     x = 77
     if ram_state[46] & 128:
-        x += ((~ram_state[46]) >> 1)+2
+        _x = 255 - ram_state[46]
+        x = 77 + round(_x * 17 / 26)
     else:
-        x -= (ram_state[46] >> 1)
+        _x = ram_state[46]
+        x = 77 - round(_x * 13 / 25)
     objects[0].xy = x, 89 + ram_state[52]
 
-    # ram 34 changes depending on turn, I guess it is a singed byte value
-    car_bb = []
+    
+    # ppd: player perspective drift
+    if ram_state[46] & 128:
+        ppd = round((x - 77) * 27 / 17)
+    else:
+        ppd = round((x - 77) * 32 / 17)
+    
+    # td: turns drift
+    if ram_state[34] & 128:
+        td = 0 - (255 - ram_state[34])
+    else:
+        td = ram_state[34]
+    
+    # end point: 87 + td - ppd, 51
+    # left start point: 37 - ppd, 154
+    # right start point: 138 - ppd, 154
+
+    # finding the roads edge curves, assuming they are parts of two circles
+    if td == 0:
+        rr = 4500
+        lr = 4500
+    else:
+        rr = abs(min(96 / td, 225 / td)) * 50
+        lr = abs(max(96 / td, 225 / td)) * 50
+    lc = find_center((87 + td - ppd, 51), (37 - ppd, 154), lr, td >= 0)
+    rc = find_center((87 + td - ppd, 51), (138 - ppd, 154), rr, td > 0)
+    
+    xs = []
+    ys = []
     for i in range(7):
+        objects[i + 1] = NoObject()
         if ram_state[27+i]:
-            car = Car()
-            # x = 0
-
-            # # x: 103, 103, 104, 104, 104, 105, 105, 106, 107, 107, 108, 108, 108, 108
-
-            # if ram_state[46]&128:
-            #     x = x - (~ram_state[46])
-            # else:
-            #     x = x + ram_state[46]
-
             y = 150 - int(1.2*((18-i)*i) + ((ram_state[59] >> 3)*(0.9**i)))
-            x = 0
-
-            x_drift = ram_state[46]
-            if x_drift & 128:
-                x_drift = (-(ram_state[46]))*(-1)
-
-            if not ram_state[27+i] & 16:
-                x = 87
-            elif ram_state[27+i] & 1:
-                x = int((2192 + (7*y))/30) + x_drift
-            else:
-                # 50  + int(((5*(0.95**i))*i) + ((ram_state[59]>>4)&7)*(0.9**i))
-                x = int(((6498 - (23*y))/60) + x_drift)
-
-            car.xy = x, y
-            car_bb.append(car.xywh)
-
-    match_objects(objects, car_bb, 1, 7, Car)
+            ys.append(y)
+            xs.append(ram_state[27+i])
+    
+    ys = sorted(ys, reverse=True)
+    _xs = [20, 80, 130]
+    for i in range(len(xs)):
+        lx, rx = get_road_edges(lc, lr, rc, rr, ys[i], td)
+        w = 16 * (ys[i] - 50) / 103 + 1
+        h = 10 * (ys[i] - 50) / 103 + 1
+        if xs[i]%32 == 22:
+            x = lx + (rx - lx) * 0.25
+        elif xs[i]%32 == 23:
+            x = lx + (rx - lx) * 0.75
+        else:
+            x = lx + (rx - lx) * 0.5
+        car = Car()
+        car.xy = round(x - w / 2), ys[i]
+        car.wh = w, h
+        objects[i + 1] = car
 
     if hud:
         # ram_state[45] indicates the level
