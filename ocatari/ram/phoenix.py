f5cd6cf59bed122cba85916dde50ab7627c0ad63
BJenrajb <bj.nutzmail@gmail.com>
2025-06-05 19:27:36 +0200

    Phoenix ns_implementation

--- ocatari/ram/phoenix.py
+++ ocatari/ram/phoenix.py
@@ -1,10 +1,10 @@
-from .game_objects import GameObject, ValueObject
+from .game_objects import GameObject, ValueObject, NoObject
 from ._helper_methods import _convert_number
 import sys
 
-MAX_NB_OBJECTS = {"Player": 1, "Player_Projectile": 1, "Phoenix": 8, "Bat": 7,
+MAX_NB_OBJECTS = {"Player": 1, "Player_Projectile": 1, "Enemy_Projectile": 4, "Phoenix": 8, "Bat": 7,
                   "Boss": 1, "Boss_Block_Green": 2, "Boss_Block_Blue": 48, "Boss_Block_Red": 104}
-MAX_NB_OBJECTS_HUD = {"Player": 1, "Player_Projectile": 1, "Phoenix": 8, "Bat": 7, "Boss": 1,
+MAX_NB_OBJECTS_HUD = {"Player": 1, "Player_Projectile": 1, "Enemy_Projectile": 4, "Phoenix": 8, "Bat": 7, "Boss": 1,
                       "Boss_Block_Green": 2, "Boss_Block_Blue": 48, "Boss_Block_Red": 104, "Score": 1, "Life": 5}
 
 
@@ -179,11 +179,10 @@ def _init_objects_ram(hud=False):
     (Re)Initialize the objects
     """
     objects = [Player()]
-    objects.extend([None] * 168)
+    objects.extend([NoObject()] * 184)
 
     if hud:
-        objects.extend([Score()])
-        objects.extend([None] * 5)
+        objects.extend([Score(), NoObject()])
     return objects
 
 
@@ -201,8 +200,11 @@ def _detect_objects_ram(objects, ram_state, hud=False):
     objects[0].xy = ram_state[94] - 70, 173
 
     if not ram_state[84]:
-        objects[1] = Player_Projectile()
+        if type(objects[1]) is NoObject:
+            objects[1] = Player_Projectile()
         objects[1].xy = ram_state[93] - 71, 201 - ram_state[89]
+    else:
+        objects[1] = NoObject()
 
     for i in range(4):
         if ram_state[58+i] < 250:
@@ -212,13 +214,13 @@ def _detect_objects_ram(objects, ram_state, hud=False):
                 x = 5 + (right-3) * 15 + (left ^ 15) + 1
             else:
                 x = 5 + (right-3) * 15 - left
-
-            objects[2+i] = Enemy_Projectile()
+            if type(objects[2+i]) is NoObject:
+                objects[2+i] = Enemy_Projectile()
             objects[2+i].xy = x, 189 - ram_state[58+i]
         else:
-            objects[2+i] = None
+            objects[2+i] = NoObject()
 
-    if ram_state[11]:
+    if ram_state[74] < 4: #ram_state[11]:
         # ram[99-106] == enemy_x
         # ram[41-44] == enemy_y, ram[41] corresponds to ram[99] and ram[103] and so on
         # ram[27-30] == left side types, ram[34-37] == right side types
@@ -230,25 +232,34 @@ def _detect_objects_ram(objects, ram_state, hud=False):
                 # orange
                 rgb = 227, 151, 89
             for i in range(4):
+                enemy = objects[6+i]
+                enemy2 = objects[10+i]
+
                 # left side enemies
                 if ram_state[27+i] & 6 == 6:
-                    enemy = None
+                    objects[6+i] = NoObject()
                 else:
-                    enemy = Phoenix()
+                    if type(enemy) is NoObject:
+                        enemy = Phoenix()
+                        objects[6+i] = enemy
                     enemy.rgb = rgb
                     # enemy.xy = ram_state[99+i] - 70, 204 - ram_state[41+i]
                     enemy.xy = ram_state[99+i] - 70, 204 - ram_state[41+i]
 
                 # right side enemies
                 if ram_state[34+i] & 6 == 6:
-                    enemy2 = None
+                    objects[10+i] = NoObject()
                 else:
-                    enemy2 = Phoenix()
+                    if type(enemy2) is NoObject:
+                        enemy2 = Phoenix()
+                        objects[10+i] = enemy2
                     enemy2.rgb = rgb
                     enemy2.xy = ram_state[103+i] - 70, 204 - ram_state[41+i]
+            
+            # remove all bats
+            for i in range(14, 20):
+                objects[i] = NoObject()
 
-                objects[5+i] = enemy
-                objects[9+i] = enemy2
         else:
             # ram[27-33] == left wing, ram[34-40] == right wing
             if ram_state[27] & 32:
@@ -260,7 +271,10 @@ def _detect_objects_ram(objects, ram_state, hud=False):
             for i in range(7):
                 wing_left = ram_state[27+i] & 15
                 if wing_left < 8:
-                    bat = Bat()
+                    if type(objects[14+i]) is NoObject:
+                        bat = Bat()
+                    else:
+                        bat = objects[14+i]
                     x = ram_state[99+i] - 65
                     w, h = 8, 10
                     # left wing
@@ -298,24 +312,33 @@ def _detect_objects_ram(objects, ram_state, hud=False):
                     bat.xy = x, 201 - ram_state[41+i]
                     bat.wh = w, h
                     bat.rgb = rgb
-                    objects[5+i] = bat
+                    objects[14+i] = bat
 
                 else:
-                    objects[5+i] = None
-            objects[12] = None
-        for i in range(13, 169):
-            objects[i] = None
+                    objects[14+i] = NoObject()
+            # remove all phoenixes
+            for i in range(5, 14):
+                objects[i] = NoObject()
+        # remove all boss phase objects
+        if type(objects[21]) is not NoObject:
+            for i in range(21, 184):
+                objects[i] = NoObject()
     else:
+        for i in range(6, 21):
+            objects[i] = NoObject()
         # boss -> ram[33-46] == red blocks, ram[27-32] == blue blocks (moving one block every time)
         # ram[95] == boss_y
-        objects[5] = Boss()
-        objects[5].xy = 76, 198 - ram_state[95]
+        if type(objects[21]) is NoObject:
+            objects[21] = Boss()
+        objects[21].xy = 76, 198 - ram_state[95]
 
         # indestructable blocks, simple implementation
-        objects[6] = Boss_Block_Green()
-        objects[6].xy = 56, 199 - ram_state[95]
-        objects[7] = Boss_Block_Green()
-        objects[7].xy = 84, 199 - ram_state[95]
+        if type(objects[22]) is NoObject:
+            objects[22] = Boss_Block_Green()
+        objects[22].xy = 56, 199 - ram_state[95]
+        if type(objects[23]) is NoObject:
+            objects[23] = Boss_Block_Green()
+        objects[23].xy = 84, 199 - ram_state[95]
 
         # Outer blocks of first 2 blue rows
         left1 = ram_state[27] >> 4
@@ -324,44 +347,54 @@ def _detect_objects_ram(objects, ram_state, hud=False):
         right2 = ram_state[30] & 15
         for i in range(4):
             if 2**(3-i) & left1:
-                objects[8+i] = Boss_Block_Blue(32+i*4, 214 - ram_state[95])
+                if type(objects[24+i]) is NoObject:
+                    objects[24+i] = Boss_Block_Blue()
+                objects[24+i].xy = 32+i*4, 214 - ram_state[95]
             else:
-                objects[8+i] = None
+                objects[24+i] = NoObject()
 
             if 2**i & right1:
-                objects[12+i] = Boss_Block_Blue(112+i*4, 214 - ram_state[95])
+                if type(objects[28+i]) is NoObject:
+                    objects[28+i] = Boss_Block_Blue()
+                objects[28+i].xy = 112+i*4, 214 - ram_state[95]
             else:
-                objects[12+i] = None
+                objects[28+i] = NoObject()
 
             if 2**(3-i) & left2:
-                objects[16+i] = Boss_Block_Blue(32+i*4, 216 - ram_state[95])
+                if type(objects[32+i]) is NoObject:
+                    objects[32+i] = Boss_Block_Blue()
+                objects[32+i].xy = 32+i*4, 216 - ram_state[95]
             else:
-                objects[16+i] = None
+                objects[32+i] = NoObject()
 
             if 2**i & right2:
-                objects[20+i] = Boss_Block_Blue(112+i*4, 216 - ram_state[95])
+                if type(objects[36+i]) is NoObject:
+                    objects[36+i] = Boss_Block_Blue()
+                objects[36+i].xy = 112+i*4, 216 - ram_state[95]
             else:
-                objects[20+i] = None
+                objects[36+i] = NoObject()
 
         # left side of blue blocks
         states = [28, 31]
         for i in range(2):
             for j in range(8):
                 if ram_state[states[i]] & (2**j):
-                    objects[24+j+(8*i)] = Boss_Block_Blue(48 +
-                                                          j*4, 214+(3*i) - ram_state[95])
+                    if type(objects[40+j+(8*i)]) is NoObject:
+                        objects[40+j+(8*i)] = Boss_Block_Blue()
+                    objects[40+j+(8*i)].xy = 48 + j*4, 214+(3*i) - ram_state[95]
                 else:
-                    objects[24+j+(8*i)] = None
+                    objects[40+j+(8*i)] = NoObject()
 
         # righte side of blue block
         states = [29, 32]
         for i in range(2):
             for j in range(8):
                 if ram_state[states[i]] & (2**(7-j)):
-                    objects[40+j+(8*i)] = Boss_Block_Blue(80 +
-                                                          j*4, 214+(3*i) - ram_state[95])
+                    if type(objects[56+j+(8*i)]) is NoObject:
+                        objects[56+j+(8*i)] = Boss_Block_Blue()
+                    objects[56+j+(8*i)].xy =80 + j*4, 214+(3*i) - ram_state[95]
                 else:
-                    objects[40+j+(8*i)] = None
+                    objects[56+j+(8*i)] = NoObject()
 
         # Outer blocks of first 2 red rows
         left1 = ram_state[33] >> 4
@@ -370,44 +403,54 @@ def _detect_objects_ram(objects, ram_state, hud=False):
         right2 = ram_state[36] & 15
         for i in range(4):
             if 2**(3-i) & left1:
-                objects[56+i] = Boss_Block_Red(32+i*4, 218 - ram_state[95])
+                if type(objects[72+i]) is NoObject:
+                    objects[72+i] = Boss_Block_Red()
+                objects[72+i].xy = 32+i*4, 218 - ram_state[95]
             else:
-                objects[56+i] = None
+                objects[72+i] = NoObject()
 
             if 2**i & right1:
-                objects[60+i] = Boss_Block_Red(112+i*4, 218 - ram_state[95])
+                if type(objects[76+i]) is NoObject:
+                    objects[76+i] = Boss_Block_Red()
+                objects[76+i].xy = 112+i*4, 218 - ram_state[95]
             else:
-                objects[60+i] = None
+                objects[76+i] = NoObject()
 
             if 2**(3-i) & left2:
-                objects[64+i] = Boss_Block_Red(32+i*4, 221 - ram_state[95])
+                if type(objects[80+i]) is NoObject:
+                    objects[80+i] = Boss_Block_Red()
+                objects[80+i].xy = 32+i*4, 221 - ram_state[95]
             else:
-                objects[64+i] = None
+                objects[80+i] = NoObject()
 
             if 2**i & right2:
-                objects[68+i] = Boss_Block_Red(112+i*4, 221 - ram_state[95])
+                if type(objects[84+i]) is NoObject:
+                    objects[84+i] = Boss_Block_Red()
+                objects[84+i].xy = 112+i*4, 221 - ram_state[95]
             else:
-                objects[68+i] = None
+                objects[84+i] = NoObject()
 
         # left side of red blocks
         states = [34, 37, 39, 41, 43]
         for i in range(5):
             for j in range(8):
                 if ram_state[states[i]] & (2**j):
-                    objects[72+j+(8*i)] = Boss_Block_Red(48+j *
-                                                         4, 218+(3*i) - ram_state[95])
+                    if type(objects[88+j+(8*i)]) is NoObject:
+                        objects[88+j+(8*i)] = Boss_Block_Red()
+                    objects[88+j+(8*i)].xy = 48+j * 4, 218+(3*i) - ram_state[95]
                 else:
-                    objects[72+j+(8*i)] = None
+                    objects[88+j+(8*i)] = NoObject()
 
         # righte side of red block
         states = [35, 38, 40, 42, 44]
         for i in range(5):
             for j in range(8):
                 if ram_state[states[i]] & (2**(7-j)):
-                    objects[112+j+(8*i)] = Boss_Block_Red(80 +
-                                                          j*4, 218+(3*i) - ram_state[95])
+                    if type(objects[128+j+(8*i)]) is NoObject:
+                        objects[128+j+(8*i)] = Boss_Block_Red()
+                    objects[128+j+(8*i)].xy = 80 + j*4, 218+(3*i) - ram_state[95]
                 else:
-                    objects[112+j+(8*i)] = None
+                    objects[128+j+(8*i)] = NoObject()
 
         # last 2 rows
         for i in range(2):
@@ -415,16 +458,18 @@ def _detect_objects_ram(objects, ram_state, hud=False):
             right = ram_state[45+i] & 15
             for j in range(4):
                 if (2**j) & left:
-                    objects[152+j+(8*i)] = Boss_Block_Red(64 +
-                                                          j*4, 233+(3*i) - ram_state[95])
+                    if type(objects[168+j+(8*i)]) is NoObject:
+                        objects[168+j+(8*i)] = Boss_Block_Red()
+                    objects[168+j+(8*i)].xy = 64 + j*4, 233+(3*i) - ram_state[95]
                 else:
-                    objects[152+j+(8*i)] = None
+                    objects[168+j+(8*i)] = NoObject()
 
                 if (2**(3-j)) & right:
-                    objects[156+j+(8*i)] = Boss_Block_Red(80 +
-                                                          j*4, 233+(3*i) - ram_state[95])
+                    if type(objects[172+j+(8*i)]) is NoObject:
+                        objects[172+j+(8*i)] = Boss_Block_Red()
+                    objects[172+j+(8*i)].xy = 80 + j*4, 233+(3*i) - ram_state[95]
                 else:
-                    objects[156+j+(8*i)] = None
+                    objects[172+j+(8*i)] = NoObject()
 
     if hud:
         # ram[71-73] == score
@@ -446,15 +491,17 @@ def _detect_objects_ram(objects, ram_state, hud=False):
             x -= 8
             w += 8
 
-        objects[-6].xy = x, y
-        objects[-6].wh = w, h
-        objects[-6].value = _convert_number(ram_state[73])*10000 + _convert_number(
+        objects[-2].xy = x, y
+        objects[-2].wh = w, h
+        objects[-2].value = _convert_number(ram_state[73])*10000 + _convert_number(
             ram_state[72])*100 + _convert_number(ram_state[71])
 
         lives = ram_state[75] & 7
-        for i in range(5):
-            if i+1 < lives:
-                objects[-5+i] = Life()
-                objects[-5+i].xy = 99 - (4*i), 13
-            else:
-                objects[-5+i] = None
+        if ram_state[75] & 7 > 1:
+            if type(objects[-1]) is NoObject:
+                objects[-1] = Life()
+            objects[-1].xy = 99 - 4*(lives - 2), 13
+            objects[-1].wh = 3 + 4*(lives - 2), 5
+            objects[-1].value = lives - 1
+        else:
+            objects[-1] = NoObject()
--- ocatari/vision/phoenix.py
+++ ocatari/vision/phoenix.py
@@ -1,5 +1,5 @@
-from .utils import find_objects, find_mc_objects
-from .game_objects import GameObject
+from .utils import find_objects, find_mc_objects, match_objects
+from .game_objects import GameObject, NoObject
 
 
 objects_colors = {"player": [213, 130, 74], "player_projectile": [158, 208, 101], "enemy_projectile": [236, 236, 236], "enemy_projectile2": [227, 151, 89],
@@ -77,89 +77,105 @@ class Life(GameObject):
 
 def _detect_objects(objects, obs, hud=False):
     # detection and filtering
-    objects.clear()
 
     player = find_objects(obs, objects_colors["player"], miny=100)
-    for el in player:
-        objects.append(Player(*el))
+    if player:
+        objects[0].xywh = player[0]
 
     pproj = find_objects(obs, objects_colors["player_projectile"])
     for el in pproj:
         if el[2] == 1:
-            objects.append(Player_Projectile(*el))
+            if type(objects[1]) is not Player_Projectile:
+                objects[1] = Player_Projectile(*el)
+            else:
+                objects[1].xywh = el
+            break
+    else:
+        objects[1] = NoObject()
 
     phoenix1 = find_mc_objects(
-        obs, [objects_colors["phoenix_orange"], objects_colors["phoenix_base"]])
-    for el in phoenix1:
-        if el[2] > 1:
-            objects.append(Phoenix(*el))
-
-    phoenix2 = find_mc_objects(
-        obs, [objects_colors["phoenix_green"], objects_colors["phoenix_base"]])
-    for el in phoenix2:
-        if el[2] > 1:
-            ph = Phoenix(*el)
-            ph.rgb = objects_colors["phoenix_green"]
-            objects.append(ph)
+        obs, [objects_colors["phoenix_orange"], objects_colors["phoenix_base"]], size=(6, 8), tol_s=2)
+    phoenix1.extend(find_mc_objects(
+        obs, [objects_colors["phoenix_green"], objects_colors["phoenix_base"]], size=(6, 8), tol_s=2))
+    
+    match_objects(objects, phoenix1[:8], 6, 8, Phoenix)
 
     green = find_objects(obs, objects_colors["block_green"])
-    for el in green:
-        objects.append(Boss_Block_Green(*el))
+    match_objects(objects, green, 22, 2, Boss_Block_Green)
 
     blue = find_objects(obs, objects_colors["block_blue"])
-    for el in blue:
-        # if obs[el[1]][el[0]][0] == 45:
-        for y in range(int(el[3]/3)):
-            for x in range(int(el[2]/4)):
-                if obs[el[1]+3*y][el[0]+4*x][0] == 45:
-                    objects.append(Boss_Block_Blue(el[0]+4*x, el[1]+3*y, 4, 3))
+    match_objects(objects, blue, 24, 48, Boss_Block_Blue)
+    # for el in blue:
+    #     # if obs[el[1]][el[0]][0] == 45:
+    #     for y in range(int(el[3]/3)):
+    #         for x in range(int(el[2]/4)):
+    #             if obs[el[1]+3*y][el[0]+4*x][0] == 45:
+    #                 objects.append(Boss_Block_Blue(el[0]+4*x, el[1]+3*y, 4, 3))
 
-    boss = find_mc_objects(
-        obs, [objects_colors["block_red"], objects_colors["boss"]])
-    for el in boss:
-        objects.append(Boss(*el))
+    boss = find_mc_objects(obs, [objects_colors["block_red"], objects_colors["boss"]])
+    if boss:
+        if type(objects[21]) is NoObject:
+            objects[21] = Boss(*boss[0])
+        else:
+            objects[21].xywh = boss[0]
 
     if len(green) > 0:
         red = find_objects(obs, objects_colors["block_red"])
-        for el in red:
-            if obs[el[1]][el[0]][0] == 167:
-                for y in range(int(el[3]/3)):
-                    for x in range(int(el[2]/4)):
-                        if obs[el[1]+3*y][el[0]+4*x][0] == 167:
-                            objects.append(Boss_Block_Red(
-                                el[0]+4*x, el[1]+3*y, 4, 3))
+        match_objects(objects, red, 72, 112, Boss_Block_Red)
+        # for el in red:
+        #     if obs[el[1]][el[0]][0] == 167:
+        #         for y in range(int(el[3]/3)):
+        #             for x in range(int(el[2]/4)):
+        #                 if obs[el[1]+3*y][el[0]+4*x][0] == 167:
+        #                     objects.append(Boss_Block_Red(
+        #                         el[0]+4*x, el[1]+3*y, 4, 3))
     else:
         bat2 = find_mc_objects(obs, [objects_colors["bat_red1"], objects_colors["bat_red2"],
                                objects_colors["bat_red3"], objects_colors["bat_red4"], objects_colors["bat_red5"]], all_colors=False)
+        bat = []
+        pe = []
         for el in bat2:
             if el[2] > 1:
-                ph = Bat(*el)
-                ph.rgb = objects_colors["bat_red2"]
-                objects.append(ph)
-            else:
-                objects.append(Enemy_Projectile(*el))
+                bat.append(el)
+            elif el[3] > 2:
+                pe.append(el)
+                
+        match_objects(objects, bat, 14, 7, Bat)
+        match_objects(objects, pe, 2, 4, Enemy_Projectile)
 
     bat1 = find_mc_objects(obs, [objects_colors["bat_blue1"], objects_colors["bat_blue2"],
                            objects_colors["bat_blue3"], objects_colors["bat_blue4"], objects_colors["bat_blue5"]], all_colors=False)
+    bat = []
+    pe = []
     for el in bat1:
         if el[2] > 1:
-            objects.append(Bat(*el))
-        else:
-            objects.append(Enemy_Projectile(*el))
+            bat.append(el)
+        elif el[3] > 2:
+            pe.append(el)
+    match_objects(objects, bat, 14, 7, Bat)
+    match_objects(objects, pe, 2, 4, Enemy_Projectile)
 
     eproj = find_objects(obs, objects_colors["enemy_projectile"])
     eproj.extend(find_objects(obs, objects_colors["enemy_projectile2"]))
+    pe = []
     for el in eproj:
-        if el[2] == 1:
-            objects.append(Enemy_Projectile(*el))
+        if el[2] == 1 and el[3] > 2:
+            pe.append(el)
+
+    match_objects(objects, pe, 2, 4, Enemy_Projectile)
 
     if hud:
         score = find_objects(
             obs, objects_colors["score"], maxy=50, closing_dist=20)
-        for el in score:
-            objects.append(Score(*el))
+        if score:
+            objects[-2].xywh = score[0]
 
         life = find_objects(
-            obs, objects_colors["player"], maxy=50, closing_active=False)
-        for el in life:
-            objects.append(Life(*el))
+            obs, objects_colors["player"], maxy=50)
+        if life:
+            if type(objects[-1]) is not Life:
+                objects[-1] = Life(*life[0])
+            else:
+                objects[-1].xywh = life[0]
+        else:
+            objects[-1] = NoObject()
